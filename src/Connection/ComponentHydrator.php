fecb1a719d363a9724a5733f9249391a150afeef
Caleb Porzio <calebporzio@gmail.com>
2019-03-28 11:51:21 -0400

    Switch to base64 encode

--- src/Connection/ComponentHydrator.php
+++ src/Connection/ComponentHydrator.php
@@ -6,17 +6,17 @@ class ComponentHydrator
 {
     public static function dehydrate($instance)
     {
-        return encrypt([
+        return base64_encode(serialize([
             'class' => get_class($instance),
             'id' => $instance->id,
             'prefix' => $instance->prefix,
             'properties' => $instance->getAllPropertiesDefinedBySubClass(),
-        ]);
+        ]));
     }
 
     public static function hydrate($serialized)
     {
-        list($class, $id, $prefix, $properties) = array_values(decrypt($serialized));
+        list($class, $id, $prefix, $properties) = array_values(unserialize(base64_decode($serialized)));
 
         return tap(new $class($id, $prefix), function ($unHydratedInstance) use ($properties) {
             foreach ($properties as $property => $value) {
