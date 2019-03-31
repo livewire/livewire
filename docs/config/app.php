18cf4975186dcfc7fea5dda475991ede7afb892f
Caleb Porzio <calebporzio@gmail.com>
2019-03-31 15:24:31 -0400

    Fix missing Service Provider

--- docs/config/app.php
+++ docs/config/app.php
@@ -1,5 +1,10 @@
 <?php
 
+class ForSomeReasonGitDownNeedsThisToWorkServiceProvider extends Illuminate\Support\ServiceProvider
+{
+    public function boot() { app('view'); }
+}
+
 return [
     'name' => 'Livewire Documentation',
     'env' => env('APP_ENV', 'production'),
@@ -13,19 +18,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         // Non-Laravel Service Providers.
-        AppServiceProvider::class,
+        ForSomeReasonGitDownNeedsThisToWorkServiceProvider::class,
         GitDown\GitDownServiceProvider::class,
     ],
 ];
-
-class AppServiceProvider extends Illuminate\Support\ServiceProvider
-{
-    public function boot()
-    {
-        // For some silly reason, this line needs to be here for the
-        // @gitdown blade directive to register.
-        // This line effectively makes the "blade.compiler" available
-        // to the rest of the application.
-        app('view');
-    }
-}
