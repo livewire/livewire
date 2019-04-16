9666005eecd33d24a7c6f2d49cf998eb5e663357
David Piesse <piesse@gmail.com>
2019-04-16 21:16:58 +0100

    Add Echo Tests

--- tests/js/event_listeners.spec.js
+++ tests/js/event_listeners.spec.js
@@ -1,5 +1,6 @@
 import { wait } from 'dom-testing-library'
 import { mountWithEvent } from './utils'
+import MockEcho from 'mock-echo'
 
 test('receive event from global fire', async () => {
     var payload
@@ -24,5 +25,54 @@ test('receive event from action fire', async () => {
         expect(payload.actionQueue[0].type).toEqual('fireEvent')
         expect(payload.actionQueue[0].payload.event).toEqual('foo')
         expect(payload.actionQueue[0].payload.params).toEqual(['bar'])
+    })
+})
+
+describe('test Laravel Echo', () => {
+    let mockEcho
+
+    beforeEach(() => {
+        mockEcho = new MockEcho()
+        global.Echo = mockEcho
+    })
+
+    afterEach(() => {
+        delete global.Echo
+    })
+
+    test('public echo channel is created and reacts', async () => {
+        expect(mockEcho.channelExist('foo')).toBe(false)
+    
+        var payload
+        mountWithEvent('<div></div>', ['echo:foo,bar'], i => payload = i)
+    
+        expect(mockEcho.channelExist('foo')).toBe(true)
+        expect(mockEcho.getChannel('foo').eventExist('bar')).toBe(true)
+    
+        mockEcho.getChannel('foo').broadcast('bar', 'baz')
+    
+        await wait(() => {
+            expect(payload.actionQueue[0].type).toEqual('fireEvent')
+            expect(payload.actionQueue[0].payload.event).toEqual('echo:foo,bar')
+            expect(payload.actionQueue[0].payload.params).toEqual(['baz'])
+        })
+    })
+    
+    test('private echo channel is created and reacts', async () => {
+        expect(mockEcho.privateChannelExist('foo')).toBe(false)
+    
+        var payload
+        mountWithEvent('<div></div>', ['echo-private:foo,bar'], i => payload = i)
+    
+        expect(mockEcho.privateChannelExist('foo')).toBe(true)
+        expect(mockEcho.getPrivateChannel('foo').eventExist('bar')).toBe(true)
+    
+        mockEcho.getPrivateChannel('foo').broadcast('bar', 'baz')
+    
+        await wait(() => {
+            expect(payload.actionQueue[0].type).toEqual('fireEvent')
+            expect(payload.actionQueue[0].payload.event).toEqual('echo-private:foo,bar')
+            expect(payload.actionQueue[0].payload.params).toEqual(['baz'])
+        })
     })
 })
