563c109cdd50a16a8940eeb3eb5b9f3481b385c6
Caleb Porzio <calebporzio@gmail.com>
2019-05-03 21:28:03 -0400

    Refactor make command

---
+++ tests/LivewireMakeCommandParserTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests;
+
+use Livewire\Commands\LivewireMakeCommandParser;
+
+class LivewireMakeCommandParserTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider classPathProvider
+     */
+    function something($input, $component, $namespace, $classPath, $viewName, $viewPath)
+    {
+        $parser = new LivewireMakeCommandParser(
+            '', // App directory path.
+            '', // Views directory path.
+            $input
+        );
+
+        $this->assertEquals($component, $parser->component());
+        $this->assertEquals($namespace, $parser->classNamespace());
+        $this->assertEquals($classPath, $parser->classPath());
+        $this->assertEquals($viewName, $parser->viewName());
+        $this->assertEquals($viewPath, $parser->viewPath());
+    }
+
+    function classPathProvider()
+    {
+        return [
+            [
+                'Foo',
+                'Foo',
+                'App\Http\Livewire',
+                '/Http/Livewire/Foo.php',
+                'livewire.foo',
+                '/livewire/foo.blade.php',
+            ],
+            [
+                'Foo\\Bar',
+                'Bar',
+                'App\Http\Livewire\Foo',
+                '/Http/Livewire/Foo/Bar.php',
+                'livewire.foo.bar',
+                '/livewire/foo/bar.blade.php',
+            ],
+            [
+                'Foo/Bar',
+                'Bar',
+                'App\Http\Livewire\Foo',
+                '/Http/Livewire/Foo/Bar.php',
+                'livewire.foo.bar',
+                '/livewire/foo/bar.blade.php',
+            ],
+            [
+                'Foo\Bar',
+                'Bar',
+                'App\Http\Livewire\Foo',
+                '/Http/Livewire/Foo/Bar.php',
+                'livewire.foo.bar',
+                '/livewire/foo/bar.blade.php',
+            ],
+            [
+                'FooBar',
+                'FooBar',
+                'App\Http\Livewire',
+                '/Http/Livewire/FooBar.php',
+                'livewire.foo-bar',
+                '/livewire/foo-bar.blade.php',
+            ],
+            [
+                'FooBar\\FooBar',
+                'FooBar',
+                'App\Http\Livewire\FooBar',
+                '/Http/Livewire/FooBar/FooBar.php',
+                'livewire.foo-bar.foo-bar',
+                '/livewire/foo-bar/foo-bar.blade.php',
+            ],
+        ];
+    }
+
+
+}
