dbbb2aa4c55739505b24022e3853610dfdc998f9
Kofi Boakye <2kodes@gmail.com>
2019-07-30 04:10:05 +0000

    Allow requests to be made correctly
    Currently requests made with the forward slash `/livewire/` will 404 when the livewire app is not being served from the root folder of a webserver or being run with `php artisan serve`
    
    This fix allows the request to be made to the correct root path of the app irrespective of the hosting path

--- src/js/connection/drivers/http.js
+++ src/js/connection/drivers/http.js
@@ -7,7 +7,7 @@ export default {
     },
 
     keepAlive() {
-        fetch('/livewire/keep-alive', {
+        fetch('livewire/keep-alive', {
             credentials: "same-origin",
             headers: {
                 'X-CSRF-TOKEN': this.getCSRFToken(),
@@ -18,7 +18,7 @@ export default {
 
     sendMessage(payload) {
         // Forward the query string for the ajax requests.
-        fetch('/livewire/message'+window.location.search, {
+        fetch('livewire/message'+window.location.search, {
             method: 'POST',
             body: JSON.stringify(payload),
             // This enables "cookies".
