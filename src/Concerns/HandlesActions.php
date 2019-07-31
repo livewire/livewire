4ba16906efa842acfba148eb1c2d986bd4721713
Justin Seliga <justinrseliga@gmail.com>
2019-07-31 11:25:54 -0400

    helper method to resolve method parameters from container

--- src/Concerns/HandlesActions.php
+++ src/Concerns/HandlesActions.php
@@ -60,6 +60,17 @@ trait HandlesActions
         }
     }
 
+    protected function resolveActionParameters($method, $params)
+    {
+        return collect((new \ReflectionMethod($this, $method))->getParameters())->map(function ($parameter) use ($params) {
+            return rescue(function () use ($parameter) {
+                return app($parameter->getClass()->name);
+            }, function () use ($params) {
+                return array_shift($params);
+            }, false);
+        });
+    }
+
     protected function methodIsPublicAndNotDefinedOnBaseClass($methodName)
     {
         return collect((new \ReflectionClass($this))->getMethods(\ReflectionMethod::IS_PUBLIC))
