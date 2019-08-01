026f98a3efa286f1f9429c7dd4eaa29f5fddd02c
Francis Lavoie <lavofr@gmail.com>
2019-08-01 01:40:05 -0400

    Stream the livewire.js file instead
    As mentioned in my comment here https://github.com/calebporzio/livewire/pull/53#issuecomment-517119194, this should stream the content of the file instead of loading it into memory before sending it off. Also, it handles Last-Modified itself, so that code can be removed.

--- src/LivewireJavaScriptAssets.php
+++ src/LivewireJavaScriptAssets.php
@@ -7,15 +7,12 @@ class LivewireJavaScriptAssets
     public function __invoke()
     {
         $file = __DIR__ . '/../dist/livewire.js';
-        $lastModified = filemtime($file);
-        $contents = file_get_contents($file);
 
         // These headers will enable browsers to cache this asset.
-        return response($contents)
-            ->withHeaders([
+        return response()
+            ->file($file, [
                 'Content-Type' => 'application/javascript; charset=utf-8',
                 'Cache-Control' => 'public, max-age=3600',
-                'Last-Modified' => gmdate("D, d M Y H:i:s", $lastModified) . " GMT",
             ]);
     }
 }
