64c29f502b06b74d21174956f98fce0ddda3706b
Brian Faust <hello@basecode.sh>
2019-08-07 05:28:34 +0300

    test that default component is generated properly

--- tests/ComponentUsesCustomName.php
+++ tests/ComponentUsesCustomName.php
@@ -8,12 +8,31 @@ use Livewire\LivewireManager;
 class ComponentUsesCustomName extends TestCase
 {
     /** @test */
+    public function uses_default_component_name()
+    {
+        $component = app(LivewireManager::class)->test(UsesDefaultComponentName::class);
+
+        $this->assertEquals('Hello World', $component->instance->name);
+        $this->assertEquals('tests.uses-default-component-name', $component->instance->getName());
+    }
+
+    /** @test */
     public function preserves_name_property()
     {
         $component = app(LivewireManager::class)->test(PreservesNameProperty::class);
 
         $this->assertEquals('Hello World', $component->instance->name);
         $this->assertEquals('uses-custom-name', $component->instance->getName());
+    }
+}
+
+class UsesDefaultComponentName extends Component
+{
+    public $name = 'Hello World';
+
+    public function render()
+    {
+        return app('view')->make('null-view');
     }
 }
 
