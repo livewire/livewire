842ec70b2d38feb1f8eefc1a7554d82aecdfe58d
Roy Verrips <roy@verrips.org>
2019-08-07 22:05:40 -0500

    Add Polyfill for closestByAttribute(attribute) i.e. return new DOMElement(this.el.closest())

--- src/js/dom/polyfills.js
+++ src/js/dom/polyfills.js
@@ -176,3 +176,53 @@ if (!Array.prototype.find) {
   });
 }
 
+/**
+ * @see https://dom.spec.whatwg.org/#interface-element
+ * @see https://developer.mozilla.org/docs/Web/API/Element/matches#Polyfill
+ * @see https://gist.github.com/jonathantneal/3062955
+ * @see https://github.com/jonathantneal/closest
+ */
+(function(global){
+var Element;
+var ElementPrototype;
+var matches;
+if (Element = global.Element) {
+    ElementPrototype = Element.prototype;
+/**
+     * @see https://dom.spec.whatwg.org/#dom-element-matches
+     */
+if (!(matches = ElementPrototype.matches)) {
+if ((
+        matches = ElementPrototype.matchesSelector ||
+ElementPrototype.mozMatchesSelector ||
+ElementPrototype.msMatchesSelector ||
+ElementPrototype.oMatchesSelector ||
+ElementPrototype.webkitMatchesSelector ||
+          (ElementPrototype.querySelectorAll && function matches(selectors) {
+var element = this;
+var nodeList = (element.parentNode || element.document || element.ownerDocument).querySelectorAll(selectors);
+var index = nodeList.length;
+while (--index >= 0 && nodeList.item(index) !== element) {}
+return index > -1;
+          })
+      )) {
+ElementPrototype.matches = matches;
+      }
+    }
+/**
+     * @see https://dom.spec.whatwg.org/#dom-element-closest
+     */
+if (!ElementPrototype.closest && matches) {
+ElementPrototype.closest = function closest(selectors) {
+var element = this;
+while (element) {
+if (element.nodeType === 1 && element.matches(selectors)) {
+return element;
+          }
+          element = element.parentNode;
+        }
+return null;
+      };
+    }
+  }
+}(Function('return this')()));
