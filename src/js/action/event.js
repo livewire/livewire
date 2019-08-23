6f82975e89eb644b0ce7fa054dbdb75a7190d826
Erik Scheepers <erik@ngtech.nl>
2019-08-23 11:31:32 +0200

    Replaced "btoa(string)" by "btoa(unescape(encodeURIComponent(string)))" to fix issue where string to be encoded contains characters outside of Latin1 range

--- src/js/action/event.js
+++ src/js/action/event.js
@@ -11,8 +11,8 @@ export default class extends Action {
         }
     }
 
-    // Overriding toId() becuase some EventActions don't have an "el"
+    // Overriding toId() because some EventActions don't have an "el"
     toId() {
-        return btoa(this.type, this.payload.event, JSON.stringify(this.payload.params))
+        return btoa(unescape(encodeURIComponent(this.type, this.payload.event, JSON.stringify(this.payload.params))))
     }
 }
--- src/js/action/index.js
+++ src/js/action/index.js
@@ -9,6 +9,6 @@ export default class {
     }
 
     toId() {
-        return btoa(this.el.el.outerHTML)
+        return btoa(unescape(encodeURIComponent(this.el.el.outerHTML)))
     }
 }
