6713a0dd7d71fc96189c0145574ee917b9359d6f
David Piesse <piesse@gmail.com>
2019-08-28 11:52:00 +0100

    Create EchoManager file

---
+++ src/js/Component/EchoManager.js
@@ -0,0 +1,13 @@
+import store from '@/Store'
+
+class EchoManager {
+    constructor(component) {
+        this.component = component
+    }
+
+    registerListeners() {
+
+    }
+}
+
+export default EchoManager
