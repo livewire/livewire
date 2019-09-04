76d211165f80d56be29d1a22bed79fe943aa6b16
Till Krüss <till@kruss.io>
2019-09-04 10:06:16 -0700

    add ETag header

--- src/LivewireJavaScriptAssets.php
+++ src/LivewireJavaScriptAssets.php
@@ -19,8 +19,14 @@ class LivewireJavaScriptAssets
         $lastModified = filemtime($file);
         $expires = strtotime('+1 year');
 
+        $manifest = json_decode(file_get_contents(__DIR__.'/../dist/mix-manifest.json'), true);
+        $versionedFileName = $manifest['/' . basename($file)];
+
+        parse_str(parse_url($versionedFileName, PHP_URL_QUERY), $query);
+
         return response()->file($file, [
             'Content-Type' => 'application/javascript; charset=utf-8',
+            'ETag' => $query['id'],
             'Cache-Control' => 'public, max-age=31536000',
             'Expires' => sprintf('%s GMT', gmdate('D, d M Y H:i:s', $expires)),
             'Last-Modified' => sprintf('%s GMT', gmdate('D, d M Y H:i:s', $lastModified)),
