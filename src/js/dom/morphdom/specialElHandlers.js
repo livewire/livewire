4e825fcdc1d4849bfbf2ca537d1c3e5540a96e1f
Angus Allman <angus.allman@gmail.com>
2019-10-09 21:13:46 +0100

    change second parameter to empty quotes

--- src/js/dom/morphdom/specialElHandlers.js
+++ src/js/dom/morphdom/specialElHandlers.js
@@ -2,7 +2,7 @@ function syncBooleanAttrProp(fromEl, toEl, name) {
     if (fromEl[name] !== toEl[name]) {
         fromEl[name] = toEl[name];
         if (fromEl[name]) {
-            fromEl.setAttribute(name, name);
+            fromEl.setAttribute(name, '');
         } else {
             fromEl.removeAttribute(name);
         }
