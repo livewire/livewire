649bcd2a02d3751f22df443398aafb083dbcafc0
Kevin <kevin@intellow.com>
2019-11-08 17:37:02 -0600

    stubs are generated

--- src/Commands/StubCommand.php
+++ src/Commands/StubCommand.php
@@ -4,6 +4,7 @@ namespace Livewire\Commands;
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class StubCommand extends Command
 {
@@ -13,13 +14,30 @@ class StubCommand extends Command
 
     public function handle()
     {
-        $this->info('you ran the command');
+        $input = preg_split('/[.]+/', $this->argument('name'));
+        $component = Str::kebab(array_pop($input));
+        $componentClass = Str::studly($component);
+        $this->ensureDirectoryExists('app/Http/Livewire/Stubs');
+        $this->ensureDirectoryExists('resources/views/livewire/stubs');
+        $this->createViewStub($component);
+        $this->createClassStub($componentClass);
+        $this->info('you ran the command with name: ' . $this->argument('name'));
     }
 
     protected function ensureDirectoryExists($path)
     {
-        if (! File::isDirectory(dirname($path))) {
-            File::makeDirectory(dirname($path), 0777, $recursive = true, $force = true);
+        if (! File::isDirectory($path)) {
+            File::makeDirectory($path, 0777, $recursive = true, $force = true);
         }
+    }
+
+    protected function createClassStub($name)
+    {
+        File::put('app/Http/Livewire/Stubs/'.$name.'.stub', file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'Component.stub'));
+    }
+
+    protected function createViewStub($name)
+    {
+        File::put('resources/views/livewire/stubs/'.$name.'.stub', file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'view.stub'));
     }
 }
