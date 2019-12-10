28e41a45a3c45326b2b5494196333b80af9f7fcc
Caleb Porzio <calebporzio@gmail.com>
2019-12-10 09:36:46 -0500

    Add trait for passing public properties into the view automatically

--- tests/PublicPropertiesAreAvailableInTheViewTest.php
+++ tests/PublicPropertiesAreAvailableInTheViewTest.php
@@ -2,16 +2,42 @@
 
 namespace Tests;
 
+use ErrorException;
 use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\LivewireManager;
+use Livewire\PassPublicPropertiesToView;
 
 class PublicPropertiesAreAvailableInTheViewTest extends TestCase
 {
     /** @test */
-    public function public_property_is_accessible_in_view()
+    public function public_property_is_accessible_in_view_via_this()
     {
         $component = app(LivewireManager::class)->test(PublicPropertiesInViewStub::class);
+
+        $this->assertTrue(Str::contains(
+            $component->dom,
+            'Caleb'
+        ));
+    }
+
+    /** @test */
+    public function public_property_is_accessible_in_view_without_this_by_default()
+    {
+        $this->expectException(ErrorException::class);
+
+        $component = app(LivewireManager::class)->test(PublicPropertiesInViewWithoutThisAndWithoutTraitStub::class);
+
+        $this->assertTrue(Str::contains(
+            $component->dom,
+            'Caleb'
+        ));
+    }
+
+    /** @test */
+    public function public_property_is_accessible_in_view()
+    {
+        $component = app(LivewireManager::class)->test(PublicPropertiesInViewWithoutThisStub::class);
 
         $this->assertTrue(Str::contains(
             $component->dom,
@@ -22,6 +48,28 @@ class PublicPropertiesAreAvailableInTheViewTest extends TestCase
 
 class PublicPropertiesInViewStub extends Component
 {
+    public $name = 'Caleb';
+
+    public function render()
+    {
+        return app('view')->make('show-name-with-this');
+    }
+}
+
+class PublicPropertiesInViewWithoutThisAndWithoutTraitStub extends Component
+{
+    public $name = 'Caleb';
+
+    public function render()
+    {
+        return app('view')->make('show-name');
+    }
+}
+
+class PublicPropertiesInViewWithoutThisStub extends Component
+{
+    use PassPublicPropertiesToView;
+
     public $name = 'Caleb';
 
     public function render()
