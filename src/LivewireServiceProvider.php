856e5a784aa5bc812c4b090508af9b107888682a
Lucas Romano <lucasromanojf@gmail.com>
2019-12-17 17:56:50 -0300

    Fixing Vapor check

--- src/LivewireServiceProvider.php
+++ src/LivewireServiceProvider.php
@@ -50,7 +50,7 @@ class LivewireServiceProvider extends ServiceProvider
         $this->app->singleton('livewire', LivewireManager::class);
 
         $this->app->singleton(LivewireComponentsFinder::class, function () {
-            $isHostedOnVapor = $_ENV['SERVER_SOFTWARE'] === 'vapor';
+            $isHostedOnVapor = $_ENV['SERVER_SOFTWARE'] ?? null === 'vapor';
             return new LivewireComponentsFinder(
                 new Filesystem,
                 config('livewire.manifest_path') ?? (
