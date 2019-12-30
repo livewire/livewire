e031289d3e6eeb0dc1af64477c8f2bdc8053260e
Brian Faust <hello@basecode.sh>
2019-12-30 08:15:12 +0200

    implement assertRedirect assertion

---
+++ tests/TestableLivewireCanAssertRedirectTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests;
+
+use Livewire\Component;
+use Livewire\LivewireManager;
+
+class TestableLivewireCanAssertRedirectTest extends TestCase
+{
+    /** @test */
+    public function can_assert_a_redirect_without_a_uri()
+    {
+        $component = app(LivewireManager::class)->test(RedirectComponent::class);
+
+        $component->performRedirect();
+
+        $component->assertRedirect();
+    }
+
+    /** @test */
+    public function can_assert_a_redirect_with_a_uri()
+    {
+        $component = app(LivewireManager::class)->test(RedirectComponent::class);
+
+        $component->performRedirect();
+
+        $component->assertRedirect('/some');
+    }
+}
+
+class RedirectComponent extends Component
+{
+    public function performRedirect()
+    {
+        $this->redirect('/some');
+    }
+
+    public function render()
+    {
+        return view('null-view');
+    }
+}
