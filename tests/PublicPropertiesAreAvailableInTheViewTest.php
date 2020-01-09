5369618d59dda9cc6cb92e1145c2849fa3fe9d20
Caleb Porzio <calebporzio@gmail.com>
2020-01-09 15:08:08 -0500

    Cleanup

--- tests/PublicPropertiesAreAvailableInTheViewTest.php
+++ tests/PublicPropertiesAreAvailableInTheViewTest.php
@@ -2,30 +2,23 @@
 
 namespace Tests;
 
-use ErrorException;
-use Illuminate\Support\Str;
 use Livewire\Component;
-use Livewire\LivewireManager;
-use Livewire\PassPublicPropertiesToView;
+use Livewire\Livewire;
 
 class PublicPropertiesAreAvailableInTheViewTest extends TestCase
 {
     /** @test */
     public function public_property_is_accessible_in_view_via_this()
     {
-        $component = app(LivewireManager::class)->test(PublicPropertiesInViewWithThisStub::class);
-
-        $this->assertTrue(Str::contains(
-            $component->payload['dom'],
-            'Caleb'
-        ));
+        Livewire::test(PublicPropertiesInViewWithThisStub::class)
+            ->assertSee('Caleb');
     }
 
     /** @test */
     public function public_properties_are_accessible_in_view_without_this()
     {
-        $component = app(LivewireManager::class)->test(PublicPropertiesInViewWithoutThisStub::class);
-        $component->assertSee('Caleb');
+        Livewire::test(PublicPropertiesInViewWithoutThisStub::class)
+            ->assertSee('Caleb');
     }
 }
 
