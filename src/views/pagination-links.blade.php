7b7a4d64c00448bcb210a66a8a1b1ce795ebc9d4
Kevin Dion <54263485+kdion4891@users.noreply.github.com>
2020-01-28 23:36:56 -0500

    make pagination responsive

--- src/views/pagination-links.blade.php
+++ src/views/pagination-links.blade.php
@@ -3,11 +3,17 @@
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
             <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                <span class="page-link" aria-hidden="true">
+                    <span class="d-none d-md-block">&lsaquo;</span>
+                    <span class="d-block d-md-none">@lang('pagination.previous')</span>
+                </span>
             </li>
         @else
             <li class="page-item">
-                <button type="button" class="page-link" wire:click="previousPage" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</button>
+                <button type="button" class="page-link" wire:click="previousPage" rel="prev" aria-label="@lang('pagination.previous')">
+                    <span class="d-none d-md-block">&lsaquo;</span>
+                    <span class="d-block d-md-none">@lang('pagination.previous')</span>
+                </button>
             </li>
         @endif
 
@@ -15,16 +21,16 @@
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                <li class="page-item disabled d-none d-md-block" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                        <li class="page-item active d-none d-md-block" aria-current="page"><span class="page-link">{{ $page }}</span></li>
                     @else
-                        <li class="page-item"><button type="button" class="page-link" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
+                        <li class="page-item d-none d-md-block"><button type="button" class="page-link" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
                     @endif
                 @endforeach
             @endif
@@ -33,11 +39,17 @@
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <li class="page-item">
-                <button type="button" class="page-link" wire:click="nextPage" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</button>
+                <button type="button" class="page-link" wire:click="nextPage" rel="next" aria-label="@lang('pagination.next')">
+                    <span class="d-block d-md-none">@lang('pagination.next')</span>
+                    <span class="d-none d-md-block">&rsaquo;</span>
+                </button>
             </li>
         @else
             <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                <span class="page-link" aria-hidden="true">
+                    <span class="d-block d-md-none">@lang('pagination.next')</span>
+                    <span class="d-none d-md-block">&rsaquo;</span>
+                </span>
             </li>
         @endif
     </ul>
