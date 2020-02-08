aa9609d98c8d4e004d056f08477217721674d5b4
Ruud Schuurmans <ruud.schuurmans@schuurmans-it.nl>
2020-02-08 16:40:25 +0100

    rebuild js assets

--- babel.config.js
+++ babel.config.js
@@ -5,9 +5,11 @@ module.exports = {
             {
                 targets: {
                     node: 'current',
-                    edge: '18'
+                    edge: '18',
+                    ie: "11"
                 },
             },
         ],
     ],
+    plugins: ["@babel/plugin-proposal-object-rest-spread"]
 };
