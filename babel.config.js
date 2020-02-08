e0ee7cb595a516f1b86be721ea8b0160c24aa28f
Ruud Schuurmans <ruud.schuurmans@schuurmans-it.nl>
2020-02-08 16:33:41 +0100

    Add babel plugin-transform-runtime to fix tests

--- babel.config.js
+++ babel.config.js
@@ -10,5 +10,8 @@ module.exports = {
             },
         ],
     ],
-    plugins: ["@babel/plugin-proposal-object-rest-spread"]
+    plugins: [
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-transform-runtime"
+    ]
 };
