92007824a60923fdeb2d35e5c6f979f9287abbce
Caleb Porzio <calebporzio@gmail.com>
2020-02-10 12:05:01 -0500

    Cleanup dep-resolver trait

--- src/DependencyResolverTrait.php
+++ src/DependencyResolverTrait.php
@@ -2,11 +2,10 @@
 
 namespace Livewire;
 
-use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Support\Arr;
-use ReflectionFunctionAbstract;
 use ReflectionMethod;
 use ReflectionParameter;
+use ReflectionFunctionAbstract;
+use Illuminate\Contracts\Container\BindingResolutionException;
 
 trait DependencyResolverTrait
 {
@@ -80,56 +79,5 @@ trait DependencyResolverTrait
 
             throw $e;
         }
-    }
-
-    public function hey($parameters, $reflector) {
-        $instanceCount = 0;
-
-        $values = array_values($parameters);
-
-        foreach ($reflector->getParameters() as $key => $parameter) {
-            $instance = $this->transformDependency(
-                $parameter, $parameters
-            );
-
-            if (! is_null($instance)) {
-                $instanceCount++;
-
-                $this->spliceIntoParameters($parameters, $key, $instance);
-            } elseif (! array_key_exists($key - $instanceCount, $values) &&
-                      $parameter->isDefaultValueAvailable()) {
-                $this->spliceIntoParameters($parameters, $key, $parameter->getDefaultValue());
-            }
-        }
-
-        return $parameters;
-    }
-
-    protected function transformDependency(ReflectionParameter $parameter, $parameters)
-    {
-        $class = $parameter->getClass();
-
-        // If the parameter has a type-hinted class, we will check to see if it is already in
-        // the list of parameters. If it is we will just skip it as it is probably a model
-        // binding and we do not want to mess with those; otherwise, we resolve it here.
-        if ($class && ! $this->alreadyInParameters($class->name, $parameters)) {
-            return $parameter->isDefaultValueAvailable()
-                ? $parameter->getDefaultValue()
-                : $this->container->make($class->name);
-        }
-    }
-
-    protected function alreadyInParameters($class, array $parameters)
-    {
-        return ! is_null(Arr::first($parameters, function ($value) use ($class) {
-            return $value instanceof $class;
-        }));
-    }
-
-    protected function spliceIntoParameters(array &$parameters, $offset, $value)
-    {
-        array_splice(
-            $parameters, $offset, 0, [$value]
-        );
     }
 }
