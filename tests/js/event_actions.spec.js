8d70bbfe28c0852f73937a995a587f2f4bfb04f9
Aryeh Raber <aryeh.raber@gmail.com>
2020-03-03 15:36:11 +0100

    Tweak time before assertion

--- tests/js/event_actions.spec.js
+++ tests/js/event_actions.spec.js
@@ -151,7 +151,7 @@ test('polling on minute', async () => {
 
         mountAsRoot('<div wire:id="123" wire:initial-data="{}" wire:poll.on-minute></div>', () => { pollHappened = true })
 
-        jest.advanceTimersByTime(msUntilMinute)
+        jest.advanceTimersByTime(msUntilMinute - 500)
 
         expect(pollHappened).toBeFalsy()
 
