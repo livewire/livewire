2d41110b4d83dde3390087234b8b070c95d516a8
Aryeh Raber <aryeh.raber@gmail.com>
2020-03-03 12:04:24 +0100

    Add 'on-minute' polling modifier

--- js/component/Polling.js
+++ js/component/Polling.js
@@ -1,21 +1,29 @@
 import MethodAction from '@/action/method'
 import store from '@/Store'
 
+let defaultDuration = 2000
+
 export default function () {
     store.registerHook('elementInitialized', (el, component) => {
         if (el.directives.missing('poll')) return
 
-        fireActionOnInterval(el, component)
+        const directive = el.directives.get('poll')
+        const method = directive.method || '$refresh'
+
+        if (directive.modifiers.includes('on-minute')) {
+          defaultDuration = 60000
+
+          fireActionOnMinute(el, component, directive, method)
+        } else {
+          fireActionOnInterval(el, component, directive, method)
+        }
     })
 }
 
-function fireActionOnInterval(el, component) {
-    const directive = el.directives.get('poll')
-    const method = directive.method || '$refresh'
-
+function fireActionOnInterval(el, component, directive, method) {
     setInterval(() => {
         // Don't poll when the tab is in the background.
-        // The "Math.random" business effectivlly prevents 95% of requests
+        // The "Math.random" business effectively prevents 95% of requests
         // from executing. We still want "some" requests to get through.
         if (store.livewireIsInBackground && Math.random() < .95) return
 
@@ -23,5 +31,15 @@ function fireActionOnInterval(el, component) {
         if (store.livewireIsOffline) return
 
         component.addAction(new MethodAction(method, directive.params, el))
-    }, directive.durationOr(2000));
+    }, directive.durationOr(defaultDuration))
+}
+
+function fireActionOnMinute(el, component, directive, method) {
+    const msUntilMinute = (60 - (new Date).getSeconds()) * 1000
+
+    setTimeout(() => {
+        component.addAction(new MethodAction(method, directive.params, el))
+
+        fireActionOnInterval(el, component, directive, method)
+    }, msUntilMinute)
 }
