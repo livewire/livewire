2dd1f60e7c7b49a48bb434c2991d8b5018c04523
Tim Chandler <tim@code-distortion.net>
2020-03-15 11:01:31 +1100

    Allow an empty literal string value (using single quotes) to be passed to a component when using tag-syntax

--- src/LivewireTagCompiler.php
+++ src/LivewireTagCompiler.php
@@ -28,7 +28,7 @@ class LivewireTagCompiler extends ComponentTagCompiler
                             (?:
                                 \\\"[^\\\"]*\\\"
                                 |
-                                \'[^\']+\'
+                                \'[^\']*\'
                                 |
                                 [^\'\\\"=<>]+
                             )
