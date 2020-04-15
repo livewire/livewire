07f80be575bac5ce471f6bea0f906d3eb7fc1098
Jorge González <iksaku@me.com>
2020-04-15 14:16:53 -0500

    Added back typed property tests. Classes are evaluated from string, otherwise, PHP Parser will complain about typed properties.

--- tests/PublicPropertiesAreInitializedTest.php
+++ tests/PublicPropertiesAreInitializedTest.php
@@ -20,6 +20,58 @@ class PublicPropertiesAreInitializedTest extends TestCase
         Livewire::test(InitializedPublicPropertyComponent::class)
             ->assertSee('Non-typed Properties are boring');
     }
+
+    /** @test */
+    public function uninitialized_public_typed_property_is_null()
+    {
+        if (version_compare(PHP_VERSION, '7.4', '<')) {
+            $this->markTestSkipped('Typed Property Initialization not supported prior to PHP 7.4');
+        }
+
+        $class = <<<'PHP'
+namespace Tests;
+use Livewire\Component;
+class UninitializedPublicTypedPropertyComponent extends Component
+{
+    public string $message;
+
+    public function render()
+    {
+        return app('view')->make('null-view');
+    }
+}
+PHP;
+        eval($class);
+
+        Livewire::test(UninitializedPublicTypedPropertyComponent::class)
+            ->assertSet('message', null);
+    }
+
+    /** @test */
+    public function initialized_public_typed_property_shows_value()
+    {
+        if (version_compare(PHP_VERSION, '7.4', '<')) {
+            $this->markTestSkipped('Typed Property Initialization not supported prior to PHP 7.4');
+        }
+
+        $class = <<<'PHP'
+namespace Tests;
+use Livewire\Component;
+class InitializedPublicTypedPropertyComponent extends Component
+{
+    public string $message = 'Typed Properties FTW!';
+
+    public function render()
+    {
+        return app('view')->make('show-property-value');
+    }
+}
+PHP;
+        eval($class);
+
+        Livewire::test(InitializedPublicTypedPropertyComponent::class)
+            ->assertSee('Typed Properties FTW!');
+    }
 }
 
 class UninitializedPublicPropertyComponent extends Component
