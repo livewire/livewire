6f50c60bb1fad0a1750a5f1be320eabb44e850ba
Dan Harrin <git@DanHarrin.com>
2020-04-22 08:14:39 +0100

    Revert "Refactor to avoid code duplication"
    This reverts commit a0ccf414697168dbd497e4e5909459c145d13c0c.

--- src/CompilerEngine.php
+++ src/CompilerEngine.php
@@ -12,18 +12,14 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
-class CompilerEngine extends LaravelCompilerEngine
-{
-    // Errors thrown while a view is rendering are caught by the Blade
-    // compiler and wrapped in an "ErrorException". This makes Livewire errors
-    // harder to read, AND causes issues like `abort(404)` not actually working.
-    protected function handleViewException(Throwable $e, $obLevel)
+if (Application::VERSION === '7.x-dev' || version_compare(Application::VERSION, '7.0', '>=')) {
+    class CompilerEngine extends LaravelCompilerEngine
     {
-        if (
-            $e instanceof Exception
-            || Application::VERSION === '7.x-dev'
-            || version_compare(Application::VERSION, '7.0', '>=')
-        ) {
+        // Errors thrown while a view is rendering are caught by the Blade
+        // compiler and wrapped in an "ErrorException". This makes Livewire errors
+        // harder to read, AND causes issues like `abort(404)` not actually working.
+        protected function handleViewException(Throwable $e, $obLevel)
+        {
             $uses = array_flip(class_uses_recursive($e));
 
             if (
@@ -43,8 +39,39 @@ class CompilerEngine extends LaravelCompilerEngine
             }
 
             parent::handleViewException($e, $obLevel);
-        } else {
-            throw($e);
+        }
+    }
+} else {
+    class CompilerEngine extends LaravelCompilerEngine
+    {
+        // Errors thrown while a view is rendering are caught by the Blade
+        // compiler and wrapped in an "ErrorException". This makes Livewire errors
+        // harder to read, AND causes issues like `abort(404)` not actually working.
+        protected function handleViewException(Exception $e, $obLevel)
+        {
+            if ($e instanceof Exception){
+                $uses = array_flip(class_uses_recursive($e));
+
+                if (
+                    // Don't wrap "abort(403)".
+                    $e instanceof AuthorizationException
+                    // Don't wrap "abort(404)".
+                    || $e instanceof NotFoundHttpException
+                    // Don't wrap "abort(500)".
+                    || $e instanceof HttpException
+                    // Don't wrap most Livewire exceptions.
+                    || isset($uses[BypassViewHandler::class])
+                ) {
+                    // This is because there is no "parent::parent::".
+                    PhpEngine::handleViewException($e, $obLevel);
+
+                    return;
+                }
+
+                parent::handleViewException($e, $obLevel);
+            } else {
+                throw($e);
+            }
         }
     }
 }
