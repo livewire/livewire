b03208e9b46fce2d67a1bd9e5f7aec8d2644a210
Dan Harrin <git@DanHarrin.com>
2020-05-09 16:51:26 +0100

    Test

--- tests/ResetPropertiesTest.php
+++ tests/ResetPropertiesTest.php
@@ -13,20 +13,37 @@ class ResetPropertiesTest extends TestCase
         Livewire::test(ResetPropertiesComponent::class)
             ->assertSet('foo', 'bar')
             ->assertSet('bob', 'lob')
+            ->assertSet('mwa', 'hah')
             ->set('foo', 'baz')
             ->set('bob', 'law')
+            ->set('mwa', 'aha')
             ->assertSet('foo', 'baz')
             ->assertSet('bob', 'law')
+            ->assertSet('mwa', 'aha')
             // Reset all.
             ->call('resetAll')
             ->assertSet('foo', 'bar')
             ->assertSet('bob', 'lob')
+            ->assertSet('mwa', 'hah')
             ->set('foo', 'baz')
             ->set('bob', 'law')
+            ->set('mwa', 'aha')
             ->assertSet('foo', 'baz')
             ->assertSet('bob', 'law')
+            ->assertSet('mwa', 'aha')
+            // Reset foo and bob.
+            ->call('resetKeys', ['foo', 'bob'])
+            ->assertSet('foo', 'bar')
+            ->assertSet('bob', 'lob')
+            ->assertSet('mwa', 'aha')
+            ->set('foo', 'baz')
+            ->set('bob', 'law')
+            ->set('mwa', 'aha')
+            ->assertSet('foo', 'baz')
+            ->assertSet('bob', 'law')
+            ->assertSet('mwa', 'aha')
             // Reset only foo.
-            ->call('resetKey', 'foo')
+            ->call('resetKeys', 'foo')
             ->assertSet('foo', 'bar')
             ->assertSet('bob', 'law');
     }
@@ -36,13 +53,14 @@ class ResetPropertiesComponent extends Component
 {
     public $foo = 'bar';
     public $bob = 'lob';
+    public $mwa = 'hah';
 
     public function resetAll()
     {
         $this->reset();
     }
 
-    public function resetKey($keys)
+    public function resetKeys($keys)
     {
         $this->reset($keys);
     }
