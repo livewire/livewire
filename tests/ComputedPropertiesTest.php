482e4677f0c8a691293bbb3878731b9d74124fa6
Caleb Porzio <calebporzio@gmail.com>
2020-05-11 14:33:26 -0400

    wip

--- tests/ComputedPropertiesTest.php
+++ tests/ComputedPropertiesTest.php
@@ -15,10 +15,27 @@ class ComputedPropertiesTest extends TestCase
     }
 
     /** @test */
-    public function compute_property_is_memoized_after_its_accessed()
+    public function computed_property_is_memoized_after_its_accessed()
     {
         Livewire::test(MemoizedComputedPropertyStub::class)
             ->assertSee('int(2)');
+    }
+
+    /** @test */
+    public function computed_property_cache_can_be_cleared()
+    {
+        Livewire::test(MemoizedComputedPropertyStub::class)
+            ->assertSee('int(2)')
+            ->call('callForgetComputed')
+            ->assertSee('int(4)')
+            ->call('callForgetComputed', 'foo')
+            ->assertSee('int(6)')
+            ->call('callForgetComputed', 'bar')
+            ->assertSee('int(7)')
+            ->call('callForgetComputed', ['foo', 'bar'])
+            ->assertSee('int(9)')
+            ->call('callForgetComputed', 'bar', 'foo')
+            ->assertSee('int(11)');
     }
 }
 
@@ -44,6 +61,33 @@ class MemoizedComputedPropertyStub extends Component
     public function getFooProperty()
     {
         return $this->count += 1;
+    }
+
+    public function callForgetComputed(...$args)
+    {
+        $this->foo;
+
+        $this->forgetComputed(...$args);
+
+        $this->foo;
+    }
+
+    public function callForgetComputedArray($args)
+    {
+        $this->foo;
+
+        $this->forgetComputed($args);
+
+        $this->foo;
+    }
+
+    public function callForgetComputedSpread(...$args)
+    {
+        $this->foo;
+
+        $this->forgetComputed(...$args);
+
+        $this->foo;
     }
 
     public function render()
