e2b05ee3667298425f42bc94c8babfa4fe80eed9
Rasim ÖZDİNÇ <rsmxtr@gmail.com>
2020-05-29 23:15:56 +0300

    Create ResetExceptPropertiesTest.php

---
+++ tests/ResetExceptPropertiesTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests;
+
+use Livewire\Component;
+use Livewire\Livewire;
+
+class ResetExceptPropertiesTest extends TestCase
+{
+    /** @test */
+    public function can_reset_except_properties()
+    {
+        Livewire::test(ResetPropertiesComponent::class)
+            ->assertSet('foo', 'bar')
+            ->assertSet('bob', 'lob')
+            ->assertSet('john', 'doe')
+            ->set('foo', 'baz')
+            ->set('bob', 'law')
+            ->set('john', 'poe')
+            ->assertSet('foo', 'baz')
+            ->assertSet('bob', 'law')
+            ->assertSet('john', 'poe')
+            // Reset except foo and bob.
+            ->call('resetExceptFooAndBob')
+            ->assertSet('foo', 'baz')
+            ->assertSet('bob', 'law')
+            ->assertSet('john', 'doe');            ;
+    }
+}
+
+class ResetPropertiesComponent extends Component
+{
+    public $foo  = 'bar';
+    public $bob  = 'lob';
+    public $john = 'doe';
+
+    public function resetExceptFooAndBob()
+    {
+        $this->resetExcept(['foo', 'bob']);
+    }
+
+    public function render()
+    {
+        return view('null-view');
+    }
+}
