8c9301a33f40b618bc352987840fe364c14c004b
Matt Harcourt <matt.s.harcourt@gmail.com>
2020-06-06 07:38:30 -0700

    Add test to catch issue

--- tests/RouteRegistrationTest.php
+++ tests/RouteRegistrationTest.php
@@ -2,12 +2,50 @@
 
 namespace Tests;
 
-use Livewire\Livewire;
-use Livewire\Component;
+use Illuminate\Routing\RouteCollection;
 use Illuminate\Support\Facades\Route;
+use Livewire\Component;
+use Livewire\Livewire;
 
 class RouteRegistrationTest extends TestCase
 {
+    /** @test */
+    public function can_register_route_with_component_mount_as_the_route_action()
+    {
+        Livewire::component('foo', ComponentForRouteRegistration::class);
+
+        Route::livewire('/foo', 'foo');
+
+        $this->get('/foo')->assertSee('bar');
+    }
+
+    /** @test */
+    public function can_register_mount_as_route_action_without_side_effects()
+    {
+        Livewire::component('foo', ComponentForRouteRegistration::class);
+
+        Route::livewire('/foo', 'foo');
+
+        // request the route to trigger the registered callback
+        $this->get('/foo');
+
+        try {
+            // check for side effects - copy routes to new collection
+            // as Livewire::test() does to insert test route
+            $routes = new RouteCollection;
+            foreach (app('router')->getRoutes() as $route) {
+                $routes->add($route);
+            }
+        } catch (\Exception $e) {
+            $this->fail(sprintf(
+                "Failed to register Livewire route without side effect ['%s']",
+                $e->getMessage()
+            ));
+        }
+
+        $this->assertTrue(true);
+    }
+
     /** @test */
     public function can_pass_parameters_to_a_layout_file()
     {
@@ -23,7 +61,12 @@ class RouteRegistrationTest extends TestCase
 
 class ComponentForRouteRegistration extends Component
 {
-    public $name = 'bar';
+    public $name = '';
+
+    public function mount()
+    {
+        $this->name = 'bar';
+    }
 
     public function render()
     {
