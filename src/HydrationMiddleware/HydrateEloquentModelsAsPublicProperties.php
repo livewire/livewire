d9695bdb51294f547356ebaf1982d4f30384d2b9
jonneroelofs <jonne.roelofs@it4web.nl>
2020-06-20 13:57:41 +0200

    try fix

--- src/HydrationMiddleware/HydrateEloquentModelsAsPublicProperties.php
+++ src/HydrationMiddleware/HydrateEloquentModelsAsPublicProperties.php
@@ -3,6 +3,7 @@
 namespace Livewire\HydrationMiddleware;
 
 use Illuminate\Contracts\Database\ModelIdentifier;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Queue\SerializesAndRestoresModelIdentifiers;
 
 class HydrateEloquentModelsAsPublicProperties implements HydrationMiddleware
@@ -20,6 +21,18 @@ class HydrateEloquentModelsAsPublicProperties implements HydrationMiddleware
                 $unHydratedInstance->$property = (new static)->getRestoredPropertyValue(
                     new ModelIdentifier($value['class'], $value['id'], $value['relations'], $value['connection'])
                 );
+
+                if($unHydratedInstance -> $property instanceof Collection){
+                    $collection = $unHydratedInstance -> $property ->keyBy->getKey();
+
+                    $collectionClass = get_class($collection);
+
+                    $unHydratedInstance -> $property =  new $collectionClass(
+                        collect($value['id'])->map(function ($id) use ($collection) {
+                            return $collection[$id] ?? null;
+                        })->filter()
+                    );
+                }
             }
         }
     }
