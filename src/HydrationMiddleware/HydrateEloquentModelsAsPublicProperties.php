f7cce6358aa43a7df67a7ce10152af5d0a019838
Caleb Porzio <calebporzio@gmail.com>
2020-06-22 11:22:37 -0400

    Pull up entire restoresCollection method instead of patching in a part of it

--- src/HydrationMiddleware/HydrateEloquentModelsAsPublicProperties.php
+++ src/HydrationMiddleware/HydrateEloquentModelsAsPublicProperties.php
@@ -2,9 +2,11 @@
 
 namespace Livewire\HydrationMiddleware;
 
+use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Contracts\Database\ModelIdentifier;
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Queue\SerializesAndRestoresModelIdentifiers;
+use Illuminate\Database\Eloquent\Relations\Concerns\AsPivot;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 class HydrateEloquentModelsAsPublicProperties implements HydrationMiddleware
 {
@@ -21,18 +23,6 @@ class HydrateEloquentModelsAsPublicProperties implements HydrationMiddleware
                 $unHydratedInstance->$property = (new static)->getRestoredPropertyValue(
                     new ModelIdentifier($value['class'], $value['id'], $value['relations'], $value['connection'])
                 );
-
-                // only needed in Laravel 5.6 and 5.7
-                if($unHydratedInstance -> $property instanceof EloquentCollection){
-                    $collection = $unHydratedInstance -> $property ->keyBy->getKey();
-                    $collectionClass = get_class($collection);
-
-                    $unHydratedInstance -> $property =  new $collectionClass(
-                        collect($value['id'])->map(function ($id) use ($collection) {
-                            return $collection[$id] ?? null;
-                        })->filter()
-                    );
-                }
             }
         }
     }
@@ -46,5 +36,37 @@ class HydrateEloquentModelsAsPublicProperties implements HydrationMiddleware
                 $instance->$property = (array) $serializedModel;
             }
         }
+    }
+
+    /**
+     * This method is here to overide the method includeded in the "SerializesAndRestoresModelIdentifiers" trait.
+     * This method contains a fix that exists in Laravel 5.8 and greater and brings it to Laravel 5.6-7
+     * because this version of Livewire supports down to those.
+     * https://github.com/laravel/framework/blob/7.x/src/Illuminate/Queue/SerializesAndRestoresModelIdentifiers.php
+     */
+    protected function restoreCollection($value)
+    {
+        if (! $value->class || count($value->id) === 0) {
+            return new EloquentCollection;
+        }
+
+        $collection = $this->getQueryForModelRestoration(
+            (new $value->class)->setConnection($value->connection), $value->id
+        )->useWritePdo()->get();
+
+        if (is_a($value->class, Pivot::class, true) ||
+            in_array(AsPivot::class, class_uses($value->class))) {
+            return $collection;
+        }
+
+        $collection = $collection->keyBy->getKey();
+
+        $collectionClass = get_class($collection);
+
+        return new $collectionClass(
+            collect($value->id)->map(function ($id) use ($collection) {
+                return $collection[$id] ?? null;
+            })->filter()
+        );
     }
 }
