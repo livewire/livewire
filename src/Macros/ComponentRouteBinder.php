b4f84b3704a41c3d31932f0278dce52aff407d26
Matt Harcourt <matt.s.harcourt@gmail.com>
2020-06-24 16:16:58 -0700

    Fix issue with parameter snake/camel case

--- src/Macros/ComponentRouteBinder.php
+++ src/Macros/ComponentRouteBinder.php
@@ -2,9 +2,12 @@
 
 namespace Livewire\Macros;
 
+use Illuminate\Support\Str;
+use ReflectionClass;
+
 class ComponentRouteBinder
 {
-    protected $component;
+    protected $componentReflector;
     protected $router;
 
     public function __construct($component, $router)
@@ -13,11 +16,10 @@ class ComponentRouteBinder
             $component = app('livewire')->getComponentClass($component);
         }
 
-        if (! $component instanceof \ReflectionClass) {
-            $component = new \ReflectionClass($component);
-        }
+        $this->componentReflector = $component instanceof ReflectionClass
+            ? $component
+            : new ReflectionClass($component);
 
-        $this->component = $component;
         $this->router = $router;
     }
 
@@ -32,17 +34,53 @@ class ComponentRouteBinder
         $route = clone ($route ?: $this->router->getCurrentRoute());
         $request = $request ?: $this->router->getCurrentRequest();
 
-        if ($this->component->hasMethod($method)) {
+        if ($this->componentReflector->hasMethod($method)) {
             // bind the method parameters to the request as if it's the route action
-            $route->uses($this->component->getName().'@'.$method)->bind($request);
+            $route->uses($this->componentReflector->getName().'@'.$method)->bind($request);
+
+            $this->normalizeParameterNames($route, $method);
 
             // perform registered and implicit route bindings
             $this->router->substituteBindings($route);
             $this->router->substituteImplicitBindings($route);
 
+            $this->substituteNormalizedParameterNames($route, $method);
+
             return $route->parameters();
         }
 
         return [];
+    }
+
+    protected function normalizeParameterNames($route, $method)
+    {
+        // Ensure router's substitution has snake case version of parameter to fall back on
+
+        foreach ($this->componentReflector->getMethod($method)->getParameters() as $parameter) {
+            if ($route->hasParameter($parameterName = $parameter->getName())) {
+                continue;
+            }
+
+            if ($route->hasParameter($camelName = Str::camel($parameterName))) {
+                $route->setParameter($parameterName, $route->parameter($camelName));
+                $route->forgetParameter($camelName);
+            }
+        }
+    }
+
+    protected function substituteNormalizedParameterNames($route, $method)
+    {
+        // Handle cases where method parameter is not snake case
+
+        foreach ($this->componentReflector->getMethod($method)->getParameters() as $parameter) {
+            if ($route->hasParameter($parameterName = $parameter->getName())) {
+                continue;
+            }
+
+            if ($route->hasParameter($snakeName = Str::snake($parameterName))) {
+                $route->setParameter($parameterName, $route->parameter($snakeName));
+                $route->forgetParameter($snakeName);
+            }
+        }
     }
 }
