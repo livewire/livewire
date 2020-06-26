fa24a0a91afbfd748a0d28af1f67050c25f74809
Caleb Porzio <calebporzio@gmail.com>
2020-06-26 10:11:06 -0400

    Re-enable disabled form inputs on message failure

--- js/component/DisableForms.js
+++ js/component/DisableForms.js
@@ -1,13 +1,17 @@
 import store from '@/Store'
 
 export default function () {
+    let cleanupStackByComponentId = {}
+
     store.registerHook('elementInitialized', (el, component) => {
         if (el.directives.missing('submit')) return
 
         // Set a forms "disabled" state on inputs and buttons.
         // Livewire will clean it all up automatically when the form
         // submission returns and the new DOM lacks these additions.
-        el.el.addEventListener('submit', ()=> {
+        el.el.addEventListener('submit', () => {
+            cleanupStackByComponentId[component.id] = []
+
             component.walk(elem => {
                 const node = elem.el
 
@@ -21,6 +25,8 @@ export default function () {
                     // <input type="checkbox|radio">
                     || (node.tagName.toLowerCase() === 'input' && (node.type === 'checkbox' || node.type === 'radio'))
                 ) {
+                    if (! node.disabled) cleanupStackByComponentId[component.id].push(() => node.disabled = false)
+
                     node.disabled = true
                 } else if (
                     // <input type="text">
@@ -28,9 +34,19 @@ export default function () {
                     // <textarea>
                     || node.tagName.toLowerCase() === 'textarea'
                 ) {
+                    if (! node.readOnly) cleanupStackByComponentId[component.id].push(() => node.readOnly = false)
+
                     node.readOnly = true
                 }
             })
         })
+    })
+
+    store.registerHook('messageFailed', (component, message) => {
+        if (! cleanupStackByComponentId[component.id]) return
+
+        while (cleanupStackByComponentId[component.id].length > 0) {
+            cleanupStackByComponentId[component.id].shift()()
+        }
     })
 }
