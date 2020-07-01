5b4376646e1b863968b02f70a11da6393f9bb20b
Dan Harrin <git@DanHarrin.com>
2020-07-01 15:28:12 +0100

    refactor test

--- tests/ComponentLocaleIsPersistedTest.php
+++ tests/ComponentLocaleIsPersistedTest.php
@@ -11,26 +11,26 @@ class ComponentLocaleIsPersisted extends TestCase
     /** @test */
     public function a_livewire_component_can_persist_its_locale()
     {
-        $component = Livewire::test(ComponentForLocalePersistanceHydrationMiddleware::class)
-            ->assertSet('locale', null);
-
-        // Ensure locale isn't persisted in the test session
+        // Set locale
         App::setLocale('en');
+        $this->assertEquals(App::getLocale(), 'en');
 
-        $component->call('checkLocale')
-            ->assertSet('locale', 'es');
+        // Mount component and new ensure locale is set
+        $component = Livewire::test(ComponentForLocalePersistanceHydrationMiddleware::class);
+        $this->assertEquals(App::getLocale(), 'es');
+
+        // Reset locale to ensure it isn't persisted in the test session
+        App::setLocale('en');
+        $this->assertEquals(App::getLocale(), 'en');
+
+        // Verify locale is persisted from component mount
+        $component->call('$refresh');
+        $this->assertEquals(App::getLocale(), 'es');
     }
 }
 
 class ComponentForLocalePersistanceHydrationMiddleware extends Component
 {
-    public $locale;
-
-    public function checkLocale()
-    {
-        $this->locale = App::getLocale();
-    }
-
     public function mount()
     {
         App::setLocale('es');
