824ab672c1617eb228aba3320189698691673e82
iantasker <ian.tasker@assembly.education>
2020-07-08 01:30:43 +0100

    simplified dehydrate logic

--- src/WithFileUploads.php
+++ src/WithFileUploads.php
@@ -102,9 +102,7 @@ trait WithFileUploads
     {
         if ($value instanceof TemporaryUploadedFile) {
             return $value->serializeForLivewireResponse();
-        } elseif (is_array($value) && isset(array_values($value)[0]) && array_values($value)[0] instanceof TemporaryUploadedFile) {
-            return array_values($value)[0]::serializeMultipleForLivewireResponse($value);
-        } elseif (is_array($value) && !empty($value) && array_keys($value) !== range(0, count($value) - 1)) {
+        } elseif (is_array($value)) {
             collect(array_keys($value))->map(function ($key) use ($value) {
                 if ($value[$key] instanceof TemporaryUploadedFile) {
                     return [$key, 'singleUpload'];
