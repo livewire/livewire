615e7857438cd21f3a86163a97985463dca35979
Caleb Porzio <calebporzio@gmail.com>
2020-07-27 20:12:20 -0400

    Add squishy

---
+++ tests/Browser/SupportCollections/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Browser\SupportCollections;
+
+use Livewire\Livewire;
+use Tests\Browser\TestCase;
+use Tests\Browser\SupportCollections\Component;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function happy_path()
+    {
+        $this->browse(function ($browser) {
+            Livewire::visit($browser, Component::class)
+                ->assertTrue();
+        });
+    }
+}
