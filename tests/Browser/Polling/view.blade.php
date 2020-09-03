b8b5c741980014e4699728cc2da608bd643e3246
Adrian Nürnberger <adrian@nuernberger.me>
2020-09-03 08:02:06 +0200

    finished polling test

--- tests/Browser/Polling/view.blade.php
+++ tests/Browser/Polling/view.blade.php
@@ -1,4 +1,5 @@
-<div>
+<div @if($count >= 4) wire:poll.90ms @endif wire:key="2">
+    <button wire:click="$refresh" dusk="button">count++</button>
     <div @if($count < 3) wire:poll.80ms @endif wire:key="1">
         <span dusk="output">{{ $count }}</span>
     </div>
