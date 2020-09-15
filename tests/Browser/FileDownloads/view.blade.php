97c3a95b756e2b11eef16833ffe5138baba441f2
Jim Peters <jpeters@pavintheway.com>
2020-09-15 16:20:20 -0400

    remove empty line

--- tests/Browser/FileDownloads/view.blade.php
+++ tests/Browser/FileDownloads/view.blade.php
@@ -3,5 +3,4 @@
     <button wire:click="downloadFromResponse" dusk="download-from-response">Download</button>
     <button wire:click="downloadQuotedContentDispositionFilename" dusk="download-quoted-disposition-filename">Download</button>
     <button wire:click="downloadQuotedContentDispositionFilenameFromResponse" dusk="download-from-response-quoted-disposition-filename">Download</button>
-
 </div>
