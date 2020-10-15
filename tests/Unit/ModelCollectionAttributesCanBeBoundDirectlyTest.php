8d0cee26a1b90d2e84aba45877fcc7d66acbac8a
Caleb Porzio <calebporzio@gmail.com>
2020-10-15 11:59:02 -0400

    Add test for custom collections

--- tests/Unit/ModelCollectionAttributesCanBeBoundDirectlyTest.php
+++ tests/Unit/ModelCollectionAttributesCanBeBoundDirectlyTest.php
@@ -6,6 +6,7 @@ use Sushi\Sushi;
 use Livewire\Livewire;
 use Livewire\Component;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Livewire\Exceptions\CorruptComponentPayloadException;
 use Livewire\Exceptions\CannotBindToModelDataWithoutValidationRuleException;
 
@@ -67,6 +68,32 @@ class ModelCollectionAttributesCanBeBoundDirectlyTest extends TestCase
     }
 
     /** @test */
+    public function can_use_a_custom_model_collection_and_bind_to_values()
+    {
+        // Reset Sushi model.
+        (new ModelWithCustomCollectionForBinding)->resolveConnection()->getSchemaBuilder()->drop((new ModelWithCustomCollectionForBinding)->getTable());
+        (new ModelWithCustomCollectionForBinding)->migrate();
+
+        Livewire::test(ComponentWithModelsProperty::class)
+            ->call('setModelsToCustomCollection')
+            ->assertSet('models.0.title', 'foo')
+            ->assertPayloadSet('models.0.title', 'foo')
+            ->set('models.0.title', 'bo')
+            ->assertSet('models.0.title', 'bo')
+            ->call('refreshModels')
+            ->assertSet('models.0.title', 'foo')
+            ->set('models.0.title', 'bo')
+            ->call('save')
+            ->assertHasErrors('models.0.title')
+            ->set('models.0.title', 'boo')
+            ->call('save')
+            ->call('refreshModels')
+            ->assertSet('models.0.title', 'boo')
+            ->call('getTypeOfModels')
+            ->assertSet('modelsType', CustomCollection::class);
+    }
+
+    /** @test */
     public function cant_set_a_model_attribute_that_isnt_present_in_rules_array()
     {
         // Reset Sushi model.
@@ -108,9 +135,31 @@ class ModelForBinding extends Model
     ];
 }
 
+class CustomCollection extends EloquentCollection
+{
+    //
+}
+
+class ModelWithCustomCollectionForBinding extends Model
+{
+    use Sushi;
+
+    protected $rows = [
+        ['title' => 'foo'],
+        ['title' => 'bar'],
+        ['title' => 'baz'],
+    ];
+
+    public function newCollection(array $models = [])
+    {
+        return new CustomCollection($models);
+    }
+}
+
 class ComponentWithModelsProperty extends Component
 {
     public $models;
+    public $modelsType;
 
     protected $rules = [
         'models.*.title' => 'required|min:3',
@@ -124,6 +173,16 @@ class ComponentWithModelsProperty extends Component
     public function addModel()
     {
         $this->models->push(new ModelForBinding);
+    }
+
+    public function setModelsToCustomCollection()
+    {
+        $this->models = ModelWithCustomCollectionForBinding::all();
+    }
+
+    public function getTypeOfModels()
+    {
+        $this->modelsType = get_class($this->models);
     }
 
     public function save()
