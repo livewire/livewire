1505d69e6b6e68b9247a4d45d5d03fd651682374
Austen Cameron <austen.cameron@gmail.com>
2020-10-24 10:20:46 -0600

    added --config option

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -6,16 +6,31 @@ use Illuminate\Console\Command;
 
 class PublishCommand extends Command
 {
-    protected $signature = 'livewire:publish {--assets : Indicates if Livewire\'s front-end assets should be published}';
+    protected $signature = 'livewire:publish 
+        { --assets : Indicates if Livewire\'s front-end assets should be published }
+        { --config : Indicates if Livewire\'s config file should be published }';
 
     protected $description = 'Publish Livewire configuration';
 
     public function handle()
     {
         if ($this->option('assets')) {
-            $this->call('vendor:publish', ['--tag' => 'livewire:assets', '--force' => true]);
+            $this->publishAssets();
+        } elseif ($this->option('config')) {
+            $this->publishConfig();
         } else {
-            $this->call('vendor:publish', ['--tag' => 'livewire:config', '--force' => true]);
+            $this->publishAssets();
+            $this->publishConfig();
         }
+    }
+
+    public function publishAssets()
+    {
+        $this->call('vendor:publish', ['--tag' => 'livewire:assets', '--force' => true]);
+    }
+
+    public function publishConfig()
+    {
+        $this->call('vendor:publish', ['--tag' => 'livewire:config', '--force' => true]);
     }
 }
