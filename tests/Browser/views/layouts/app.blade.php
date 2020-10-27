5dc43281904724ab29b54ce0c6770a05514d5eca
Caleb Porzio <calebporzio@gmail.com>
2020-10-27 14:34:11 -0400

    Bump Alpine to 2.7.3

--- tests/Browser/views/layouts/app.blade.php
+++ tests/Browser/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.x/dist/alpine.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js" defer></script>
     <!-- <script src="http://alpine.test/dist/alpine.js" defer></script> -->
     @livewireStyles
 </head>
