ca087c928facc92731741cd04df12e32a4adc8ae
Caleb Porzio <calebporzio@gmail.com>
2020-11-02 12:35:31 -0500

    Account for "dirty" data when deciding weather or not to "skip" running the Laravel watcher for @entangle

--- tests/Browser/Alpine/EntangleDefer/view.blade.php
+++
@@ -1,9 +0,0 @@
-<div x-data="{ testing:  @entangle('testing').defer }">
-    <input type="text" x-model="testing" dusk="input">
-
-    <p>Alpine: <span dusk="output.alpine" x-text="testing"></span></p>
-
-    <p>Livewire: <span dusk="output.livewire">{{$testing}}</span></p>
-    
-    <button wire:click.prevent="submit" dusk="submit">Submit</button>
-</div>
