1d1f80cac3555824ee1a91ab55d98d0c2454a940
Adrian Nürnberger <adrian@nuernberger.me>
2020-12-15 22:03:57 +0100

    skip rendering on redirects

--- src/ComponentConcerns/PerformsRedirects.php
+++ src/ComponentConcerns/PerformsRedirects.php
@@ -9,15 +9,18 @@ trait PerformsRedirects
     public function redirect($url)
     {
         $this->redirectTo = $url;
+        $this->shouldSkipRender = true;
     }
 
     public function redirectRoute($name, $parameters = [], $absolute = true)
     {
         $this->redirectTo = route($name, $parameters, $absolute);
+        $this->shouldSkipRender = true;
     }
 
     public function redirectAction($name, $parameters = [], $absolute = true)
     {
         $this->redirectTo = action($name, $parameters, $absolute);
+        $this->shouldSkipRender = true;
     }
 }
