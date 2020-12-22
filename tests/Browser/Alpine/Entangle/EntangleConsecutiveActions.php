cbc341dfafae636f04ceb5bde25eb0a4301562ad
Josh Hanley <josh@hitsystems.com.au>
2020-12-22 17:44:44 +1100

    Add failing test for consecutive changes

---
+++ tests/Browser/Alpine/Entangle/EntangleConsecutiveActions.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Browser\Alpine\Entangle;
+
+use Livewire\Component as BaseComponent;
+
+class EntangleConsecutiveActions extends BaseComponent
+{
+    public $livewireList = [];
+
+    public function add()
+    {
+        $this->livewireList[] = count($this->livewireList);
+    }
+
+    public function render()
+    {
+        return
+<<<'HTML'
+<div x-data="{ alpineList: @entangle('livewireList') }">
+    <div>Alpine</div>
+    <div dusk="alpineOutput">
+        <template x-for="(item, key) in alpineList" :key="key">
+            <div x-text="item"></div>
+        </template>
+    </div>
+
+    <div>Livewire</div>
+    <div dusk="livewireOutput">
+        @foreach($livewireList as $key => $item)
+            <div>{{ $item }}</div>
+        @endforeach
+    </div>
+
+    <div>
+        <button dusk="alpineAdd" type="button" x-on:click="alpineList.push(alpineList.length)">Add Alpine</button>
+        <button dusk="livewireAdd" type="button" wire:click="add">Add Livewire</button>
+    </div>
+</div>
+HTML;
+    }
+}
