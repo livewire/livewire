002c43158405d2dce9e38bcae3aad6c18933e588
Gertjan Roke <g.a.roke90@gmail.com>
2021-01-12 08:02:49 +0100

    Removed unrelated tests
    Signed-off-by: Gertjan Roke <g.a.roke90@gmail.com>

--- tests/Unit/ComponentLayoutTest.php
+++ tests/Unit/ComponentLayoutTest.php
@@ -9,46 +9,6 @@ use Livewire\Livewire;
 class ComponentLayoutTest extends TestCase
 {
     /** @test */
-    public function can_extend_a_blade_layout()
-    {
-        Livewire::component(ComponentWithExtendsLayout::class);
-
-        Route::get('/foo', ComponentWithExtendsLayout::class);
-
-        $this->get('/foo')->assertSee('baz');
-    }
-
-    /** @test */
-    public function can_set_custom_section()
-    {
-        Livewire::component(ComponentWithCustomSection::class);
-
-        Route::get('/foo', ComponentWithCustomSection::class);
-
-        $this->get('/foo')->assertSee('baz');
-    }
-
-    /** @test */
-    public function can_set_custom_layout()
-    {
-        Livewire::component(ComponentWithCustomLayout::class);
-
-        Route::get('/foo', ComponentWithCustomLayout::class);
-
-        $this->get('/foo')->assertSee('baz');
-    }
-
-    /** @test */
-    public function can_set_custom_slot_for_a_layout()
-    {
-        Livewire::component(ComponentWithCustomSlotForLayout::class);
-
-        Route::get('/foo', ComponentWithCustomSlotForLayout::class);
-
-        $this->withoutExceptionHandling()->get('/foo')->assertSee('baz');
-    }
-
-    /** @test */
     public function can_set_slot_value_of_a_layout()
     {
         Livewire::component(ComponentWithSettingSlotValue::class);
@@ -56,46 +16,6 @@ class ComponentLayoutTest extends TestCase
         Route::get('/foo', ComponentWithSettingSlotValue::class);
 
         $this->get('/foo')->assertSee('baz');
-    }
-}
-
-class ComponentWithExtendsLayout extends Component
-{
-    public function render()
-    {
-        return view('null-view')->extends('layouts.app-extends', [
-            'bar' => 'baz'
-        ]);
-    }
-}
-
-class ComponentWithCustomSection extends Component
-{
-    public $name = 'baz';
-
-    public function render()
-    {
-        return view('show-name')->extends('layouts.app-custom-section')->section('body');
-    }
-}
-
-class ComponentWithCustomLayout extends Component
-{
-    public function render()
-    {
-        return view('null-view')->layout('layouts.app-with-bar', [
-            'bar' => 'baz'
-        ]);
-    }
-}
-
-class ComponentWithCustomSlotForLayout extends Component
-{
-    public $name = 'baz';
-
-    public function render()
-    {
-        return view('show-name')->layout('layouts.app-custom-slot')->slot('main');
     }
 }
 
