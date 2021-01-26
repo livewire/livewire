af0b5605ab369ffb09d581c975bcf0c4df47a656
Ihor Vakulenko <vakulenko.i.wezom@gmail.com>
2021-01-26 16:01:06 +0200

    base64 make return "/" and break the path

--- src/TemporaryUploadedFile.php
+++ src/TemporaryUploadedFile.php
@@ -124,7 +124,7 @@ class TemporaryUploadedFile extends UploadedFile
     public static function generateHashNameWithOriginalNameEmbedded($file)
     {
         $hash = Str::random(30);
-        $meta = '-meta'.base64_encode($file->getClientOriginalName()).'-';
+        $meta = str_replace('/', '_', '-meta'.base64_encode($file->getClientOriginalName()).'-');
         $extension = '.'.$file->guessExtension();
 
         return $hash.$meta.$extension;
@@ -132,7 +132,7 @@ class TemporaryUploadedFile extends UploadedFile
 
     public function extractOriginalNameFromFilePath($path)
     {
-        return base64_decode(head(explode('-', last(explode('-meta', $path)))));
+        return base64_decode(head(explode('-', last(explode('-meta', str_replace('_', '/', $path))))));
     }
 
     public static function createFromLivewire($filePath)
