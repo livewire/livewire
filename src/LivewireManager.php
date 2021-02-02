ee689b019eb8b4e3cb9b889c2508f21eedd7c547
Caleb Porzio <calebporzio@gmail.com>
2021-02-02 11:34:35 -0500

    Re-order classes for line-length

--- src/LivewireManager.php
+++ src/LivewireManager.php
@@ -17,10 +17,10 @@ class LivewireManager
         \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         \Laravel\Jetstream\Http\Middleware\AuthenticateSession::class,
         \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \App\Http\Middleware\RedirectIfAuthenticated::class,
         \Illuminate\Auth\Middleware\Authenticate::class,
         \Illuminate\Auth\Middleware\Authorize::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \App\Http\Middleware\Authenticate::class,
     ];
 
