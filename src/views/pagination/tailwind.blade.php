c4fe651f8eb2918ad87660c539ebcf6ac66869b4
Mert Aşan <mertemreasan@gmail.com>
2021-03-15 05:58:18 +0300

    Update tailwind.blade.php

--- src/views/pagination/tailwind.blade.php
+++ src/views/pagination/tailwind.blade.php
@@ -30,13 +30,7 @@
             <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
                 <div>
                     <p class="text-sm text-gray-700 leading-5">
-                        <span>{!! __('Showing') !!}</span>
-                        <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                        <span>{!! __('to') !!}</span>
-                        <span class="font-medium">{{ $paginator->lastItem() }}</span>
-                        <span>{!! __('of') !!}</span>
-                        <span class="font-medium">{{ $paginator->total() }}</span>
-                        <span>{!! __('results') !!}</span>
+                        {!! __('<span>Showing <span class="font-medium">:firstItem</span> to <span class="font-medium">:lastItem</span> of <span class="font-medium">:total</span> results</span>', ['firstItem' => $paginator->firstItem(), 'lastItem' => $paginator->lastItem(), 'total' => $paginator->total()]) !!}
                     </p>
                 </div>
 
