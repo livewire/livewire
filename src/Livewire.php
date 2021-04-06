1386210324c7b0063594c5e9d82d7053893a7a31
James Kingsley <jlkingsley97@gmail.com>
2021-04-06 15:57:20 +0100

    Add actingAs method to docblock

--- src/Livewire.php
+++ src/Livewire.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static void component($alias, $viewClass)
  * @method static \Livewire\Testing\TestableLivewire test($name, $params = [])
+ * @method static \Livewire\LivewireManager actingAs($user, $driver = null)
  *
  * @see \Livewire\LivewireManager
  */
