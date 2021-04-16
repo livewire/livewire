f8d0513a0bcb052f4e762666eded2224b7bd1ea1
Mo Khosh <mskhoshnazar@gmail.com>
2021-04-16 21:45:54 +0430

    poll if any part is visible, not if every part is visible

--- js/component/Polling.js
+++ js/component/Polling.js
@@ -73,9 +73,9 @@ function inViewport(el) {
     var bounding = el.getBoundingClientRect();
 
     return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        bounding.top < (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.left < (window.innerWidth || document.documentElement.clientWidth) &&
+        bounding.bottom > 0 &&
+        bounding.right > 0
     );
 }
