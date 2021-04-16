b3f63648d1ff377f4bb2aa6e805673b829f70e95
Caleb Porzio <calebporzio@gmail.com>
2021-04-16 10:27:27 -0400

    Merge master and re-build

--- js/util/index.js
+++ js/util/index.js
@@ -4,7 +4,6 @@ export * from './wire-directives'
 export * from './walk'
 export * from './dispatch'
 export * from './getCsrfToken'
-export * from './inViewPort'
 
 export function kebabCase(subject) {
     return subject.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/[_\s]/, '-').toLowerCase()
