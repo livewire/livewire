26739ae5276017961f13a2a524eccd8bf95dfc76
Marcus Olsson <hello@marcusolsson.me>
2021-05-06 13:38:20 +0200

    Make attribute translatable

--- src/WithFileUploads.php
+++ src/WithFileUploads.php
@@ -51,9 +51,16 @@ trait WithFileUploads
         $this->emit('upload:errored', $name)->self();
 
         if (is_null($errorsInJson)) {
-            $genericValidationMessage = trans('validation.uploaded', ['attribute' => $name]);
-            if ($genericValidationMessage === 'validation.uploaded') $genericValidationMessage = "The {$name} failed to upload.";
-            throw ValidationException::withMessages([$name => $genericValidationMessage]);
+            // Handle any translations/custom names
+            $translator = app()->make('translator');
+
+            $attribute = $translator->get("validation.attributes.{$name}");
+            if ($attribute === "validation.attributes.{$name}") $attribute = $name;
+
+            $message = trans('validation.uploaded', ['attribute' => $attribute]);
+            if ($message === 'validation.uploaded') $message = "The {$name} failed to upload.";
+
+            throw ValidationException::withMessages([$name => $message]);
         }
 
         $errorsInJson = $isMultiple
