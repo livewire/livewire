b0fc2a8cfe0ee7c2f74e5f09239db3acc5f77d55
Ibrahim AshShohail <me@ibrasho.com>
2021-06-04 16:45:45 +0300

    Remove excess semicolon

--- src/Macros/livewire-view-extends.blade.php
+++ src/Macros/livewire-view-extends.blade.php
@@ -1,5 +1,5 @@
 @extends($view, $params)
 
 @section($slotOrSection)
-    {!! $manager->initialDehydrate()->toInitialResponse()->effects['html']; !!}
+    {!! $manager->initialDehydrate()->toInitialResponse()->effects['html'] !!}
 @endsection
