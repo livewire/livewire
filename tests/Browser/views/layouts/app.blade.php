8d8245509bbf77e8a6c5f35d697e0ba752c1f648
Caleb Porzio <calebporzio@gmail.com>
2021-06-14 14:21:24 -0400

    wip

--- tests/Browser/views/layouts/app.blade.php
+++ tests/Browser/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @if (config('use_alpine_v3'))
-        <script src="http://alpine.test/packages/alpinejs/dist/cdn.js" defer></script>
+        <script src="https://unpkg.com/alpinejs@3.0.2/dist/cdn.min.js" defer></script>
     @else
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
     @endif
