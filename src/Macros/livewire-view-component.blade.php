78c980758a5b33fba6c41d0401f46da66a2fbe3a
Josh Hanley <josh@hitsystems.com.au>
2021-07-11 09:25:18 +1000

    Add start and end html comments to component and extends output views

--- src/Macros/livewire-view-component.blade.php
+++ src/Macros/livewire-view-component.blade.php
@@ -1,5 +1,7 @@
 @component($view, $params)
     @slot($slotOrSection)
+        <!-- Livewire Component wire-start:{{ $manager->initialDehydrate()->toInitialResponse()->fingerprint['id'] }} -->
         {!! $manager->initialDehydrate()->toInitialResponse()->effects['html'] !!}
+        <!-- Livewire Component wire-end:{{ $manager->initialDehydrate()->toInitialResponse()->fingerprint['id'] }} -->
     @endslot
 @endcomponent
--- src/Macros/livewire-view-extends.blade.php
+++ src/Macros/livewire-view-extends.blade.php
@@ -1,5 +1,7 @@
 @extends($view, $params)
 
 @section($slotOrSection)
+    <!-- Livewire Component wire-start:{{ $manager->initialDehydrate()->toInitialResponse()->fingerprint['id'] }} -->
     {!! $manager->initialDehydrate()->toInitialResponse()->effects['html'] !!}
+    <!-- Livewire Component wire-end:{{ $manager->initialDehydrate()->toInitialResponse()->fingerprint['id'] }} -->
 @endsection
