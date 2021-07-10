0744c83053f61df8948a4445338b418b78baeb2e
Josh Hanley <josh@hitsystems.com.au>
2021-07-11 09:24:36 +1000

    Fix bug where view get data errors doesn't return an `ErrorBag`

--- src/Component.php
+++ src/Component.php
@@ -161,7 +161,7 @@ abstract class Component
         $engine->startLivewireRendering($this);
 
         $this->setErrorBag(
-            $errorBag = $errors ?: ($view->getData()['errors'] ?? $this->getErrorBag())
+            $errorBag = $errors ?: (isset($view->getData()['errors']) ? $view->getData()['errors']->getBag('default') : $this->getErrorBag())
         );
 
         $previouslySharedErrors = app('view')->getShared()['errors'] ?? new ViewErrorBag;
