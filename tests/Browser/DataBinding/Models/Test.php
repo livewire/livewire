1237bf8eac5f56e35d15b532aa817a54272cc52d
Josh Hanley <josh@hitsystems.com.au>
2021-07-28 06:38:58 +1000

    Update test to include persisting to database and checking result

--- tests/Browser/DataBinding/Models/Test.php
+++ tests/Browser/DataBinding/Models/Test.php
@@ -44,8 +44,17 @@ class Test extends TestCase
 
                 ->waitForLivewire()->type('@author.posts.0.comments.1.author.name', 'Mike')
                 ->assertSeeIn('@output.author.posts.0.comments.1.author.name', 'Mike')
+
+                ->waitForLivewire()->click('@save')
                 ;
         });
+
+        $author = Author::with(['posts', 'posts.comments', 'posts.comments.author'])->first();
+
+        $this->assertEquals('Steve', $author->name);
+        $this->assertEquals('Article 1', $author->posts[0]->title);
+        $this->assertEquals('Message 1', $author->posts[0]->comments[0]->comment);
+        $this->assertEquals('Mike', $author->posts[0]->comments[1]->author->name);
     }
 }
 
