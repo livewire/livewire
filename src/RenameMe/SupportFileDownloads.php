9af274805a349474683eb040337e51cfde0117c9
Mert Aşan <mertemreasan@gmail.com>
2021-08-07 06:40:48 +0300

    add contentType

--- src/RenameMe/SupportFileDownloads.php
+++ src/RenameMe/SupportFileDownloads.php
@@ -33,6 +33,7 @@ class SupportFileDownloads
             $this->downloadsById[$component->id] = [
                 'name' => $name,
                 'content' => $content,
+                'contentType' => $response->headers->get('Content-Type'),
             ];
 
             $component->skipRender();
