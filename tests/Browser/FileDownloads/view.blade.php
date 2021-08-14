d0a86d7f0ba29519c64f746c92ffcf761fc276c9
Mert Aşan <mertemreasan@gmail.com>
2021-08-14 18:58:06 +0300

    Add `Content-Type` to file download info in effects (#3485)
    * add contentType
    
    * add new assertions
    
    * add file download tests
    
    * pass content-type to blob
    
    * add dusk tests
    
    * force the null content-type to be an empty string
    
    * add new dusk tests
    
    * Remove extra assertions
    
    Co-authored-by: Caleb Porzio <calebporzio@gmail.com>

--- tests/Browser/FileDownloads/view.blade.php
+++ tests/Browser/FileDownloads/view.blade.php
@@ -3,4 +3,10 @@
     <button wire:click="downloadFromResponse" dusk="download-from-response">Download</button>
     <button wire:click="downloadQuotedContentDispositionFilename" dusk="download-quoted-disposition-filename">Download</button>
     <button wire:click="downloadQuotedContentDispositionFilenameFromResponse" dusk="download-from-response-quoted-disposition-filename">Download</button>
+    <button wire:click="downloadWithContentTypeHeader('text/html')" dusk="download-with-content-type-header">Download</button>
+    <button wire:click="downloadWithContentTypeHeader()" dusk="download-with-null-content-type-header">Download</button>
+    <button wire:click="downloadAnUntitledFileWithContentTypeHeader" dusk="download-an-untitled-file-with-content-type-header">Download</button>
+    <button wire:click="downloadAnUntitledFileWithContentTypeHeader('foo')" dusk="download-an-untitled-file-with-invalid-content-type-header">Download</button>
+    <button wire:click="downloadFromResponseWithContentTypeHeader" dusk="download-from-response-with-content-type-header">Download</button>
+    <input dusk="content-type" />
 </div>
