ff6f01568bc02e3ff15bcd9862a89a823cc71957
Josh Hanley <josh@hitsystems.com.au>
2021-08-21 21:10:35 +1000

    Remove null-coalescing assignment operator for older php versions

--- src/ComponentConcerns/PerformsRedirects.php
+++ src/ComponentConcerns/PerformsRedirects.php
@@ -10,20 +10,20 @@ trait PerformsRedirects
     {
         $this->redirectTo = $url;
         // This is set to true if no config found, as this was the default for Livewire
-        $this->shouldSkipRender ??= config('livewire.should_skip_render_on_redirect', true);
+        $this->shouldSkipRender = $this->shouldSkipRender ?? config('livewire.should_skip_render_on_redirect', true);
     }
 
     public function redirectRoute($name, $parameters = [], $absolute = true)
     {
         $this->redirectTo = route($name, $parameters, $absolute);
         // This is set to true if no config found, as this was the default for Livewire
-        $this->shouldSkipRender ??= config('livewire.should_skip_render_on_redirect', true);
+        $this->shouldSkipRender = $this->shouldSkipRender ?? config('livewire.should_skip_render_on_redirect', true);
     }
 
     public function redirectAction($name, $parameters = [], $absolute = true)
     {
         $this->redirectTo = action($name, $parameters, $absolute);
         // This is set to true if no config found, as this was the default for Livewire
-        $this->shouldSkipRender ??= config('livewire.should_skip_render_on_redirect', true);
+        $this->shouldSkipRender = $this->shouldSkipRender ?? config('livewire.should_skip_render_on_redirect', true);
     }
 }
