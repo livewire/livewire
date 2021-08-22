dc8c1c7b767a96bd2ee7e9b1d92249d273e638ba
Caleb Porzio <calebporzio@gmail.com>
2021-08-21 20:23:53 -0400

    Add tests

--- tests/Browser/SupportCollections/Test.php
+++ tests/Browser/SupportCollections/Test.php
@@ -13,8 +13,13 @@ class Test extends TestCase
             Livewire::visit($browser, Component::class)
                 ->assertSeeIn('@things', 'foo')
                 ->assertDontSeeIn('@things', 'bar')
+                ->assertSeeIn('@unordered', 'foo')
+                ->assertSeeIn('@unordered', 'bar')
                 ->waitForLivewire()->click('@add-bar')
                 ->assertSeeIn('@things', 'bar')
+                ->assertSeeIn('@unordered', 'foo')
+                ->assertSeeIn('@unordered', 'bar')
+                ->assertSeeIn('@unordered', 'baz')
             ;
         });
     }
