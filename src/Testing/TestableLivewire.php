b7966cb1b2cd514da5ce2c922080d06afb6dcba4
Caleb Porzio <calebporzio@gmail.com>
2021-09-07 17:26:29 -0400

    Throw error when testing non-livewire class (#3680)

--- src/Testing/TestableLivewire.php
+++ src/Testing/TestableLivewire.php
@@ -10,6 +10,7 @@ use Livewire\GenerateSignedUploadUrl;
 use Illuminate\Routing\RouteCollection;
 use Illuminate\Support\Traits\Macroable;
 use Facades\Livewire\GenerateSignedUploadUrl as GenerateSignedUploadUrlFacade;
+use Livewire\Component;
 use Livewire\Exceptions\PropertyNotFoundException;
 use Livewire\LivewireManager;
 
@@ -70,6 +71,10 @@ class TestableLivewire
         // This allows the user to test a component by it's class name,
         // and not have to register an alias.
         if (class_exists($name)) {
+            if (! is_subclass_of($name, Component::class)) {
+                throw new \Exception('Class ['.$name.'] is not a subclass of Livewire\Component.');
+            }
+
             $componentClass = $name;
             app('livewire')->component($name = str()->random(20), $componentClass);
         }
