58017dbdbed81d21d2418778d0abcf04e993160f
Caleb Porzio <calebporzio@gmail.com>
2021-09-25 02:32:30 -0400

    Add id to event firing

--- src/HydrationMiddleware/PerformEventEmissions.php
+++ src/HydrationMiddleware/PerformEventEmissions.php
@@ -13,10 +13,11 @@ class PerformEventEmissions implements HydrationMiddleware
             foreach ($request->updates as $update) {
                 if ($update['type'] !== 'fireEvent') continue;
 
+                $id = $update['payload']['id'];
                 $event = $update['payload']['event'];
                 $params = $update['payload']['params'];
 
-                $unHydratedInstance->fireEvent($event, $params);
+                $unHydratedInstance->fireEvent($event, $params, $id);
             }
         } catch (ValidationException $e) {
             Livewire::dispatch('failed-validation', $e->validator);
