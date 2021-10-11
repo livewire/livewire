1a82e97efc95a4e30f5efa902addb3a1525a1423
Łukasz Franikowski <lukasz9.3@interia.pl>
2021-10-11 16:57:51 +0200

    [Fix] Added app_url config value - fixing CDN url problem (#3929)
    * Separated appUrl from assetsUrl
    
    * Separated appUrl and assetsUrl
    
    * Added config value for livewire.app_url
    
    * Simplified conditionals
    
    * Changed new app_url config value to use app.url from Laravel config
    
    * Changed new app_url config value to use app.url from Laravel config - fixed tests
    
    * Revert "Changed new app_url config value to use app.url from Laravel config - fixed tests"
    
    This reverts commit 3663c79df0cd761d04574d0235e09ba4a7883bab.
    
    * Revert "Changed new app_url config value to use app.url from Laravel config"
    
    This reverts commit 0e4f60ff0b50a8c2eb472a1c8c36159d3ff7dbf1.
    
    Co-authored-by: Lukasz Franikowski <lfranikowski@getresponse.com>

--- src/LivewireManager.php
+++ src/LivewireManager.php
@@ -219,7 +219,11 @@ HTML;
     {
         $jsonEncodedOptions = $options ? json_encode($options) : '';
 
-        $appUrl = config('livewire.asset_url') ?: rtrim($options['asset_url'] ?? '', '/');
+        $assetsUrl = config('livewire.asset_url') ?: rtrim($options['asset_url'] ?? '', '/');
+
+        $appUrl = config('livewire.app_url')
+            ?: rtrim($options['app_url'] ?? '', '/')
+            ?: $assetsUrl;
 
         $jsLivewireToken = app()->has('session.store') ? "'" . csrf_token() . "'" : 'null';
 
@@ -227,7 +231,7 @@ HTML;
         $versionedFileName = $manifest['/livewire.js'];
 
         // Default to dynamic `livewire.js` (served by a Laravel route).
-        $fullAssetPath = "{$appUrl}/livewire{$versionedFileName}";
+        $fullAssetPath = "{$assetsUrl}/livewire{$versionedFileName}";
         $assetWarning = null;
 
         $nonce = isset($options['nonce']) ? "nonce=\"{$options['nonce']}\"" : '';
@@ -237,7 +241,7 @@ HTML;
             $publishedManifest = json_decode(file_get_contents(public_path('vendor/livewire/manifest.json')), true);
             $versionedFileName = $publishedManifest['/livewire.js'];
 
-            $fullAssetPath = ($this->isRunningServerless() ? config('app.asset_url') : $appUrl).'/vendor/livewire'.$versionedFileName;
+            $fullAssetPath = ($this->isRunningServerless() ? config('app.asset_url') : $assetsUrl).'/vendor/livewire'.$versionedFileName;
 
             if ($manifest !== $publishedManifest) {
                 $assetWarning = <<<'HTML'
