664b80a7768175b731570250057e6edba0e6cbfd
Josh Hanley <josh@hitsystems.com.au>
2021-10-18 07:07:45 +1100

    Add booted method

--- tests/Unit/BootComponentTest.php
+++ tests/Unit/BootComponentTest.php
@@ -12,27 +12,27 @@ class BootComponentTest extends TestCase
     public function boot_method_is_called_on_mount_and_on_subsequent_updates()
     {
         Livewire::test(ComponentWithBootMethod::class)
-            ->assertSet('memo', 'bootmount')
+            ->assertSet('memo', 'bootmountbooted')
             ->call('$refresh')
-            ->assertSet('memo', 'boothydrate');
+            ->assertSet('memo', 'boothydratebooted');
     }
 
     /** @test */
     public function boot_method_can_be_added_to_trait()
     {
         Livewire::test(ComponentWithBootTrait::class)
-            ->assertSet('memo', 'boottraitinitializemount')
+            ->assertSet('memo', 'boottraitboottraitinitializemountbootedtraitbooted')
             ->call('$refresh')
-            ->assertSet('memo', 'boottraitinitializehydrate');
+            ->assertSet('memo', 'boottraitboottraitinitializehydratebootedtraitbooted');
     }
 
     /** @test */
     public function boot_method_supports_dependency_injection()
     {
         Livewire::test(ComponentWithBootMethodDI::class)
-            ->assertSet('memo', 'boottrait')
+            ->assertSet('memo', 'boottraitbootbootedtraitbooted')
             ->call('$refresh')
-            ->assertSet('memo', 'boottrait');
+            ->assertSet('memo', 'boottraitbootbootedtraitbooted');
     }
 }
 
@@ -56,6 +56,11 @@ class ComponentWithBootMethod extends Component
     public function hydrate()
     {
         $this->_memo .= 'hydrate';
+    }
+
+    public function booted()
+    {
+        $this->_memo .= 'booted';
     }
 
     public function render()
@@ -90,9 +95,15 @@ class ComponentWithBootTrait extends Component
         $this->_memo .= 'hydrate';
     }
 
+    public function booted()
+    {
+        $this->_memo .= 'booted';
+    }
+
     public function render()
     {
         $this->memo = $this->_memo;
+
         return view('null-view');
     }
 }
@@ -101,12 +112,17 @@ trait BootMethodTrait
 {
     public function bootBootMethodTrait()
     {
-        $this->_memo .= 'trait';
+        $this->_memo .= 'traitboot';
     }
 
     public function initializeBootMethodTrait()
     {
-        $this->_memo .= 'initialize';
+        $this->_memo .= 'traitinitialize';
+    }
+
+    public function bootedBootMethodTrait()
+    {
+        $this->_memo .= 'traitbooted';
     }
 }
 
@@ -114,7 +130,12 @@ trait BootMethodTraitWithDI
 {
     public function bootBootMethodTraitWithDI(Stringable $string)
     {
-        $this->_memo .= $string->append('trait');
+        $this->_memo .= $string->append('traitboot');
+    }
+
+    public function bootedBootMethodTraitWithDI(Stringable $string)
+    {
+        $this->_memo .= $string->append('traitbooted');
     }
 }
 
@@ -132,9 +153,15 @@ class ComponentWithBootMethodDI extends Component
         $this->_memo .= $string->append('boot');
     }
 
+    public function booted(Stringable $string)
+    {
+        $this->_memo .= $string->append('booted');
+    }
+
     public function render()
     {
         $this->memo = $this->_memo;
+
         return view('null-view');
     }
 }
