55d851d7afb81195c153deae0ec6c00e30efe7dc
TinaH <tinahammar@gmail.com>
2021-10-27 15:54:13 +0200

    Add docBlock and remove trim()

--- src/helpers.php
+++ src/helpers.php
@@ -18,16 +18,24 @@ if (! function_exists('Livewire\str')) {
 }
 
 if (! function_exists('Livewire\js')) {
+    /**
+     * base64 encode an expression and compile it into 'JSON.parse(atob($base64))' string.
+     *
+     * @param $expression
+     * @param int|null $options
+     * @param int $depth
+     * @return string
+     */
     function js($expression, $options = null, $depth = 512): string
     {
         if (is_object($expression) || is_array($expression)) {
-            $base64 = base64_encode(json_encode($expression, trim($options), trim($depth)));
+            $base64 = base64_encode(json_encode($expression, $options, $depth));
             return "JSON.parse(atob('$base64'))";
         }
         if (is_string($expression)) {
             $string = str_replace("'", "\'", $expression);
             return "'$string'";
         }
-        return json_encode($expression, trim($options), trim($depth));
+        return json_encode($expression, $options, $depth);
     }
 }
