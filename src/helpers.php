e205485cf3a7b95ef9b4e716e15a452794a59215
TinaH <tinahammar@gmail.com>
2021-10-27 15:55:49 +0200

    Remove docBlock there are none elsewhere

--- src/helpers.php
+++ src/helpers.php
@@ -18,14 +18,6 @@ if (! function_exists('Livewire\str')) {
 }
 
 if (! function_exists('Livewire\js')) {
-    /**
-     * base64 encode an expression and compile it into 'JSON.parse(atob($base64))' string.
-     *
-     * @param $expression
-     * @param int|null $options
-     * @param int $depth
-     * @return string
-     */
     function js($expression, $options = null, $depth = 512): string
     {
         if (is_object($expression) || is_array($expression)) {
