4bb9debe08c3f20371e1d48fe4a221086fecdb23
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-10-28 22:45:32 +0200

    Skip public property tests for PHP version below 7.4

--- tests/Unit/PublicPropertyManagerTest.php
+++ tests/Unit/PublicPropertyManagerTest.php
@@ -10,23 +10,32 @@ use Livewire\Livewire;
 
 class PublicPropertyManagerTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (version_compare(PHP_VERSION, '7.4', '>=')) {
+            $this->markTestSkipped('Skip public property tests if the version is below PHP 7.4');
+        }
+    }
+
     /** @test */
     public function the_manager_will_be_available_inside_the_service_provider()
     {
         $this->assertInstanceOf(LivewirePropertyManager::class, app(LivewirePropertyManager::class));
     }
 
-// TODO: We will throw an exception, but I guess we'll need a new interface. Until then, do nothing.
-//
-//    /** @test */
-//    public function it_will_throw_an_exception_if_registering_a_class_not_implementing_the_wireable_interface()
-//    {
-//        $this->expectException(CannotRegisterPublicPropertyWithoutImplementingWireableException::class);
-//
-//        $resolver = new class() {};
-//
-//        app(LivewirePropertyManager::class)->register('className', $resolver);
-//    }
+    /** @test */
+    public function it_will_throw_an_exception_if_registering_a_class_not_implementing_the_wireable_interface()
+    {
+        $this->markTestSkipped(" We will throw an exception, but I guess we'll need a new interface. Until then, do nothing.");
+
+        $this->expectException(CannotRegisterPublicPropertyWithoutImplementingWireableException::class);
+
+        $resolver = new class() {};
+
+        app(LivewirePropertyManager::class)->register('className', $resolver);
+    }
 
     /** @test */
     public function a_custom_property_class_does_take_affect()
