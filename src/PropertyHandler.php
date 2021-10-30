389b1d517a93cc78d0f0196ed8e7a86873251511
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-10-31 00:44:58 +0200

    Change method order in the property handler class

--- src/PropertyHandler.php
+++ src/PropertyHandler.php
@@ -5,16 +5,16 @@ namespace Livewire;
 abstract class PropertyHandler
 {
     /**
-     *  In addition to render HTML, Livewire "dehydrates" or "serializes" the component's state
-     * of a public property, so it can be passed to the front-end.
-     */
-    abstract public function dehydrate();
-
-    /**
      *  When an update is triggered, a request is sent to the server INCLUDING the last-known component state.
      *  The server "hydrates" or "deserializes" the component from that state and performs any updates.
      */
     abstract public static function hydrate($value);
+
+    /**
+     * In addition to render HTML, Livewire "dehydrates" or "serializes" the component's state
+     * of a public property, so it can be passed to the front-end.
+     */
+    abstract public function dehydrate();
 
     /**
      * Some Properties like fx Carbon does need some information if the property does get updated.
