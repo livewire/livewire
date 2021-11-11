c50b0c8aeac7161f975b5235723b6c231aadd8f9
Fabio Ivona <fabio.ivona@defstudio.it>
2021-11-11 14:06:29 +0100

    restore style

--- tests/Unit/FileDownloadsTest.php
+++ tests/Unit/FileDownloadsTest.php
@@ -13,56 +13,56 @@ class FileDownloadsTest extends TestCase
     public function can_download_a_file()
     {
         Livewire::test(FileDownloadComponent::class)
-            ->call('download')
-            ->assertFileDownloaded()
-            ->assertFileDownloaded('download.txt', 'I\'m the file you should download.', 'text/plain');
+                ->call('download')
+                ->assertFileDownloaded()
+                ->assertFileDownloaded('download.txt', 'I\'m the file you should download.', 'text/plain');
     }
 
     /** @test */
     public function can_download_a_file_as_stream()
     {
         Livewire::test(FileDownloadComponent::class)
-            ->call('streamDownload', 'download.txt')
-            ->assertFileDownloaded('download.txt', 'alpinejs');
+                ->call('streamDownload', 'download.txt')
+                ->assertFileDownloaded('download.txt', 'alpinejs');
     }
 
     public function can_download_a_responsable(){
         Livewire::test(FileDownloadComponent::class)
-            ->call('responsableDownload')
-            ->assertFileDownloaded()
-            ->assertFileDownloaded('download.txt', 'I\'m the file you should download.', 'text/plain');
+                ->call('responsableDownload')
+                ->assertFileDownloaded()
+                ->assertFileDownloaded('download.txt', 'I\'m the file you should download.', 'text/plain');
     }
 
     /** @test */
     public function can_download_with_custom_filename()
     {
         Livewire::test(FileDownloadComponent::class)
-            ->call('download', 'download.csv')
-            ->assertFileDownloaded('download.csv', 'I\'m the file you should download.');
+                ->call('download', 'download.csv')
+                ->assertFileDownloaded('download.csv', 'I\'m the file you should download.');
     }
 
     /** @test */
     public function can_download_a_file_as_stream_with_custom_filename()
     {
         Livewire::test(FileDownloadComponent::class)
-            ->call('streamDownload', 'download.csv')
-            ->assertFileDownloaded('download.csv', 'alpinejs');
+                ->call('streamDownload', 'download.csv')
+                ->assertFileDownloaded('download.csv', 'alpinejs');
     }
 
     /** @test */
     public function can_download_with_custom_filename_and_headers()
     {
         Livewire::test(FileDownloadComponent::class)
-            ->call('download', 'download.csv', ['Content-Type' => 'text/csv'])
-            ->assertFileDownloaded('download.csv', 'I\'m the file you should download.', 'text/csv');
+                ->call('download', 'download.csv', ['Content-Type' => 'text/csv'])
+                ->assertFileDownloaded('download.csv', 'I\'m the file you should download.', 'text/csv');
     }
 
     /** @test */
     public function can_download_a_file_as_stream_with_custom_filename_and_headers()
     {
         Livewire::test(FileDownloadComponent::class)
-            ->call('streamDownload', 'download.csv', ['Content-Type' => 'text/csv'])
-            ->assertFileDownloaded('download.csv', 'alpinejs', 'text/csv');
+                ->call('streamDownload', 'download.csv', ['Content-Type' => 'text/csv'])
+                ->assertFileDownloaded('download.csv', 'alpinejs', 'text/csv');
     }
 
     /** @test */
@@ -92,13 +92,13 @@ class FileDownloadComponent extends Component
             echo 'alpinejs';
         }, $filename, $headers);
     }
-  
+
     public function responsableDownload()
     {
         return new DownloadableResponse();
     }
 
-  
+
     public function render()
     {
         return app('view')->make('file-download-component');
