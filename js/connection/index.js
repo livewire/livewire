81fd28a182cbca657abdf134ac98f0f85962db28
Caleb Porzio <calebporzio@gmail.com>
2021-12-20 15:56:06 -0500

    Add basic functionality

--- js/connection/index.js
+++ js/connection/index.js
@@ -17,10 +17,15 @@ export default class Connection {
         return componentStore.onErrorCallback(status, response)
     }
 
-    showExpiredMessage() {
-        confirm(
-            'This page has expired due to inactivity.\nWould you like to refresh the page?'
-        ) && window.location.reload()
+    showExpiredMessage(response, message) {
+        console.log(store.sessionHasExpiredCallback, store.sessionHasExpired, 'yo');
+        if (store.sessionHasExpiredCallback) {
+            store.sessionHasExpiredCallback(response, message)
+        } else {
+            confirm(
+                'This page has expired.\nWould you like to refresh the page?'
+            ) && window.location.reload()
+        }
     }
 
     sendMessage(message) {
@@ -73,7 +78,11 @@ export default class Connection {
 
                         store.sessionHasExpired = true
 
-                        this.showExpiredMessage()
+                        this.showExpiredMessage(response, message)
+
+                        // @todo for deployment invalidation feature:
+                        // - should we do something different than a simple 419?
+                        //   - what would an SPA like Vue/React do here?
                     } else {
                         response.text().then(response => {
                             this.showHtmlModal(response)
