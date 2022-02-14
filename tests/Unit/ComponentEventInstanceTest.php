7aed455f982240a31c74838cf69360958bfe517d
Vladyslav Gaysyuk <mikield@icloud.com>
2022-02-14 13:25:58 +0100

    Changes coverage tests

---
+++ tests/Unit/ComponentEventInstanceTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit;
+
+use Livewire\Component;
+use Livewire\Event;
+use Livewire\Livewire;
+use Livewire\LivewireManager;
+
+class ComponentEventInstanceTest extends TestCase
+{
+    /** @test */
+    public function reieves_event_with_custom_class_name()
+    {
+        $component = Livewire::test(ReceivesEvents::class);
+
+        $component->emit(new CustomEvent('baz'));
+
+
+        $this->assertEquals($component->get('foo'), 'baz');
+    }
+}
+
+class CustomEvent extends Event
+{
+    public function __construct($param)
+    {
+        parent::__construct(self::class, compact('param'));
+    }
+}
+
+class ReceivesEvents extends Component
+{
+    public $foo;
+
+    protected $listeners = [CustomEvent::class => 'onCustomEvent'];
+
+    public function onCustomEvent($value)
+    {
+        $this->foo = $value;
+    }
+
+    public function render()
+    {
+        return app('view')->make('null-view');
+    }
+}
