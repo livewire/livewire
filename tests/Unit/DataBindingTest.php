1c8fbc04f7ecb22724999864d88286b82e189f3b
Marcin Nabiałek <dev@nabialek.org>
2022-02-17 18:12:46 +0100

    Add test to make sure data is expected

--- tests/Unit/DataBindingTest.php
+++ tests/Unit/DataBindingTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use Livewire\Component;
 use Livewire\Livewire;
+use Livewire\TemporaryUploadedFile;
+use Livewire\WithFileUploads;
 
 class DataBindingTest extends TestCase
 {
@@ -43,6 +45,33 @@ class DataBindingTest extends TestCase
 
         $this->assertEquals('baz', $component->foo);
         $this->assertContains('foo', $component->payload['effects']['dirty']);
+    }
+
+    /** @test */
+    public function update_component_data_with_custom_files_structure()
+    {
+        $component = Livewire::test(DataBindingStub::class);
+
+        $component->updateProperty([
+            'files' => [
+                'type_1' => [
+                    TemporaryUploadedFile::fake()->image('sample.jpg', 521, 732),
+                    TemporaryUploadedFile::fake()->image('sample2.png', 1001, 1702),
+                    TemporaryUploadedFile::fake()->image('sample3.png', 1510, 1920),
+                ],
+                'type_2' => [],
+            ],
+        ]);
+
+        $filesProperty = $component->files;
+
+        $this->assertIsArray($filesProperty);
+
+        $this->assertArrayHasKey('type_1', $filesProperty);
+        $this->assertArrayHasKey('type_2', $filesProperty);
+        $this->assertIsString( $filesProperty['type_1']);
+        $this->assertStringStartsWith('livewire-files:[', $filesProperty['type_1']);
+        $this->assertSame([], $filesProperty['type_2']);
     }
 
     /** @test */
@@ -86,8 +115,11 @@ class DataBindingTest extends TestCase
 
 class DataBindingStub extends Component
 {
+    use WithFileUploads;
+
     public $foo;
     public $bar;
+    public $files;
     public $propertyWithHook;
     public $arrayProperty = ['foo', 'bar'];
 
