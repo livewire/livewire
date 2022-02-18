78b7e23226ab1cd40b577fc9d3c50903adc2b160
Noboru Shiroiwa <14008307+nshiro@users.noreply.github.com>
2022-02-19 05:32:19 +0900

    FIX keep child components even when a parent component is skipped rendering. (#4450)
    * Fix cursor pagination in bootstrap
    
    * FIX keep child components even when a parent component is skipped rendering.

--- src/ComponentConcerns/TracksRenderedChildren.php
+++ src/ComponentConcerns/TracksRenderedChildren.php
@@ -41,4 +41,9 @@ trait TracksRenderedChildren
     {
         return $this->renderedChildren;
     }
+
+    public function keepRenderedChildren()
+    {
+        $this->renderedChildren = $this->previouslyRenderedChildren;
+    }
 }
