020ad095cf1239138b097d22b584e2701ec3edfb
Henk Koop <hxnk@users.noreply.github.com>
2022-06-19 04:54:20 +0200

    add facade auto-completion for withQueryParams (#5039)

--- src/Livewire.php
+++ src/Livewire.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static void component($alias, $viewClass)
  * @method static \Livewire\Testing\TestableLivewire test($name, $params = [])
  * @method static \Livewire\LivewireManager actingAs($user, $driver = null)
+ * @method static \Livewire\LivewireManager withQueryParams($queryParams)
  *
  * @see \Livewire\LivewireManager
  */
