798e280b4a81698f40d6f985d7b73e5373a2749c
Josh Hanley <josh@hitsystems.com.au>
2022-11-30 12:19:28 +1100

    Move has rules property tests

--- src/Features/SupportValidation/Test.php
+++ src/Features/SupportValidation/Test.php
@@ -3,10 +3,15 @@
 namespace Livewire\Features\SupportValidation;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ViewErrorBag;
 use Livewire\Component;
+use Livewire\Exceptions\MissingRulesException;
 use Livewire\Livewire;
+use Sushi\Sushi;
+
+use function Livewire\invade;
 
 class Test extends \Tests\TestCase
 {
@@ -14,6 +19,71 @@ class Test extends \Tests\TestCase
     public function can_()
     {
         $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function validate_with_rules_property()
+    {
+        Livewire::test(ComponentWithRulesProperty::class)
+            ->set('foo', '')
+            ->call('save')
+            ->assertHasErrors(['foo' => 'required']);
+    }
+
+    /** @test */
+    public function validate_only_with_rules_property()
+    {
+        Livewire::test(ComponentWithRulesProperty::class)
+            ->set('bar', '')
+            ->assertHasErrors(['bar' => 'required']);
+    }
+
+    /** @test */
+    public function validate_without_rules_property_and_no_args_throws_exception()
+    {
+        $this->expectException(MissingRulesException::class);
+
+        Livewire::test(ComponentWithoutRulesProperty::class)->call('save');
+    }
+
+    /** @test */
+    public function can_validate_uniqueness_on_a_model()
+    {
+        Livewire::test(ComponentWithRulesPropertyAndModelWithUniquenessValidation::class)
+            ->set('foo.name', 'bar')
+            ->call('save')
+            ->assertHasErrors('foo.name')
+            ->set('foo.name', 'blubber')
+            ->call('save')
+            ->assertHasNoErrors('foo.name');
+    }
+
+    /** @test */
+    public function can_validate_uniqueness_on_a_model_but_exempt_the_model_itself()
+    {
+        Livewire::test(ComponentWithRulesPropertyAndModelUniquenessValidationWithIdExceptions::class)
+            ->set('foo.email', 'baz@example.com')
+            ->call('save')
+            ->assertHasNoErrors('foo.email')
+            ->set('foo.email', 'baz@example.com')
+            ->call('save')
+            ->assertHasNoErrors('foo.email')
+            ->set('foo.email', 'bar@example.com')
+            ->call('save')
+            ->assertHasErrors('foo.email');
+    }
+
+    /** @test */
+    public function can_validate_collection_properties()
+    {
+        Livewire::test(ComponentWithRulesProperty::class)
+            ->set('foo', 'filled')
+            ->call('save')
+            ->assertHasErrors('baz.*.foo')
+            ->set('baz.0.foo', 123)
+            ->set('baz.1.foo', 456)
+            ->call('save')
+            ->assertHasNoErrors('baz.*.foo');
     }
 
     /** @test */
@@ -484,6 +554,132 @@ class Test extends \Tests\TestCase
             ->call('runValidation')
             ->assertHasNoErrors('customCollection.0.amount')
             ;
+    }
+}
+
+class ComponentWithRulesProperty extends Component
+{
+    public $foo;
+    public $bar = 'baz';
+    public $baz;
+
+    protected $rules = [
+        'foo' => 'required',
+        'bar' => 'required',
+        'baz.*.foo' => 'numeric',
+    ];
+
+    public function mount()
+    {
+        $this->baz = collect([
+            ['foo' => 'a'],
+            ['foo' => 'b'],
+        ]);
+    }
+
+    public function updatedBar()
+    {
+        $this->validateOnly('bar');
+    }
+
+    public function save()
+    {
+        $this->validate();
+    }
+
+    public function render()
+    {
+        return app('view')->make('null-view');
+    }
+}
+
+class ComponentWithoutRulesProperty extends Component
+{
+    public $foo;
+
+    public function save()
+    {
+        $this->validate();
+    }
+
+    public function render()
+    {
+        return app('view')->make('null-view');
+    }
+}
+
+class FooModelForUniquenessValidation extends Model
+{
+    use Sushi;
+
+    protected $rows = [
+        ['name' => 'foo', 'email' => 'foo@example.com'],
+        ['name' => 'bar', 'email' => 'bar@example.com'],
+    ];
+}
+
+class ComponentWithRulesPropertyAndModelWithUniquenessValidation extends Component
+{
+    public $foo;
+
+    protected $rules = [
+        'foo.name' => 'required|unique:foo-connection.foo_model_for_uniqueness_validations,name',
+    ];
+
+    public function mount()
+    {
+        $this->foo = FooModelForUniquenessValidation::first();
+    }
+
+    public function save()
+    {
+        // Sorry about this chunk of ridiculousness. It's Sushi's fault.
+        $connection = $this->foo::resolveConnection();
+        $db = app('db');
+
+        $connections = invade($db)->connections;
+        $connections['foo-connection'] = $connection;
+        invade($db)->connections = $connections;
+
+        $this->validate();
+    }
+
+    public function render()
+    {
+        return app('view')->make('null-view');
+    }
+}
+
+class ComponentWithRulesPropertyAndModelUniquenessValidationWithIdExceptions extends Component
+{
+    public $foo;
+
+    protected function rules() {
+        return [
+            'foo.email' => 'unique:foo-connection.foo_model_for_uniqueness_validations,email,'.$this->foo->id
+        ];
+    }
+
+    public function mount()
+    {
+        $this->foo = FooModelForUniquenessValidation::first();
+    }
+
+    public function save()
+    {
+        // Sorry about this chunk of ridiculousness. It's Sushi's fault.
+        $connection = $this->foo::resolveConnection();
+        $db = app('db');
+        $connections = invade($db)->connections;
+        $connections['foo-connection'] = $connection;
+        invade($db)->connections = $connections;
+
+        $this->validate();
+    }
+
+    public function render()
+    {
+        return app('view')->make('null-view');
     }
 }
 
