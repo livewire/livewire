62c7c57002dbc995a83c7bef84eb99604a8b2a19
Josh Hanley <josh@hitsystems.com.au>
2022-11-30 11:27:22 +1100

    Move computed properties tests

--- src/Features/SupportComputedProperties/Test.php
+++ src/Features/SupportComputedProperties/Test.php
@@ -2,8 +2,9 @@
 
 namespace Livewire\Features\SupportComputedProperties;
 
-use Tests\TestCase;
 use Livewire\Component;
+use Livewire\Livewire;
+use Tests\TestCase;
 
 class Test extends TestCase
 {
@@ -53,5 +54,123 @@ class Test extends TestCase
             $browser->assertSeeIn('@getter-foo', 'bar');
             $browser->assertSeeIn('@foo', 'bar');
         });
+    }
+    
+    /** @test */
+    public function computed_property_is_accessable_within_blade_view()
+    {
+        Livewire::test(ComputedPropertyStub::class)
+            ->assertSee('foo');
+    }
+
+    /** @test */
+    public function injected_computed_property_is_accessable_within_blade_view()
+    {
+        Livewire::test(InjectedComputedPropertyStub::class)
+            ->assertSee('bar');
+    }
+
+    /** @test */
+    public function computed_property_is_memoized_after_its_accessed()
+    {
+        Livewire::test(MemoizedComputedPropertyStub::class)
+            ->assertSee('int(2)');
+    }
+
+    /** @test */
+    public function isset_is_true_on_existing_computed_property()
+    {
+        Livewire::test(IssetComputedPropertyStub::class)
+            ->assertSee('true');
+    }
+
+    /** @test */
+    public function isset_is_false_on_non_existing_computed_property()
+    {
+        Livewire::test(FalseIssetComputedPropertyStub::class)
+            ->assertSee('false');
+    }
+}
+
+class ComputedPropertyStub extends Component
+{
+    public $upperCasedFoo = 'FOO_BAR';
+
+    public function getFooBarProperty()
+    {
+        return strtolower($this->upperCasedFoo);
+    }
+
+    public function render()
+    {
+        // TODO: Fix broken view
+        return view('var-dump-foo-bar');
+    }
+}
+
+class FooDependency {
+    public $baz = 'bar';
+}
+
+class InjectedComputedPropertyStub extends Component
+{
+    public function getFooBarProperty(FooDependency $foo)
+    {
+        return $foo->baz;
+    }
+
+    public function render()
+    {
+        // TODO: Fix broken view
+        return view('var-dump-foo-bar');
+    }
+}
+
+class MemoizedComputedPropertyStub extends Component
+{
+    public $count = 1;
+
+    public function getFooProperty()
+    {
+        return $this->count += 1;
+    }
+
+    public function render()
+    {
+        // Access foo once here to start the cache.
+        $this->foo;
+
+        // TODO: Fix broken view
+        return view('var-dump-foo');
+    }
+}
+
+class IssetComputedPropertyStub extends Component{
+    public $upperCasedFoo = 'FOO_BAR';
+
+    public function getFooBarProperty()
+    {
+        return strtolower($this->upperCasedFoo);
+    }
+
+    public function render()
+    {
+        // TODO: Fix broken view
+        return view('isset-foo-bar');
+    }
+}
+
+class FalseIssetComputedPropertyStub extends Component{
+    public $upperCasedFoo = 'FOO_BAR';
+
+    public function getFooBarProperty()
+    {
+        return strtolower($this->upperCasedFoo);
+    }
+
+    public function render()
+    {
+        // TODO: Fix broken view
+        return view('isset-foo');
     }
 }
