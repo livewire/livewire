302f70eda10527c900e8d267b97b14a0a8996557
Josh Hanley <josh@hitsystems.com.au>
2022-12-03 10:39:49 +1100

    Move configurable layouts tests

--- src/Features/SupportPageComponents/Test.php
+++ src/Features/SupportPageComponents/Test.php
@@ -9,6 +9,167 @@ use Livewire\Livewire;
 class Test extends \Tests\TestCase
 {
     /** @test */
+    public function uses_standard_app_layout_by_default()
+    {
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTest::class);
+
+        $this
+            ->withoutExceptionHandling()
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertDontSee('baz');
+    }
+
+    /** @test */
+    public function can_configure_a_default_layout()
+    {
+        config()->set('livewire.layout', 'layouts.app-with-baz-hardcoded');
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTest::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertSee('baz');
+    }
+
+    /** @test */
+    public function can_configure_the_default_layout_to_a_class_based_component_layout()
+    {
+        config()->set('livewire.layout', \LegacyTests\AppLayout::class);
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTest::class);
+
+        $this
+            ->withoutExceptionHandling()
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertSee('bar')
+            ->assertDontSee('baz');
+    }
+
+    /** @test */
+    public function can_show_params_with_a_configured_class_based_component_layout()
+    {
+        config()->set('livewire.layout', \LegacyTests\AppLayout::class);
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTestWithCustomParams::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertSee('bar')
+            ->assertSee('baz');
+    }
+
+    /** @test */
+    public function can_set_custom_slot_for_a_configured_class_based_component_layout()
+    {
+        config()->set('livewire.layout', \LegacyTests\AppLayout::class);
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTestWithCustomSlot::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertSee('bar');
+    }
+
+    /** @test */
+    public function can_configure_the_default_layout_to_an_anonymous_component_layout()
+    {
+        config()->set('livewire.layout', 'layouts.app-anonymous-component');
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTest::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertSee('bar')
+            ->assertDontSee('baz');
+    }
+
+    /** @test */
+    public function can_show_params_with_a_configured_anonymous_component_layout()
+    {
+        config()->set('livewire.layout', 'layouts.app-anonymous-component');
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTestWithCustomParams::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertSee('bar')
+            ->assertSee('baz');
+    }
+
+    /** @test */
+    public function can_set_custom_slot_for_a_configured_anonymous_component_layout()
+    {
+        config()->set('livewire.layout', 'layouts.app-anonymous-component');
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTestWithCustomSlot::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertSee('bar');
+    }
+
+    /** @test */
+    public function can_show_params_with_a_configured_anonymous_component_layout_that_has_a_required_prop()
+    {
+        config()->set('livewire.layout', 'layouts.app-anonymous-component-with-required-prop');
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTestWithCustomParams::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertSee('bar')
+            ->assertSee('baz');
+    }
+
+    /** @test */
+    public function can_override_optional_props_with_a_configured_anonymous_component_layout()
+    {
+        config()->set('livewire.layout', 'layouts.app-anonymous-component');
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTestWithCustomFooParam::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('foo')
+            ->assertDontSee('bar')
+            ->assertSee('baz');
+    }
+
+    /** @test */
+    public function can_pass_attributes_to_a_configured_class_based_component_layout()
+    {
+        config()->set('livewire.layout', \LegacyTests\AppLayout::class);
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTestWithCustomAttributes::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('class="foo"', false)
+            ->assertSee('id="foo"', false);
+    }
+
+    /** @test */
+    public function can_pass_attributes_to_a_configured_anonymous_component_layout()
+    {
+        config()->set('livewire.layout', 'layouts.app-anonymous-component');
+
+        Route::get('/configurable-layout', ComponentForConfigurableLayoutTestWithCustomAttributes::class);
+
+        $this
+            ->get('/configurable-layout')
+            ->assertSee('class="foo"', false);
+    }
+
+    /** @test */
     public function can_extend_a_blade_layout()
     {
         $this->withoutExceptionHandling();
@@ -131,6 +292,65 @@ class Test extends \Tests\TestCase
             ->assertSee('livewire');
     }
 }
+
+class ComponentForConfigurableLayoutTest extends Component
+{
+    public $name = 'foo';
+
+    public function render()
+    {
+        return view('show-name');
+    }
+}
+
+class ComponentForConfigurableLayoutTestWithCustomParams extends Component
+{
+    public $name = 'foo';
+
+    public function render()
+    {
+        return view('show-name')->layoutData([
+            'bar' => 'baz',
+        ]);
+    }
+}
+
+class ComponentForConfigurableLayoutTestWithCustomSlot extends Component
+{
+    public $name = 'foo';
+
+    public function render()
+    {
+        return view('show-name')->slot('bar');
+    }
+}
+
+class ComponentForConfigurableLayoutTestWithCustomFooParam extends Component
+{
+    public $name = 'foo';
+
+    public function render()
+    {
+        return view('show-name')->layoutData([
+            'foo' => 'baz',
+        ]);
+    }
+}
+
+class ComponentForConfigurableLayoutTestWithCustomAttributes extends Component
+{
+    public $name = 'foo';
+
+    public function render()
+    {
+        return view('show-name')->layoutData([
+            'attributes' => [
+                'class' => 'foo',
+            ]
+        ]);
+    }
+}
+
 
 class ComponentWithExtendsLayout extends Component
 {
