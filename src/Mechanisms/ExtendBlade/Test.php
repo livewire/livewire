5b3e40447bb584191000f60d12eca3af3801f641
Josh Hanley <josh@hitsystems.com.au>
2022-12-06 11:03:29 +1100

    Move blade property binding tests

--- src/Mechanisms/ExtendBlade/Test.php
+++ src/Mechanisms/ExtendBlade/Test.php
@@ -66,6 +66,34 @@ class Test extends \Tests\TestCase
             ->assertDontSee('@this')
             ->assertSee('window.livewire.find(');
     }
+
+    /** @test */
+    public function public_property_is_accessible_in_view_via_this()
+    {
+        Livewire::test(PublicPropertiesInViewWithThisStub::class)
+            ->assertSee('Caleb');
+    }
+
+    /** @test */
+    public function public_properties_are_accessible_in_view_without_this()
+    {
+        Livewire::test(PublicPropertiesInViewWithoutThisStub::class)
+            ->assertSee('Caleb');
+    }
+
+    /** @test */
+    public function protected_property_is_accessible_in_view_via_this()
+    {
+        Livewire::test(ProtectedPropertiesInViewWithThisStub::class)
+            ->assertSee('Caleb');
+    }
+
+    /** @test */
+    public function protected_properties_are_not_accessible_in_view_without_this()
+    {
+        Livewire::test(ProtectedPropertiesInViewWithoutThisStub::class)
+            ->assertDontSee('Caleb');
+    }
 }
 
 class ExtendBladeTestComponent extends Component
@@ -97,5 +125,46 @@ class ComponentForTestingNestedThisDirective extends Component
     public function render()
     {
         return "<div>@component('this-directive')@endcomponent</div>";
+    }
+}
+
+class PublicPropertiesInViewWithThisStub extends Component
+{
+    public $name = 'Caleb';
+
+    public function render()
+    {
+        return app('view')->make('show-name-with-this');
+    }
+}
+
+class PublicPropertiesInViewWithoutThisStub extends Component
+{
+    public $name = 'Caleb';
+
+    public function render()
+    {
+        return app('view')->make('show-name');
+    }
+}
+
+
+class ProtectedPropertiesInViewWithThisStub extends Component
+{
+    protected $name = 'Caleb';
+
+    public function render()
+    {
+        return app('view')->make('show-name-with-this');
+    }
+}
+
+class ProtectedPropertiesInViewWithoutThisStub extends Component
+{
+    protected $name = 'Caleb';
+
+    public function render()
+    {
+        return app('view')->make('show-name');
     }
 }
