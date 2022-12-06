0c503d956e8a45ff49488211cc0178f6435eb53d
Josh Hanley <josh@hitsystems.com.au>
2022-12-07 09:20:43 +1100

    Move request middleware tests

--- src/Mechanisms/UpdateComponents/Test.php
+++ src/Mechanisms/UpdateComponents/Test.php
@@ -26,6 +26,63 @@ class Test extends \Tests\TestCase
         $this->post('laravel', ['name' => '    aaa    '])
         ->assertSee('laravelaaalaravel');
     }
+    /** @test */
+    public function livewire_request_data_doesnt_get_manipulated()
+    {
+        // This test is better done as a Laravel dusk test now.
+        $this->markTestSkipped();
+
+        // LivewireManager::$isLivewireRequestTestingOverride = true;
+
+        // $this->refreshApplication();
+
+        // $component = app(LivewireManager::class)->test(ComponentWithStringPropertiesStub::class);
+
+        // $this->withHeader('X-Livewire', 'true')->post("/livewire/message/{$component->componentName}", [
+        //     'updateQueue' => [],
+        //     'name' => $component->componentName,
+        //     'children' => $component->payload['children'],
+        //     'data' => $component->payload['data'],
+        //     'meta' => $component->payload['meta'],
+        //     'id' => $component->payload['id'],
+        //     'checksum' => $component->payload['checksum'],
+        //     'locale' => $component->payload['locale'],
+        //     'fromPrefetch' => [],
+        // ])->assertJson(['data' => [
+        //     'emptyString' => '',
+        //     'oneSpace' => ' ',
+        // ]]);
+
+        // LivewireManager::$isLivewireRequestTestingOverride = null;
+
+        // $this->refreshApplication();
+    }
+
+    /** @test */
+    public function non_livewire_requests_do_get_manipulated()
+    {
+        // This test is better done as a Laravel dusk test now.
+        $this->markTestSkipped();
+
+        // $this->expectException(CorruptComponentPayloadException::class);
+
+        // $component = app(LivewireManager::class)->test(ComponentWithStringPropertiesStub::class);
+
+        // $this->withMiddleware()->post("/livewire/message/{$component->componentName}", [
+        //     'updateQueue' => [],
+        //     'name' => $component->componentName,
+        //     'children' => $component->payload['children'],
+        //     'data' => $component->payload['data'],
+        //     'meta' => $component->payload['meta'],
+        //     'id' => $component->payload['id'],
+        //     'checksum' => $component->payload['checksum'],
+        //     'locale' => $component->payload['locale'],
+        //     'fromPrefetch' => [],
+        // ])->assertJson(['data' => [
+        //     'emptyString' => null,
+        //     'oneSpace' => null,
+        // ]]);
+    }
 }
 
 class BasicComponent extends Component
@@ -35,5 +92,16 @@ class BasicComponent extends Component
     public function render()
     {
         return view('null-view');
+    }
+}
+
+class ComponentWithStringPropertiesStub extends Component
+{
+    public $emptyString = '';
+    public $oneSpace = ' ';
+
+    public function render()
+    {
+        return app('view')->make('null-view');
     }
 }
