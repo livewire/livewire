3f06378c2ff1eb722d96c6c628c395ab60327ae7
Josh Hanley <josh@hitsystems.com.au>
2022-12-07 09:18:05 +1100

    Move route registration tests

--- src/Features/SupportPageComponents/Test.php
+++ src/Features/SupportPageComponents/Test.php
@@ -324,6 +324,41 @@ PHP;
 
         $this->get('/awesome-js/jquery')->assertRedirect('/awesome-js/alpine');
     }
+
+    /** @test */
+    public function can_pass_parameters_to_a_layout_file()
+    {
+        Livewire::component(ComponentForRouteRegistration::class);
+
+        Route::get('/foo', ComponentForRouteRegistration::class);
+
+        $this->withoutExceptionHandling()->get('/foo')->assertSee('baz');
+    }
+
+    /** @test */
+    public function can_handle_requests_after_application_is_created()
+    {
+        Livewire::component(ComponentForRouteRegistration::class);
+
+        Route::get('/foo', ComponentForRouteRegistration::class);
+
+        // After application is created,
+        // request()->route() is null
+        $this->createApplication();
+
+        $this->withoutExceptionHandling()->get('/foo')->assertSee('baz');
+    }
+
+    /** @test */
+    public function component_uses_alias_instead_of_full_name_if_registered()
+    {
+        Livewire::component('component-alias', ComponentForRouteRegistration::class);
+
+        Route::get('/foo', ComponentForRouteRegistration::class);
+
+        $this->withoutExceptionHandling()->get('/foo')
+            ->assertSee('component-alias');
+    }
 }
 
 class ComponentForConfigurableLayoutTest extends Component
@@ -507,3 +542,14 @@ class FrameworkModel extends Model
     }
 }
 
+class ComponentForRouteRegistration extends Component
+{
+    public $name = 'bar';
+
+    public function render()
+    {
+        return view('show-name')->layout('layouts.app-with-bar', [
+            'bar' => 'baz',
+        ]);
+    }
+}
