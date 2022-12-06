c7ab01ba792aababcf67942250a03c0bbce07205
Josh Hanley <josh@hitsystems.com.au>
2022-12-07 10:00:23 +1100

    Refactor property bindings test stubs to be in test file now that types are supported in the minimum php version Livewire supports

--- src/Features/SupportModels/Tests/ComponentTypedPropertyBindingsTest.php
+++ src/Features/SupportModels/Tests/ComponentTypedPropertyBindingsTest.php
@@ -2,24 +2,13 @@
 
 namespace Livewire\Features\SupportModels;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Route;
+use Livewire\Component;
 use Livewire\Livewire;
 
 class ComponentTypedPropertyBindingsTest extends \Tests\TestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        if (PHP_VERSION_ID < 70400) {
-            $this->markTestSkipped('Only applies to PHP 7.4 and above.');
-            return;
-        }
-
-        // Only load the stubs if we're on PHP 7.4 or above, as the syntax is invalid otherwise
-        require_once __DIR__.'/ComponentPropertyBindingsStubs.php';
-    }
-
     /** @test */
     public function props_are_set_via_mount()
     {
@@ -60,5 +49,95 @@ class ComponentTypedPropertyBindingsTest extends \Tests\TestCase
         // In the case that a parent is a public property, and a child is injected via mount(),
         // the result will *not* resolve via the relationship (it's super edge-case and makes everything terrible)
         $this->withoutExceptionHandling()->get('/foo/parent-model/child/child-model')->assertSeeText('via-route:parent-model:via-route:child-model');
+    }
+}
+
+class PropBoundModel extends Model
+{
+    public $value;
+
+    public function __construct($value = 'model-default')
+    {
+        $this->value = $value;
+    }
+
+    public function resolveRouteBinding($value, $field = null)
+    {
+        $this->value = "via-route:$value";
+        return $this;
+    }
+
+    public function resolveChildRouteBinding($childType, $value, $field)
+    {
+        return new static("via-parent:$value");
+    }
+}
+
+class ComponentWithPropBindings extends Component
+{
+    public PropBoundModel $model;
+
+    public $name;
+
+    public function render()
+    {
+        $this->name = 'prop:'.$this->model->value;
+
+        return app('view')->make('show-name-with-this');
+    }
+}
+
+class ComponentWithDependentPropBindings extends Component
+{
+    public PropBoundModel $parent;
+
+    public PropBoundModel $child;
+
+    public $name;
+
+    public function render()
+    {
+        $this->name = collect(['prop', $this->parent->value, $this->child->value])->implode(':');
+
+        return app('view')->make('show-name-with-this');
+    }
+}
+
+class ComponentWithPropBindingsAndMountMethod extends Component
+{
+    public PropBoundModel $child;
+
+    public $parent;
+
+    public function mount(PropBoundModel $parent)
+    {
+        $this->parent = $parent;
+    }
+
+    public function render()
+    {
+        $this->name = "{$this->parent->value}:{$this->child->value}";
+
+        return app('view')->make('show-name-with-this');
+    }
+}
+
+class ComponentWithDependentMountBindings extends Component
+{
+    public $parent;
+    public $child;
+    public $name;
+
+    public function mount(PropBoundModel $parent, PropBoundModel $child)
+    {
+        $this->parent = $parent;
+        $this->child = $child;
+    }
+
+    public function render()
+    {
+        $this->name = collect(['prop', $this->parent->value, $this->child->value])->implode(':');
+
+        return app('view')->make('show-name-with-this');
     }
 }
