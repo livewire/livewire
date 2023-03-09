0b51648ccb84422e0d10fd88d3801d0bf4a78c94
Josh Hanley <josh@hitsystems.com.au>
2023-03-09 14:44:37 +1100

    Extract middleware index transformation methods to dedicated class

--- src/Mechanisms/PersistentMiddleware/PersistentMiddleware.php
+++ src/Mechanisms/PersistentMiddleware/PersistentMiddleware.php
@@ -5,9 +5,6 @@ namespace Livewire\Mechanisms\PersistentMiddleware;
 use function Livewire\on;
 use Illuminate\Pipeline\Pipeline;
 
-use Illuminate\Support\Str;
-use Livewire\Mechanisms\HandleRequests\HandleRequests;
-
 class PersistentMiddleware
 {
     protected $persistentMiddleware = [
@@ -21,19 +18,19 @@ class PersistentMiddleware
         \App\Http\Middleware\Authenticate::class,
     ];
 
-    protected $componentMiddleware = [];
+    protected $middlewareTransformer;
 
     function boot()
     {
-        app()->singleton($this::class);
+        app()->singleton($this::class, fn() => $this);
+
+        $this->middlewareTransformer = new MiddlewareByIndexTransformer;
 
         on('dehydrate', function($component, $context) {
-            $middleware = app($this::class)->getFilteredMiddlewareIndexes(request());
-
-            $context->addMemo('middleware', $middleware);
+            $this->middlewareTransformer->addDataToContext($context, request());
         });
 
-        on('flush-state', app($this::class)->flushState(...));
+        on('flush-state', $this->flushState(...));
     }
 
     function addPersistentMiddleware($middleware)
@@ -51,110 +48,24 @@ class PersistentMiddleware
         return $this->persistentMiddleware;
     }
 
-    function runRequestThroughMiddleware($request, $components, $handle)
+    function runRequestThroughMiddleware($request, $componentsData, $handle)
     {
         // Assign to class property so it can be used in dehydration and dynamic child components
-        $this->componentMiddleware = $this->getMiddlewareFromComponentsData($components);
+        $middleware = $this->middlewareTransformer->getMiddlewareFromComponentsData($componentsData);
 
         // Only send through pipeline if there are middleware found
-        if (is_null($this->componentMiddleware)) return $handle($components);
+        if (is_null($middleware)) return $handle($componentsData);
 
         return (new Pipeline(app()))
             ->send($request)
-            ->through($this->componentMiddleware)
-            ->then(function() use ($handle, $components) {
-                return $handle($components);
+            ->through($middleware)
+            ->then(function() use ($handle, $componentsData) {
+                return $handle($componentsData);
             });
     }
 
     function flushState()
     {
-        $this->componentMiddleware = [];
-    }
-
-    protected function getFilteredMiddlewareIndexes($request)
-    {
-        $middleware = $this->componentMiddleware;
-
-        if (empty($middleware)) {
-            $middleware = $this->getInitialRouteMiddleware($request);
-        }
-
-        return $this->convertMiddlewareToIndexes($middleware);
-    }
-
-    protected function getInitialRouteMiddleware($request)
-    {
-        if (app(HandleRequests::class)->isDefinitelyLivewireRequest()) return [];
-
-        $initialRoute = $request->route();
-
-        if (is_null($initialRoute)) return [];
-
-        // Use Laravel to convert string middleware, such as 'auth' to classes
-        return app('router')->gatherRouteMiddleware($initialRoute);
-    }
-
-    protected function getMiddlewareFromComponentsData($components)
-    {
-        $firstGroupOfMiddleware = [];
-
-        foreach ($components as $component) {
-            // If a component doesn't have middleware, then skip
-            if (! isset($component['snapshot']['memo']['middleware'])) continue;
-
-            // Store the middleware for the first component that has it
-            if (empty($firstGroupOfMiddleware)) {
-                $firstGroupOfMiddleware = $component['snapshot']['memo']['middleware'];
-                
-                continue;
-            }
-
-            // Check middleware from other components match and throw exception if not
-            if ($firstGroupOfMiddleware != $component['snapshot']['memo']['middleware']) {
-                throw new \Exception('Something went wrong, middleware are different!');
-            }
-        }
-
-        return $this->convertIndexesToMiddleware($firstGroupOfMiddleware);
-    }
-
-    protected function convertMiddlewareToIndexes($middleware) {
-        $middleware = collect($middleware);
-
-        $persistentMiddleware = collect($this->getPersistentMiddleware());
-
-        return $persistentMiddleware
-            ->filter(function ($value, $key) use ($middleware) {
-                
-                return $middleware->contains(function($iValue, $iKey) use ($value) {
-                    $iValue = Str::before($iValue, ':');
-
-                    if ($iValue == $value) return true;
-
-                    if (
-                        class_exists($value) 
-                        && class_exists($iValue)
-                        && is_subclass_of($iValue, $value)
-                    ) return true;
-
-                    return false;
-                });
-            })
-            ->keys()
-            ->all();
-    }
-
-    protected function convertIndexesToMiddleware($middlewareIndexes) {
-        $middlewareIndexes = collect($middlewareIndexes);
-        
-        $persistentMiddleware = collect($this->getPersistentMiddleware());
-
-        return $persistentMiddleware
-            ->filter(function($value, $key) use ($middlewareIndexes) {
-                return $middlewareIndexes->contains($key);
-            })
-            ->values()
-            ->all();
+        $this->middlewareTransformer = null;
     }
 }
