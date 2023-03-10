979e1d2475ac8ec80ae9ecff630ce2aa460291d3
Josh Hanley <josh@hitsystems.com.au>
2023-03-10 16:03:01 +1100

    Refactor transformer

--- src/Mechanisms/PersistentMiddleware/MiddlewareByPathAndMethodTransformer.php
+++ src/Mechanisms/PersistentMiddleware/MiddlewareByPathAndMethodTransformer.php
@@ -4,62 +4,27 @@ namespace Livewire\Mechanisms\PersistentMiddleware;
 
 use Illuminate\Support\Str;
 use Livewire\Mechanisms\HandleRequests\HandleRequests;
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
-
-use function Livewire\invade;
 
 class MiddlewareByPathAndMethodTransformer
 {
     protected $path;
     protected $method;
-    protected $originalRoute;
     protected $fakeRequest;
+    protected $originalRoute;
 
     public function getMiddlewareFromComponentsData($componentsData)
     {
-        $this->processPathAndMethodFromComponentsData($componentsData);
+        [$this->path, $this->method] = $this->getPathAndMethodFromComponentsData($componentsData);
 
-        $middleware = $this->getMiddlewareFromPathAndMethod();
+        $this->fakeRequest = $this->makeFakeRequestFromPathAndMethod();
 
-        return $middleware;
-    }
+        $this->originalRoute = $this->getOriginalRouteFromFakeRequest();
 
-    public function makeRequestFromData()
-    {
-        $originalPath = $this->formatPath($this->path);
-        $originalMethod = $this->method;
-        $currentPath = $this->formatPath(request()->path());
+        if (! $this->originalRoute) return;
 
-        $serverBag = clone request()->server;
+        $originalMiddleware = $this->getMiddlewareFromOriginalRoute();
 
-        $serverBag->set(
-            'REQUEST_URI',
-            str_replace($currentPath, $originalPath, $serverBag->get('REQUEST_URI'))
-        );
-
-        $serverBag->set('REQUEST_METHOD', $originalMethod);
-
-        $request = request()->duplicate(
-            server: $serverBag->all()
-        );
-
-        ray('newRequest', $request);
-
-        ray('session', $request->hasSession());
-
-        // ray('routeContainer', invade($this->originalRoute)->container);
-        // ray(app());
-
-        ray('requestResolver', $request);
-        $route = app('router')->getRoutes()->match($request);
-
-        $this->originalRoute = $route;
-
-        $request->setRouteResolver(fn() => $route);
-        ray('requestResolverAfter', $request);
-        ray($route);
-
-        $this->fakeRequest = $request;
+        return $this->filterMiddlewareByPersistentMiddleware($originalMiddleware);
     }
 
     public function getRequest()
@@ -69,18 +34,21 @@ class MiddlewareByPathAndMethodTransformer
 
     public function addDataToContext($context, $request)
     {
-        ray('originalRequest', request(), request()->route());
         [$path, $method] = $this->getPathAndMethod($request);
 
         $context->addMemo('path', $path);
         $context->addMemo('method', $method);
     }
 
-    protected function processPathAndMethodFromComponentsData($componentsData)
+    protected function getPathAndMethodFromComponentsData($componentsData)
     {
         $path = null;
         $method = null;
 
+        /**
+         * Iterate through each of the components in the request to find the path
+         * and method, and ensure that they're the same for each component.
+         */
         foreach ($componentsData as $component) {
             // If a component doesn't have path or method, then skip
             if (
@@ -88,7 +56,7 @@ class MiddlewareByPathAndMethodTransformer
                 || ! isset($component['snapshot']['memo']['method'])
             ) continue;
 
-            // Store the middleware for the first component that has it
+            // Store the path and method from the first component that has them
             if (empty($path) && empty($method)) {
                 $path = $component['snapshot']['memo']['path'];
                 $method = $component['snapshot']['memo']['method'];
@@ -96,18 +64,85 @@ class MiddlewareByPathAndMethodTransformer
                 continue;
             }
 
-            // Check path and method from other components match and throw exception if not
+            // Check path and method from other components match and throw an exception if not
             if (
                 $path != $component['snapshot']['memo']['path']
                 || $path != $component['snapshot']['memo']['method']
             ) {
-                throw new \Exception('Something went wrong, middleware are different!');
+                throw new \Exception('Something went wrong, path and method are not the same for all the components in this request,');
             }
         }
+        
+        return [$path, $method];
+    }
 
-        // Store for later, so it can be added to the context on dehydration.
-        $this->path = $path;
-        $this->method = $method;
+    protected function makeFakeRequestFromPathAndMethod()
+    {
+        $originalPath = $this->formatPath($this->path);
+        $originalMethod = $this->method;
+
+        $currentPath = $this->formatPath(request()->path());
+
+        // Clone server bag to ensure changes below don't overwrite the original.
+        $serverBag = clone request()->server;
+
+        // Replace the Livewire endpoint path with the path from the original request.
+        $serverBag->set(
+            'REQUEST_URI',
+            str_replace($currentPath, $originalPath, $serverBag->get('REQUEST_URI'))
+        );
+
+        $serverBag->set('REQUEST_METHOD', $originalMethod);
+
+        /**
+         * Make the fake request from the current request with path and method changed so
+         * all other request data, such as headers, are available in the fake request,
+         * but merge in the new server bag with the updated `REQUEST_URI`.
+         */
+        $request = request()->duplicate(
+            server: $serverBag->all()
+        );
+
+        return $request;
+    }
+
+    protected function formatPath($path)
+    {
+        return '/' . ltrim($path, '/');
+    }
+
+    protected function getOriginalRouteFromFakeRequest()
+    {
+        $route = app('router')->getRoutes()->match($this->fakeRequest);
+
+        $this->fakeRequest->setRouteResolver(fn() => $route);
+
+        return $route;
+    }
+
+    protected function getMiddlewareFromOriginalRoute()
+    {
+        return app('router')->gatherRouteMiddleware($this->originalRoute);
+    }
+
+    protected function filterMiddlewareByPersistentMiddleware($middleware)
+    {
+        $middleware = collect($middleware);
+
+        $persistentMiddleware = collect(app(PersistentMiddleware::class)->getPersistentMiddleware());
+
+        return $middleware
+            ->filter(function ($value, $key) use ($persistentMiddleware) {
+                return $persistentMiddleware->contains(function($iValue, $iKey) use ($value) {
+                    // Some middlewares can be closures.
+                    if (! is_string($value)) return false;
+
+                    // Ensure any middleware arguments aren't included in the comparison
+                    return Str::before($value, ':') == $iValue;
+                });
+            })
+            ->values()
+            ->all();
     }
 
     protected function getPathAndMethod()
@@ -120,67 +155,5 @@ class MiddlewareByPathAndMethodTransformer
         $method = request()->method();
 
         return [$path, $method];
-    }
-
-    protected function getMiddlewareFromPathAndMethod()
-    {
-        $this->makeRequestFromData();
-        // $this->originalRoute = $this->getRouteFromPathAndMethod();
-
-        if (! $this->originalRoute) return;
-
-        $originalMiddleware = app('router')->gatherRouteMiddleware($this->originalRoute);
-
-        return $this->getFilteredMiddleware($originalMiddleware);
-    }
-
-    // protected function getRouteFromPathAndMethod()
-    // {
-    //     $routes = collect(app('router')->getRoutes()->get($this->method));
-
-    //     [$fallbacks, $routes] = $routes->partition(function ($route) {
-    //         return $route->isFallback;
-    //     });
-
-    //     return $routes->merge($fallbacks)->first(
-    //         fn ($route) => $this->pathMatchesRoute($this->path, $route)
-    //     );
-    // }
-
-    // protected function pathMatchesRoute($path, $route)
-    // {
-    //     $path = rtrim($path, '/') ?: '/';
-
-    //     $path = '/' . ltrim($path, '/');
-
-    //     invade($route)->compileRoute();
-
-    //     return preg_match($route->getCompiled()->getRegex(), rawurldecode($path));
-    // }
-
-    protected function formatPath($uri)
-    {
-        return '/' . ltrim($uri, '/');
-    }
-
-    protected function getFilteredMiddleware($middleware)
-    {
-        $middleware = collect($middleware);
-
-        $persistentMiddleware = collect(app(PersistentMiddleware::class)->getPersistentMiddleware());
-
-        ray('getFilteredMiddleware', $middleware, $persistentMiddleware);
-
-        return ray()->pass($middleware
-            ->filter(function ($value, $key) use ($persistentMiddleware) {
-                return $persistentMiddleware->contains(function($iValue, $iKey) use ($value) {
-                    // Some middlewares can be closures.
-                    if (! is_string($value)) return false;
-
-                    return Str::before($value, ':') == $iValue;
-                });
-            })
-            ->values()
-            ->all());
     }
 }
