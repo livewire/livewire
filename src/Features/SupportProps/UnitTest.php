5051384f45ad78a33bb706fbbdbe9dc60fbf2c0e
Nuno Maduro <enunomaduro@gmail.com>
2023-06-26 21:09:21 +0100

    Adds failing test

--- src/Features/SupportProps/UnitTest.php
+++ src/Features/SupportProps/UnitTest.php
@@ -32,7 +32,28 @@ class UnitTest extends \Tests\TestCase
     /** @test */
     function can_change_reactive_prop_in_child_component()
     {
-        // @todo - Nuno, can you write a failing test here...
+        $component = Livewire::test([new class extends Component {
+            public $todos = [];
+
+            public function render() {
+                return '<livewire:child :todos="$todos" />';
+            }
+        }, 'child' => new class extends Component {
+            #[Prop(reactive: true)]
+            public $todos;
+
+            public function render() {
+                return '<div>Count: {{ count($todos) }}.</div>';
+            }
+        }]);
+
+        $component->assertSee('Count: 0.');
+
+        $component->set('todos', ['todo 1']);
+        $component->assertSee('Count: 1.');
+
+        $component->set('todos', ['todo 1', 'todo 2', 'todo 3']);
+        $component->assertSee('Count: 3.');
     }
 }
 
