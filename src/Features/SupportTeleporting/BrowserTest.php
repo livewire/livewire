89065e9bafea2bd6fc6b881c16bc2c77b0b9d4c1
Dan Harrin <git@danharrin.com>
2023-06-29 09:46:47 +0100

    failing test: Updating teleported content

--- src/Features/SupportTeleporting/BrowserTest.php
+++ src/Features/SupportTeleporting/BrowserTest.php
@@ -25,4 +25,33 @@ class BrowserTest extends BrowserTestCase
             HTML; }
         })->assertSeeIn('#foo', 'bar');
     }
+
+    /** @test */
+    public function can_teleport_dom_via_blade_directive_then_change_it()
+    {
+        Livewire::visit(new class extends Component {
+            public $foo = 'bar';
+
+            public function setFoo()
+            {
+                $this->foo = 'baz';
+            }
+
+            public function render() { return <<<'HTML'
+            <div>
+                <div>
+                    <span id="foo"></span>
+                </div>
+
+                <button dusk="setFoo" type="button" wire:click="setFoo">
+                    Set foo
+                </button>
+
+                @teleport('#foo')
+                    <span>{{ $foo }}</span>
+                @endteleport
+            </div>
+            HTML; }
+        })->assertSeeIn('#foo', 'bar')->click('@setFoo')->assertSeeIn('#foo', 'baz');
+    }
 }
