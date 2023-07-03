57def1295a0db9c8edec8eefe00b46c839dcedcb
Philo Hermans <me@philo.dev>
2023-07-03 16:29:22 +0200

    Improved input prompts for missing arguments

--- src/Features/SupportConsoleCommands/Commands/FormCommand.php
+++ src/Features/SupportConsoleCommands/Commands/FormCommand.php
@@ -5,13 +5,38 @@ namespace Livewire\Features\SupportConsoleCommands\Commands;
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class FormCommand extends GeneratorCommand
 {
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
     protected $signature = 'livewire:form {name} {--model=} {--force}';
 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'Create a new Livewire form class';
 
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Form';
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
     protected function buildClass($name)
     {
         $stub = parent::buildClass($name);
@@ -21,11 +46,22 @@ class FormCommand extends GeneratorCommand
         return $this->replaceModel($stub, $model);
     }
 
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
     public function getStub()
     {
         return __DIR__ . '/livewire.form.stub';
     }
 
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
     public function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace . '\Forms';
@@ -54,5 +90,29 @@ class FormCommand extends GeneratorCommand
         $stub = str_replace("{createExample}", $model.'::create($this->all());', $stub);
 
         return $stub;
+    }
+
+    /**
+     * Interact further with the user if they were prompted for missing arguments.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    protected function afterPromptingForMissingArguments(InputInterface $input, OutputInterface $output)
+    {
+        if ($this->isReservedName($this->getNameInput()) || $this->didReceiveOptions($input)) {
+            return;
+        }
+
+        $model = $this->components->askWithCompletion(
+            'What model should this form apply to?',
+            $this->possibleModels(),
+            'none'
+        );
+
+        if ($model && $model !== 'none') {
+            $input->setOption('model', $model);
+        }
     }
 }
