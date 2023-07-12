63db80d6161eda331e71582f55b0fbf93a46f39e
Caleb Porzio <calebporzio@gmail.com>
2023-07-12 17:23:48 -0400

    Allow $this->js('$wire');

--- dist/livewire.js
+++ dist/livewire.js
@@ -7956,15 +7956,15 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     if (js) {
       Object.entries(js).forEach(([method, body]) => {
         overrideMethod(component, method, () => {
-          let func = new Function([], body);
-          func.bind(component.$wire)();
+          let func = new Function(["$wire"], body);
+          func.bind(component.$wire)(component.$wire);
         });
       });
     }
     if (xjs) {
       xjs.forEach((expression) => {
-        let func = new Function([], expression);
-        func.bind(component.$wire)();
+        let func = new Function(["$wire"], expression);
+        func.bind(component.$wire)(component.$wire);
       });
     }
   });
--- src/Features/SupportJsEvaluation/BrowserTest.php
+++ src/Features/SupportJsEvaluation/BrowserTest.php
@@ -70,4 +70,32 @@ class BrowserTest extends \Tests\BrowserTestCase
         ->assertSee('Toggle Me!')
         ;
     }
+
+    /** @test */
+    public function can_use_dollar_sign_wire_instead_of_this()
+    {
+        Livewire::visit(
+            new class extends \Livewire\Component {
+                public $show = false;
+
+                function toggle()
+                {
+                    $this->js('$wire.show = true');
+                }
+
+                public function render() { return <<<'HTML'
+                <div>
+                    <button wire:click="toggle" dusk="toggle">Toggle</button>
+
+                    <div dusk="target" x-show="$wire.show">
+                        Toggle Me!
+                    </div>
+                </div>
+                HTML; }
+        })
+        ->assertDontSee('Toggle Me!')
+        ->waitForLivewire()->click('@toggle')
+        ->assertSee('Toggle Me!')
+        ;
+    }
 }
