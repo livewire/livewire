15d241cf84bd6de0c8dfe344fbc08f061ce339fd
luanfreitasdev <luanfreitas10@protonmail.com>
2023-07-22 18:43:33 -0300

    fix order tests

--- src/Features/SupportReactiveProps/BrowserTest.php
+++ src/Features/SupportReactiveProps/BrowserTest.php
@@ -8,53 +8,6 @@ use Livewire\Component;
 class BrowserTest extends \Tests\BrowserTestCase
 {
     /** @test */
-    public function can_throw_exception_cannot_mutate_reactive_prop()
-    {
-        Livewire::visit([
-            new class extends Component {
-                public $count = 0;
-
-                public function inc() { $this->count++; }
-
-                public function render() { return <<<'HTML'
-                    <div>
-                        <h1>Parent count: <span dusk="parent.count">{{ $count }}</span>
-
-                        <button wire:click="inc" dusk="parent.inc">inc</button>
-
-                        <livewire:child :$count />
-                    </div>
-                    HTML;
-                }
-            },
-            'child' => new class extends Component {
-                #[Reactive]
-                public $count;
-
-                public function inc() { $this->count++; }
-
-                public function render() { return <<<'HTML'
-                    <div>
-                        <h1>Child count: <span dusk="child.count">{{ $count }}</span>
-                        <button wire:click="inc" dusk="child.inc">inc</button>
-                    </div>
-                    HTML;
-                }
-            }
-        ])
-        ->assertSeeIn('@parent.count', 0)
-        ->assertSeeIn('@child.count', 0)
-        ->waitForLivewire()->click('@parent.inc')
-        ->assertSeeIn('@parent.count', 1)
-        ->assertSeeIn('@child.count', 1)
-        ->waitForLivewire()->click('@child.inc')
-        ->waitFor('#livewire-error')
-        ->click('#livewire-error')
-        ->assertSeeIn('@parent.count', 1)
-        ->assertSeeIn('@child.count', 1);
-    }
-
-    /** @test */
     public function can_pass_a_reactive_property_from_parent_to_child()
     {
         Livewire::visit([
@@ -116,6 +69,53 @@ class BrowserTest extends \Tests\BrowserTestCase
             ->waitForLivewire()->click('@parent.inc')
             ->assertSeeIn('@parent.count', 0)
             ->assertSeeIn('@child.count', 0);
+    }
+
+    /** @test */
+    public function can_throw_exception_cannot_mutate_reactive_prop()
+    {
+        Livewire::visit([
+            new class extends Component {
+                public $count = 0;
+
+                public function inc() { $this->count++; }
+
+                public function render() { return <<<'HTML'
+                    <div>
+                        <h1>Parent count: <span dusk="parent.count">{{ $count }}</span>
+
+                        <button wire:click="inc" dusk="parent.inc">inc</button>
+
+                        <livewire:child :$count />
+                    </div>
+                    HTML;
+                }
+            },
+            'child' => new class extends Component {
+                #[Reactive]
+                public $count;
+
+                public function inc() { $this->count++; }
+
+                public function render() { return <<<'HTML'
+                    <div>
+                        <h1>Child count: <span dusk="child.count">{{ $count }}</span>
+                        <button wire:click="inc" dusk="child.inc">inc</button>
+                    </div>
+                    HTML;
+                }
+            }
+        ])
+        ->assertSeeIn('@parent.count', 0)
+        ->assertSeeIn('@child.count', 0)
+        ->waitForLivewire()->click('@parent.inc')
+        ->assertSeeIn('@parent.count', 1)
+        ->assertSeeIn('@child.count', 1)
+        ->waitForLivewire()->click('@child.inc')
+        ->waitFor('#livewire-error')
+        ->click('#livewire-error')
+        ->assertSeeIn('@parent.count', 1)
+        ->assertSeeIn('@child.count', 1);
     }
 
     /** @test */
