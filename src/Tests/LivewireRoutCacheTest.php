cba61523c676494576344694d71303d8ef9c4734
Adrian Nürnberger <adrian@nuernberger.me>
2023-07-23 23:23:41 +0200

    add failing test for route caching

---
+++ src/Tests/LivewireRoutCacheTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Livewire\Tests;
+
+use Error;
+use Exception;
+use Illuminate\Routing\Route;
+use Tests\TestCase;
+
+class LivewireRoutCacheTest extends TestCase
+{
+    /** @test */
+    public function livewire_script_route_is_cacheable(): void
+    {
+        $route = $this->getRoute('livewire/livewire.js');
+
+        $this->cacheRoute($route, "Failed to cache route 'livewire/livewire.js'");
+    }
+
+    /** @test */
+    public function livewire_update_route_is_cacheable(): void
+    {
+        $route = $this->getRoute('livewire/update');
+
+        $this->cacheRoute($route, "Failed to cache route 'livewire/update'");
+    }
+
+    protected function getRoute(string $uri): Route
+    {
+        $route = collect(\Illuminate\Support\Facades\Route::getRoutes())
+            ->firstWhere(fn(Route $route) => $route->uri() === $uri);
+
+        if ($route === null) {
+            $this->fail("Route '$uri' not found.");
+        }
+
+        return $route;
+    }
+
+    protected function cacheRoute(Route $route, string $message): void
+    {
+        try {
+            $route->prepareForSerialization();
+        } catch (Error|Exception) {
+            $this->fail($message);
+        }
+
+        $this->assertTrue(true);
+    }
+}
