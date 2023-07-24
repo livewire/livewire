d2f64670b63eb1c53dc1da7cb41a43262447e534
Adrian Nürnberger <adrian@nuernberger.me>
2023-07-24 11:58:18 +0200

    allow multiple listener attributes

--- src/Features/SupportEvents/On.php
+++ src/Features/SupportEvents/On.php
@@ -2,11 +2,12 @@
 
 namespace Livewire\Features\SupportEvents;
 
+use Attribute;
 use Livewire\Features\SupportAttributes\Attribute as LivewireAttribute;
 
 use function Livewire\store;
 
-#[\Attribute]
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
 class On extends LivewireAttribute
 {
     public function __construct(public $event) {}
