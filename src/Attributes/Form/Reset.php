157115c92f7b02b7fcc6536ad45ab2797c38f4cb
AJ Meireles <alvaro.meireles@live.com>
2023-07-26 20:25:59 -0300

    fixing tests by adding Attribute::TARGET_PROPERTY

--- src/Attributes/Form/Reset.php
+++ src/Attributes/Form/Reset.php
@@ -5,7 +5,7 @@ namespace Livewire\Attributes\Form;
 use Attribute;
 use Livewire\Features\SupportFormObjects\Reset as BaseReset;
 
-#[Attribute(Attribute::IS_REPEATABLE)]
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_PROPERTY)]
 class Reset extends BaseReset
 {
     public function __construct(
