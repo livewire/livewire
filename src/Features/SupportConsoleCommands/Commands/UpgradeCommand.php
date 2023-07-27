9bbf4c013de140b78f3893f87a8a7e7242e2ccd6
Philo Hermans <me@philo.dev>
2023-07-27 16:08:24 +0200

    Add simple automations to emit to dispatch changes

--- src/Features/SupportConsoleCommands/Commands/UpgradeCommand.php
+++ src/Features/SupportConsoleCommands/Commands/UpgradeCommand.php
@@ -20,7 +20,7 @@ use Livewire\Features\SupportConsoleCommands\Commands\Upgrade\RepublishNavigatio
 use Livewire\Features\SupportConsoleCommands\Commands\Upgrade\ThirdPartyUpgradeNotice;
 use Livewire\Features\SupportConsoleCommands\Commands\Upgrade\UpgradeAlpineInstructions;
 use Livewire\Features\SupportConsoleCommands\Commands\Upgrade\UpgradeConfigInstructions;
-use Livewire\Features\SupportConsoleCommands\Commands\Upgrade\UpgradeEmitInstructions;
+use Livewire\Features\SupportConsoleCommands\Commands\Upgrade\ReplaceEmitWithDispatch;
 use Livewire\Features\SupportConsoleCommands\Commands\Upgrade\UpgradeIntroduction;
 
 class UpgradeCommand extends Command
@@ -50,13 +50,15 @@ class UpgradeCommand extends Command
             RepublishNavigation::class,
             ChangeTestAssertionMethods::class,
 
+            // Partially automated steps
+            ReplaceEmitWithDispatch::class,
+
             // Third-party steps
             ... static::$thirdPartyUpgradeSteps,
 
             // Manual steps
             UpgradeConfigInstructions::class,
             UpgradeAlpineInstructions::class,
-            UpgradeEmitInstructions::class,
 
             ClearViewCache::class,
         ])->thenReturn();
