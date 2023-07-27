49757cf176b0cb7ff36fc60701c267efd1b14280
Philo Hermans <me@philo.dev>
2023-07-27 13:15:49 +0200

    wip

--- src/Features/SupportConsoleCommands/Commands/Upgrade/ChangeForgetComputedToUnset.php
+++ src/Features/SupportConsoleCommands/Commands/Upgrade/ChangeForgetComputedToUnset.php
@@ -13,8 +13,24 @@ class ChangeForgetComputedToUnset extends UpgradeStep
             title: 'The forgetComputed component method is replaced by PHP\'s unset function',
             before: '$this->forgetComputed(\'title\')',
             after: 'unset($this->title)',
-            pattern: '/\$this->forgetComputed\(((?:[^)(]|\((?:[^)(]|\((?:[^)(]|\([^)(]*\))*\))*\))*)\)/',
-            replacement: 'unset($this->$1)',
+            pattern: '/\$this->forgetComputed\((.*?)\);/',
+            replacement: function($matches) {
+                $replacement = '';
+
+                if(isset($matches[1])) {
+                    preg_match_all('/(?:\'|")(.*?)(?:\'|")/', $matches[1], $keys);
+
+                    $replacement .= 'unset(';
+                    foreach($keys[1] ?? [] as $key) {
+                        $replacement .= '$this->' . $key . ', ';
+                    }
+                    $replacement = rtrim($replacement, ', ');
+                    $replacement .= ');';
+                }
+
+                // Leave unchanged if no replacement was possible.
+                return $replacement ?: $matches[0];
+            },
             directories: 'app',
         );
 
