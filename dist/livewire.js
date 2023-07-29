6376cc968234326d1e5c4453abd6a3bda8045d8f
Caleb Porzio <calebporzio@gmail.com>
2023-07-29 10:50:57 -0400

    Add laravel echo support (#6092)

--- dist/livewire.js
+++ dist/livewire.js
@@ -1829,7 +1829,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
           innerSet(outer);
           inner = outer;
         } else {
-          outerSet(JSON.parse(JSON.stringify(inner)));
+          outerSet(JSON.parse(innerHashLatest ?? null));
           outer = inner;
         }
       }
@@ -6783,7 +6783,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
           innerSet(outer);
           inner = outer;
         } else {
-          outerSet(JSON.parse(JSON.stringify(inner)));
+          outerSet(JSON.parse(innerHashLatest ?? null));
           outer = inner;
         }
       }
@@ -8083,6 +8083,56 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
       return { ...fullerDefaults, ...value };
     }
   }
+
+  // js/features/supportLaravelEcho.js
+  on("effects", (component, effects) => {
+    let listeners2 = effects.listeners || [];
+    listeners2.forEach((event) => {
+      if (event.startsWith("echo")) {
+        if (typeof window.Echo === "undefined") {
+          console.warn("Laravel Echo cannot be found");
+          return;
+        }
+        let event_parts = event.split(/(echo:|echo-)|:|,/);
+        if (event_parts[1] == "echo:") {
+          event_parts.splice(2, 0, "channel", void 0);
+        }
+        if (event_parts[2] == "notification") {
+          event_parts.push(void 0, void 0);
+        }
+        let [
+          s1,
+          signature,
+          channel_type,
+          s2,
+          channel,
+          s3,
+          event_name
+        ] = event_parts;
+        if (["channel", "private", "encryptedPrivate"].includes(channel_type)) {
+          window.Echo[channel_type](channel).listen(event_name, (e) => {
+            dispatchSelf(component, event, e);
+          });
+        } else if (channel_type == "presence") {
+          if (["here", "joining", "leaving"].includes(event_name)) {
+            window.Echo.join(channel)[event_name]((e) => {
+              dispatchSelf(component, event, e);
+            });
+          } else {
+            window.Echo.join(channel).listen(event_name, (e) => {
+              dispatchSelf(component, event, e);
+            });
+          }
+        } else if (channel_type == "notification") {
+          window.Echo.private(channel).notification((notification) => {
+            dispatchSelf(component, event, notification);
+          });
+        } else {
+          console.warn("Echo channel type not yet supported");
+        }
+      }
+    });
+  });
 
   // js/features/supportNavigate.js
   var isNavigating = false;
