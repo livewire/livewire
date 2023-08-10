2254769d5e55ea42c170a7eb3783f0ec0094f08a
Mark Jaquith <mark@jaquith.me>
2023-08-10 00:55:18 -0400

    Add failing test for rendering and updating components with a root <tr> element
    see #6247

---
+++ src/Tests/UpdatingTableRowsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Livewire\Tests;
+
+use Livewire\Component;
+use Livewire\Livewire;
+
+class UpdatingTableRowsTest extends \Tests\BrowserTestCase
+{
+    /** @test */
+    public function component_renders_table_rows_and_updates_properly()
+    {
+        Livewire::visit(new class extends Component {
+            public int $counter = 0;
+
+            public function increment()
+            {
+                $this->counter++;
+            }
+
+            public function render() {
+                return <<<'HTML'
+                    <tr dusk="table-row">
+                        <td>
+                            <button type="button" wire:click="increment" dusk="increment">+</button>
+                        </td>
+                        <td>
+                            <input wire:model="counter" dusk="counter">
+                        </td>
+                    </tr>
+                HTML;
+            }
+        })
+            ->assertVisible('@table-row')
+            ->assertInputValue('@counter', '0')
+            ->click('@increment')
+            ->waitForLivewire()
+            ->assertVisible('@table-row')
+            ->assertInputValue('@counter', '1')
+        ;
+    }
+}
