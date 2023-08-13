35f6b080158d687ffbab850e17c33b615ba1f9c8
edalzell <erin@silentz.co>
2023-08-12 17:18:17 -0700

    failing test

---
+++ src/Features/SupportValidation/BrowserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Livewire\Features\SupportValidation;
+
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\Livewire;
+
+/** @group morphing */
+class BrowserTest extends \Tests\BrowserTestCase
+{
+    /** @test */
+    public function validation_attribute_rules_triggered_on_save()
+    {
+        Livewire::visit(new class extends Component
+        {
+            #[Rule('required', 'min:3')]
+            public string $title = '';
+
+            public function render()
+            {
+                return <<<'HTML'
+                <div>
+                    <input type="text" wire:model="title" dusk="title"/>
+                    <div dusk="error">@error('title') {{ $message }} @enderror</div>
+
+                    <button wire:click="save" dusk="save">Save</button>
+                </div>
+                HTML;
+            }
+
+            public function save()
+            {
+            }
+        })
+            ->type('@title', 'a')
+            ->click('@save')
+            ->assertSeeIn('@error', 'The title field must be at least 3 characters.');
+    }
+}
