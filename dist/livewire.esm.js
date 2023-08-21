2481efd9af471f89959209f7ee024bafe5106a29
Caleb Porzio <calebporzio@gmail.com>
2023-08-21 10:34:15 -0400

    Support Laravel Echo event payloads and fix test

--- dist/livewire.esm.js
+++ dist/livewire.esm.js
@@ -9097,21 +9097,21 @@ on("effects", (component, effects) => {
       ] = event_parts;
       if (["channel", "private", "encryptedPrivate"].includes(channel_type)) {
         window.Echo[channel_type](channel).listen(event_name, (e) => {
-          dispatchSelf(component, event, e);
+          dispatchSelf(component, event, [e]);
         });
       } else if (channel_type == "presence") {
         if (["here", "joining", "leaving"].includes(event_name)) {
           window.Echo.join(channel)[event_name]((e) => {
-            dispatchSelf(component, event, e);
+            dispatchSelf(component, event, [e]);
           });
         } else {
           window.Echo.join(channel).listen(event_name, (e) => {
-            dispatchSelf(component, event, e);
+            dispatchSelf(component, event, [e]);
           });
         }
       } else if (channel_type == "notification") {
         window.Echo.private(channel).notification((notification) => {
-          dispatchSelf(component, event, notification);
+          dispatchSelf(component, event, [notification]);
         });
       } else {
         console.warn("Echo channel type not yet supported");
