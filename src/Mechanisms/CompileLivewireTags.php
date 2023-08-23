05cd6e8e5b3f4158a111418b933bee999f62f098
Adrian Nürnberger <adrian@nuernberger.me>
2023-08-23 19:21:29 +0200

    getClass does no longer exists on LivewireManger

--- src/Mechanisms/CompileLivewireTags.php
+++ src/Mechanisms/CompileLivewireTags.php
@@ -62,11 +62,10 @@ class CompileLivewireTags extends ComponentTagCompiler
             if ($component === 'styles') return '@livewireStyles';
             if ($component === 'scripts') return '@livewireScripts';
             if ($component === 'dynamic-component' || $component === 'is') {
-                    $dynamicComponentExists = rescue(function() use ($component, $attributes) {
-                        // Need to run this in rescue otherwise running this during a test causes Livewire directory not found exception
-                        return $component === 'dynamic-component' && app('livewire')->getClass('dynamic-component');
-                    });
                 if (! isset($attributes['component']) && ! isset($attributes['is'])) {
+                    // Check if 'dynamic-component' or 'is' is an actual livewire component
+                    // Need to run this in rescue otherwise running this during a test causes Livewire directory not found exception
+                    $dynamicComponentExists = rescue(fn() => app(ComponentRegistry::class)->getClass($component));
 
                     if ($dynamicComponentExists) {
                         return $this->componentString("'{$component}'", $attributes);
