29bb54fc7e41fb99608c9ce6abe282ee606a5f6b
Kenneth Sese <ksese@hope4tomorrow.net>
2023-08-25 15:53:39 -0600

    Update BrowserTest.php

--- src/Features/SupportEntangle/BrowserTest.php
+++ src/Features/SupportEntangle/BrowserTest.php
@@ -87,4 +87,114 @@ class BrowserTest extends BrowserTestCase
             ->assertMissing('@item2')
             ->assertMissing('@item3');
     }
+
+    /** @test */
+    public function can_removed_nested_items_without_multiple_requests_when_entangled_items_are_present()
+    {
+        Livewire::visit([
+            new class extends Component {
+                public $components = [];
+
+                public $filters = [
+                    'name' => 'bob',
+                    'phone' => '123',
+                    'address' => 'street',
+                ];
+
+                public $counter = 0;
+
+                public function boot()
+                {
+                    $this->counter++;
+                }
+
+                public function removeFilter($filter)
+                {
+                    $this->filters[$filter] = null;
+                }
+
+                public function addBackFiltersWithEntangled()
+                {
+                    $this->filters = [
+                        'name' => 'bob',
+                        'phone' => '123',
+                        'address' => 'street',
+                        'entangled' => 'hello world', // This filter will be entangled,
+                    ];
+                }
+
+                public function addBackFiltersWithoutEntangled()
+                {
+                    // Add back the same non entangled filters to show that
+                    // removing/adding non entangled items is not the issue.
+                    $this->filters = [
+                        'name' => 'bob',
+                        'phone' => '123',
+                        'address' => 'street',
+                    ];
+                }
+
+                function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <div>Page</div>
+
+                        <div dusk="counter">Boot counter: {{ $counter }}</div>
+
+                        @foreach ($filters as $filter => $value)
+                            @if ($filter === 'entangled')
+                                <div x-data="{ value: $wire.entangle('filters.{{ $filter }}') }">
+                                    <span
+                                        x-text="'Entangled: ' + value"
+                                    ></span>
+                                </div>
+                                <div>
+                                    <button dusk="remove-{{ $filter }}" wire:click="removeFilter('{{ $filter }}')">Remove {{ $filter }}</button>
+                                </div>    
+                            @else
+                                <div>
+                                    Normal: {{ $value }}
+                                </div>
+                                <div>
+                                    <button dusk="remove-{{ $filter }}" wire:click="removeFilter('{{ $filter }}')">Remove {{ $filter }}</button>
+                                </div>
+                            @endif
+                        @endforeach
+
+                        <button dusk="add-entangled-filter" wire:click="addEntangledFilter()">Add entangled filter</button>
+                        <button dusk="add-back-filters-without-entangled" wire:click="addBackFiltersWithoutEntangled()">Add back filters without entangled</button>
+                        <button dusk="add-back-filters-with-entangled" wire:click="addBackFiltersWithEntangled()">Add back filters with entangled</button>
+                    </div>
+                    HTML;
+                }
+            },
+            'first-component' => FirstComponent::class,
+        ]) 
+        ->assertSeeIn('@counter', '1')
+        ->waitForLivewire()->click('@remove-name')
+        ->assertSeeIn('@counter', '2')
+        ->waitForLivewire()->click('@remove-phone')
+        ->assertSeeIn('@counter', '3')
+        ->waitForLivewire()->click('@remove-address')
+        ->assertSeeIn('@counter', '4')
+        ->waitForLivewire()->click('@add-back-filters-without-entangled')
+        ->assertSeeIn('@counter', '5')
+        ->waitForLivewire()->click('@remove-name')
+        ->assertSeeIn('@counter', '6')
+        ->waitForLivewire()->click('@remove-phone')
+        ->assertSeeIn('@counter', '7')
+        ->waitForLivewire()->click('@remove-address')
+        ->assertSeeIn('@counter', '8')
+        ->waitForLivewire()->click('@add-back-filters-with-entangled')
+        ->assertSeeIn('@counter', '9') 
+        ->waitForLivewire()->click('@remove-name')
+        ->assertSeeIn('@counter', '10') // This test will fail here since the boot method will be called twice
+        ->waitForLivewire()->click('@remove-phone')
+        ->assertSeeIn('@counter', '11')
+        ->waitForLivewire()->click('@remove-address')
+        ->assertSeeIn('@counter', '12')
+        ->waitForLivewire()->click('@remove-email')
+        ->assertSeeIn('@counter', '13');
+    }
 }
