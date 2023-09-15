1d31d589b657b45614fd8954f2ad12806c971fae
Simone <simoneungaro@hotmail.it>
2023-09-15 15:18:06 +0200

    add failing test

--- src/Features/SupportDataBinding/BrowserTest.php
+++ src/Features/SupportDataBinding/BrowserTest.php
@@ -2,6 +2,7 @@
 
 namespace Livewire\Features\SupportDataBinding;
 
+use Illuminate\Support\Facades\Blade;
 use Livewire\Component;
 use Livewire\Livewire;
 use Tests\BrowserTestCase;
@@ -34,5 +35,48 @@ class BrowserTest extends BrowserTestCase
             ->assertSee('The data is in-sync...')
             ->assertDontSee('Unsaved changes...')
         ;
+    }
+
+    /** @test */
+    function can_set_array_value()
+    {
+        Livewire::visit(new class extends Component {
+            public array $filters = [];
+
+            public function render()
+            {
+                return <<<'BLADE'
+                    <div>
+                        @foreach(['a', 'b', 'c'] as $key)
+                            <input dusk="checkbox.int.{{ $key }}" value="{{ $loop->index + 1}}" type="checkbox" wire:model.live="filters.{{ $key }}" />
+                        @endforeach
+
+                        @foreach(['d', 'e', 'f'] as $key)
+                            <input dusk="checkbox.string.{{ $key }}" value="value_{{ $key }}" type="checkbox" wire:model.live="filters.{{ $key }}" />
+                        @endforeach
+
+                        <button wire:click="$set('filters', [])" dusk="filters.reset">Reset</button>
+
+                        <div dusk="output">@json($filters)</div>
+                    </div>
+                BLADE;
+            }
+        })
+            ->check('@checkbox.int.a')
+            ->check('@checkbox.int.b')
+            ->check('@checkbox.int.c')
+            ->waitForLivewireToLoad()
+            ->assertSeeIn('@output', '{"a":"1","b":"2","c":"3"}')
+            ->click('@filters.reset')
+            ->waitForLivewireToLoad()
+            ->assertSeeIn('@output', '[]')
+            ->check('@checkbox.string.d')
+            ->check('@checkbox.string.e')
+            ->check('@checkbox.string.f')
+            ->pause(10000)
+            ->waitForLivewireToLoad()
+            ->assertSeeIn('@output', '{"d":"value_d","e":"value_e","f":"value_f"}')
+        ;
+
     }
 }
