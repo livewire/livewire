3978645bb1b2a7a533394289d8bac8cce8f57b59
Quentin Gabriele <quentin.gabriele@gmail.com>
2023-10-08 17:35:38 +0200

    Fix EloquentCollectionSynth with empty collection

--- src/Features/SupportModels/BrowserTest.php
+++ src/Features/SupportModels/BrowserTest.php
@@ -2,6 +2,7 @@
 
 namespace Livewire\Features\SupportModels;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Component;
@@ -17,7 +18,8 @@ class BrowserTest extends \Tests\BrowserTestCase
     public function parent_component_with_eloquent_collection_property_does_not_error_when_child_deletes_a_model_contained_within_it()
     {
         Livewire::visit([
-            new class extends Component {
+            new class extends Component
+            {
                 public $posts;
 
                 public function mount()
@@ -26,7 +28,8 @@ class BrowserTest extends \Tests\BrowserTestCase
                 }
 
                 #[BaseOn('postDeleted')]
-                public function setPosts() {
+                public function setPosts()
+                {
                     $this->posts = Post::all();
                 }
 
@@ -43,7 +46,8 @@ class BrowserTest extends \Tests\BrowserTestCase
                     HTML;
                 }
             },
-            'child' => new class extends Component {
+            'child' => new class extends Component
+            {
                 public $post;
 
                 public function delete($id)
@@ -68,8 +72,41 @@ class BrowserTest extends \Tests\BrowserTestCase
             ->assertPresent('@post-1')
             ->assertSeeIn('@post-1', 'Post #1')
             ->waitForLivewire()->click('@delete-1')
-            ->assertNotPresent('@parent-post-1')
-            ;
+            ->assertNotPresent('@parent-post-1');
+    }
+
+    /** @test */
+    public function empty_eloquent_collection_property_is_dehydrated_without_errors()
+    {
+        Livewire::visit([
+            new class extends Component
+            {
+                public $posts;
+
+                public function mount()
+                {
+                    $this->posts = new Collection();
+                }
+
+                function refresh()
+                {
+                    // 
+                }
+
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <div dusk="refresh" wire:click="refresh">Foo</div>
+                    </div>
+                    HTML;
+                }
+            },
+
+        ])
+            ->waitForLivewireToLoad()
+            ->waitForLivewire()->click('@refresh')
+            ->assertSeeIn('@refresh', 'Foo');
     }
 }
 
@@ -79,7 +116,8 @@ class Post extends Model
 
     protected $guarded = [];
 
-    public function getRows() {
+    public function getRows()
+    {
         return [
             ['id' => 1, 'title' => 'Post #1'],
             ['id' => 2, 'title' => 'Post #2'],
--- src/Features/SupportModels/EloquentCollectionSynth.php
+++ src/Features/SupportModels/EloquentCollectionSynth.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Queue\SerializesAndRestoresModelIdentifiers;
 use Livewire\Mechanisms\HandleComponents\Synthesizers\Synth;
 
-class EloquentCollectionSynth extends Synth {
+class EloquentCollectionSynth extends Synth
+{
     use SerializesAndRestoresModelIdentifiers;
 
     public static $key = 'elcln';
@@ -29,7 +30,7 @@ class EloquentCollectionSynth extends Synth {
          *
          * If no alias is found, this just returns the class name
          */
-        $modelAlias = (new $modelClass)->getMorphClass();
+        $modelAlias = $modelClass ? (new $modelClass)->getMorphClass() : null;
 
         $meta = [];
 
@@ -54,7 +55,7 @@ class EloquentCollectionSynth extends Synth {
         // If no alias found, this returns `null`
         $modelAlias = Relation::getMorphedModel($modelClass);
 
-        if (! is_null($modelAlias)) {
+        if (!is_null($modelAlias)) {
             $modelClass = $modelAlias;
         }
 
@@ -72,15 +73,18 @@ class EloquentCollectionSynth extends Synth {
         return $collection;
     }
 
-    function get(&$target, $key) {
+    function get(&$target, $key)
+    {
         throw new \Exception('Can\'t access model properties directly');
     }
 
-    function set(&$target, $key, $value, $pathThusFar, $fullPath) {
+    function set(&$target, $key, $value, $pathThusFar, $fullPath)
+    {
         throw new \Exception('Can\'t set model properties directly');
     }
 
-    function call($target, $method, $params, $addEffect) {
+    function call($target, $method, $params, $addEffect)
+    {
         throw new \Exception('Can\'t call model methods directly');
     }
 }
