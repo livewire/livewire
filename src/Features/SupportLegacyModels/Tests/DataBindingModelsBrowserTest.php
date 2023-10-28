66a10f0335d7e8cfafd172b2238a15a382b9adab
Jason Beggs <jason@roasted.dev>
2023-10-28 19:32:16 -0400

    Add failing test

--- src/Features/SupportLegacyModels/Tests/DataBindingModelsBrowserTest.php
+++ src/Features/SupportLegacyModels/Tests/DataBindingModelsBrowserTest.php
@@ -64,11 +64,23 @@ class DataBindingModelsBrowserTest extends TestCase
         $author->posts[0]->comments[1]->author->name = 'John';
         $author->push();
     }
+
+    /** @test */
+    public function it_enables_changing_model_attributes_that_have_not_been_initialized()
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visitLivewireComponent($browser, DataBindingComponent::class)
+                ->waitForLivewire()->type('@post.title', 'Livewire is awesome')
+                ->assertSeeIn('@output.post.title', 'Livewire is awesome');
+        });
+    }
 }
 
 class DataBindingComponent extends BaseComponent
 {
     public $author;
+
+    public ?DataBindingPost $post;
 
     protected $rules = [
         'author.name' => '',
@@ -76,11 +88,14 @@ class DataBindingComponent extends BaseComponent
         'author.posts.*.title' => '',
         'author.posts.*.comments.*.comment' => '',
         'author.posts.*.comments.*.author.name' => '',
+        'post.title' => '',
     ];
 
     public function mount()
     {
         $this->author = DataBindingAuthor::with(['posts', 'posts.comments', 'posts.comments.author'])->first();
+
+        $this->post = new DataBindingPost();
     }
 
     public function save()
@@ -134,6 +149,12 @@ class DataBindingComponent extends BaseComponent
     </div>
 
     <button wire:click="save" type="button" dusk="save">Save</button>
+
+    <div>
+        Post Title
+        <input dusk='post.title' wire:model.live='post.title' />
+        <span dusk='output.post.title'>{{ $post->title }}</span>
+    </div>
 </div>
 HTML;
     }
