7e98f725c03d7f81be9399ea77acac015b6e326b
Josh Hanley <josh@hitsystems.com.au>
2023-11-07 23:47:24 +1000

    Add fix (#7236)

--- src/Features/SupportNestedComponentListeners/BrowserTest.php
+++ src/Features/SupportNestedComponentListeners/BrowserTest.php
@@ -88,5 +88,50 @@ class BrowserTest extends BrowserTestCase
         ->click('@button')
         ->waitForTextIn('@count', '5');
     }
+
+    /** @test */
+    function can_dispatch_multi_word_event_names()
+    {
+        Livewire::visit([
+            new class extends BaseComponent {
+                public $count = 0;
+
+                function increment() {
+                    $this->count++;
+                }
+
+                public function render() {
+                    return <<<'HTML'
+                        <div>
+                            <h1 dusk="count">{{ $count }}</h1>
+
+                            <livewire:child @fired-event="increment" />
+                        </div>
+                    HTML;
+                }
+            },
+            'child' => new class extends BaseComponent {
+                public function fire() {
+                    $this->dispatch('fired-event');
+                }
+
+                public function render() {
+                    return <<<'HTML'
+                        <div>
+                            <button dusk="button" wire:click="fire">fire from child</button>
+                            <button dusk="button2" wire:click="$dispatch('fired-event')">fire from child (directly)</button>
+                        </div>
+                    HTML;
+                }
+            }
+        ])
+        ->assertSeeIn('@count', '0')
+        ->click('@button')
+        ->waitForTextIn('@count', '1')
+        ->click('@button')
+        ->waitForTextIn('@count', '2')
+        ->click('@button2')
+        ->waitForTextIn('@count', '3');
+    }
 }
 
