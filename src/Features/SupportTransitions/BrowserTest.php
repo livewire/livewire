9d9ece0edfd8e9dcf61727b225cdac02a49fb493
Eser DENIZ <srwiez@gmail.com>
2023-11-09 17:46:10 +0000

    Failing test for wire:transition

--- src/Features/SupportTransitions/BrowserTest.php
+++ src/Features/SupportTransitions/BrowserTest.php
@@ -2,6 +2,7 @@
 
 namespace Livewire\Features\SupportTransitions;
 
+use Illuminate\Support\Arr;
 use Livewire\Livewire;
 
 class BrowserTest extends \Tests\BrowserTestCase
@@ -75,7 +76,6 @@ class BrowserTest extends \Tests\BrowserTestCase
                                 <li wire:transition.fade.duration.1000ms dusk="message-{{ $index + 1 }}">{{$message}}</li>
                             @endforeach
                         </ul>
-
                         <button type="button" wire:click="addMessage" dusk="add-message">Add message</button>
                     </div>
                     HTML;
@@ -87,5 +87,141 @@ class BrowserTest extends \Tests\BrowserTestCase
         ->assertVisible('@message-3')
         ->assertVisible('@message-4')
         ;
+    }
+
+    /** @test */
+    public function removing_element_from_an_iteration_should_not_disappear_before_transition_out()
+    {
+        $isInline = fn($target) => 'getComputedStyle(document.querySelector(\'[dusk="' . $target . '"]\')).display === "inline"';
+
+        Livewire::visit(
+            new class extends \Livewire\Component {
+
+                public $lines = [
+                    'target1' => 'code',
+                    'target2' => '    indented code',
+                    'target3' => '/code',
+                ];
+
+
+                function remove()
+                {
+                    unset($this->lines['target2']);
+                }
+
+                public function render()
+                {
+                    return <<<'HTML'
+                <div>
+                    <button wire:click='remove' dusk='remove'>Remove</button>
+
+                    <pre><code class='text-xs'>@foreach($lines as $index => $line)<span class='inline-block' wire:key='{{ $index }}' wire:transition.fade.duration.2000ms dusk='{{ $index }}'>{!! $line !!}</span>
+                @endforeach</code></pre>
+                </div>
+                HTML;
+                }
+            })
+            // Removing an element in the middle
+            ->assertPresent('@target2')
+            ->waitForLivewire()->click('@remove')
+            ->assertScript($isInline('target2'), true) // That should not have changed yet.
+            ->waitUntilMissing('@target2')
+            ->assertMissing('@target2');
+    }
+
+
+    /** @test */
+    public function removing_last_element_from_an_iteration_should_only_remove_himself()
+    {
+        $opacity = fn($target) => 'parseFloat(getComputedStyle(document.querySelector(\'[dusk="' . $target . '"]\')).opacity, 10)';
+        $isInline = fn($target) => 'getComputedStyle(document.querySelector(\'[dusk="' . $target . '"]\')).display === "inline"';
+        Livewire::visit(
+            new class extends \Livewire\Component {
+
+                public $lines = [
+                    'target1' => 'code',
+                    'target2' => '    indented code',
+                    'target3' => '/code',
+                ];
+
+
+                function remove()
+                {
+                    unset($this->lines['target3']);
+                }
+
+                public function render()
+                {
+                    return <<<'HTML'
+                <div>
+                    <button wire:click='remove' dusk='remove'>Remove</button>
+
+                    <pre><code class='text-xs'>@foreach($lines as $index => $line)<span class='inline-block' wire:key='{{ $index }}' wire:transition.fade.duration.2000ms dusk='{{ $index }}'>{!! $line !!}</span>
+                @endforeach</code></pre>
+                </div>
+                HTML;
+                }
+            })
+            // removing the last element
+            ->assertVisible('@target1')
+            ->assertVisible('@target3')
+            ->waitForLivewire()->click('@remove')
+            ->assertScript($isInline('target3'), true) // That should not have changed yet.
+            ->waitUntilMissing('@target3')
+            ->assertMissing('@target3')
+            ->assertPresent('@target1')
+            ->assertVisible('@target1')
+            ->assertScript($isInline('target1'), true) // Still inline
+            ->assertScript($opacity('target1'), 1) // Still opacity 1
+        ;
+
+    }
+
+    /** @test */
+    public function removing_last_element_from_an_iteration_should_only_remove_himself_with_transition()
+    {
+        $opacity = fn($target) => 'parseFloat(getComputedStyle(document.querySelector(\'[dusk="' . $target . '"]\')).opacity, 10)';
+        $isInline = fn($target) => 'getComputedStyle(document.querySelector(\'[dusk="' . $target . '"]\')).display === "inline"';
+        Livewire::visit(
+            new class extends \Livewire\Component {
+
+                public $lines = [
+                    'target1' => 'code',
+                    'target2' => '    indented code',
+                    'target3' => '/code',
+                ];
+
+
+                function remove()
+                {
+                    unset($this->lines['target3']);
+                }
+
+                public function render()
+                {
+                    return <<<'HTML'
+                <div>
+                    <button wire:click='shift' dusk='shift'>Shift</button>
+                    <button wire:click='remove' dusk='remove'>Remove</button>
+
+                    <pre><code class='text-xs'>@foreach($lines as $index => $line)<span class='inline-block' wire:key='{{ $index }}' wire:transition.fade.duration.2000ms dusk='{{ $index }}'>{!! $line !!}</span>@endforeach</code></pre>
+                </div>
+                HTML;
+                }
+            })
+
+            // removing the last element
+            ->assertVisible('@target1')
+            ->assertVisible('@target3')
+            ->waitForLivewire()->click('@remove')
+            ->assertScript($isInline('target3'), true) // That should not have changed yet.
+            ->waitUntilMissing('@target3')
+            ->assertMissing('@target3')
+            ->assertPresent('@target1')
+            ->assertVisible('@target1')
+            ->assertScript($isInline('target1'), true) // Still inline
+            ->assertScript($opacity('target1'), 1) // Still opacity 1
+        ;
+
     }
 }
