ceef3cfd2e4d51beeb6b80b5d75bc882cf880b83
chillbram <7299762+chillbram@users.noreply.github.com>
2023-11-14 15:55:59 +0100

    Make property public

--- src/Features/SupportLockedProperties/CannotUpdateLockedPropertyException.php
+++ src/Features/SupportLockedProperties/CannotUpdateLockedPropertyException.php
@@ -4,7 +4,7 @@ namespace Livewire\Features\SupportLockedProperties;
 
 class CannotUpdateLockedPropertyException extends \Exception
 {
-    public function __construct($property)
+    public function __construct(public $property)
     {
         parent::__construct(
             'Cannot update locked property: ['.$property.']'
