5110b9549179386caeece1208e2d0c8267a583c1
Caleb Porzio <calebporzio@gmail.com>
2023-11-16 15:28:05 -0500

    Isolate form object validation from root livewire component (#7266)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Merge form object error bags with root validator
    
    * wip

--- src/Exceptions/MissingRulesException.php
+++ src/Exceptions/MissingRulesException.php
@@ -6,10 +6,12 @@ class MissingRulesException extends \Exception
 {
     use BypassViewHandler;
 
-    public function __construct($component)
+    public function __construct($instance)
     {
+        $class = $instance::class;
+
         parent::__construct(
-            "Missing [\$rules/rules()] property/method on Livewire component: [{$component}]."
+            "Missing [\$rules/rules()] property/method on: [{$class}]."
         );
     }
 }
