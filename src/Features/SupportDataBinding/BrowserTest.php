ae222fa30d8abd95b6d1dcbc84d244e65607f4b0
luanfreitasdev <luanfreitas10@protonmail.com>
2023-12-02 11:01:22 -0300

    Add failed test to discussion #7100

--- src/Features/SupportDataBinding/BrowserTest.php
+++ src/Features/SupportDataBinding/BrowserTest.php
@@ -35,4 +35,43 @@ class BrowserTest extends BrowserTestCase
             ->assertDontSee('Unsaved changes...')
         ;
     }
+
+    /** @test */
+    function ensures_that_the_input_correctly_receives_the_value_when_binding_with_the_alpine_directive()
+    {
+        Livewire::visit(new class extends Component {
+            public string $input = '100';
+
+            public function save() { }
+
+            public function render()
+            {
+                return <<<'BLADE'
+                    <div>
+                        <input dusk='input' wire:model="input" x-mask="***-9"/>
+
+                        <div dusk="input.alpine" x-text="$wire.get('input')"></div>
+                        <div dusk="input.blade">{{ $input }}</div>
+
+                        <button wire:click="save" dusk="save">save</button>
+                    </div>
+                BLADE;
+            }
+        })
+            ->click('@save')
+            ->assertSeeIn('@input.blade', '100')
+
+            ->type('@input', 'Hello World')
+            ->assertDontSeeIn('@input.alpine', 'Hel-l') // it shouldn't be possible to insert a letter because of x-mask
+
+            ->type('@input', 'abc-d')
+            ->assertDontSeeIn('@input.alpine', 'abc-d') // it shouldn't be possible to insert a letter because of x-mask
+
+            ->type('@input', 'xyz-1')
+            ->assertSeeIn('@input.alpine', 'xyz-1')
+
+            ->waitForLivewire()->click('@save')
+            ->assertSeeIn('@input.blade', 'xyz-1')
+        ;
+    }
 }
