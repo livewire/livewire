4b2ce244fa9ff98233d53908958783dee4ad10d7
Caleb Porzio <calebporzio@gmail.com>
2023-12-10 20:21:29 -0500

    Build files

--- dist/livewire.js
+++ dist/livewire.js
@@ -2087,8 +2087,8 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   var specialBooleanAttrs = `itemscope,allowfullscreen,formnovalidate,ismap,nomodule,novalidate,readonly`;
   var isBooleanAttr2 = /* @__PURE__ */ makeMap(specialBooleanAttrs + `,async,autofocus,autoplay,controls,default,defer,disabled,hidden,loop,open,required,reversed,scoped,seamless,checked,muted,multiple,selected`);
-  var EMPTY_OBJ = false ? Object.freeze({}) : {};
-  var EMPTY_ARR = false ? Object.freeze([]) : [];
+  var EMPTY_OBJ = true ? Object.freeze({}) : {};
+  var EMPTY_ARR = true ? Object.freeze([]) : [];
   var hasOwnProperty = Object.prototype.hasOwnProperty;
   var hasOwn = (val, key) => hasOwnProperty.call(val, key);
   var isArray2 = Array.isArray;
@@ -2121,8 +2121,8 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   var targetMap = /* @__PURE__ */ new WeakMap();
   var effectStack = [];
   var activeEffect;
-  var ITERATE_KEY = Symbol(false ? "iterate" : "");
-  var MAP_KEY_ITERATE_KEY = Symbol(false ? "Map key iterate" : "");
+  var ITERATE_KEY = Symbol(true ? "iterate" : "");
+  var MAP_KEY_ITERATE_KEY = Symbol(true ? "Map key iterate" : "");
   function isEffect(fn) {
     return fn && fn._isEffect === true;
   }
@@ -2212,7 +2212,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     if (!dep.has(activeEffect)) {
       dep.add(activeEffect);
       activeEffect.deps.push(dep);
-      if (false) {
+      if (activeEffect.options.onTrack) {
         activeEffect.options.onTrack({
           effect: activeEffect,
           target,
@@ -2276,7 +2276,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
       }
     }
     const run = (effect3) => {
-      if (false) {
+      if (effect3.options.onTrigger) {
         effect3.options.onTrigger({
           effect: effect3,
           target,
@@ -2413,13 +2413,13 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   var readonlyHandlers = {
     get: readonlyGet,
     set(target, key) {
-      if (false) {
+      if (true) {
         console.warn(`Set operation on key "${String(key)}" failed: target is readonly.`, target);
       }
       return true;
     },
     deleteProperty(target, key) {
-      if (false) {
+      if (true) {
         console.warn(`Delete operation on key "${String(key)}" failed: target is readonly.`, target);
       }
       return true;
@@ -2481,7 +2481,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     if (!hadKey) {
       key = toRaw(key);
       hadKey = has2.call(target, key);
-    } else if (false) {
+    } else if (true) {
       checkIdentityKeys(target, has2, key);
     }
     const oldValue = get3.call(target, key);
@@ -2500,7 +2500,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     if (!hadKey) {
       key = toRaw(key);
       hadKey = has2.call(target, key);
-    } else if (false) {
+    } else if (true) {
       checkIdentityKeys(target, has2, key);
     }
     const oldValue = get3 ? get3.call(target, key) : void 0;
@@ -2513,7 +2513,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   function clear() {
     const target = toRaw(this);
     const hadItems = target.size !== 0;
-    const oldTarget = false ? isMap(target) ? new Map(target) : new Set(target) : void 0;
+    const oldTarget = true ? isMap(target) ? new Map(target) : new Set(target) : void 0;
     const result = target.clear();
     if (hadItems) {
       trigger2(target, "clear", void 0, void 0, oldTarget);
@@ -2558,7 +2558,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   function createReadonlyMethod(type) {
     return function(...args) {
-      if (false) {
+      if (true) {
         const key = args[0] ? `on key "${args[0]}" ` : ``;
         console.warn(`${capitalize(type)} operation ${key}failed: target is readonly.`, toRaw(this));
       }
@@ -2660,6 +2660,13 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   var readonlyCollectionHandlers = {
     get: /* @__PURE__ */ createInstrumentationGetter(true, false)
   };
+  function checkIdentityKeys(target, has2, key) {
+    const rawKey = toRaw(key);
+    if (rawKey !== key && has2.call(target, rawKey)) {
+      const type = toRawType(target);
+      console.warn(`Reactive ${type} contains both the raw and reactive versions of the same object${type === `Map` ? ` as keys` : ``}, which can lead to inconsistencies. Avoid differentiating between the raw and reactive versions of an object and only use the reactive version if possible.`);
+    }
+  }
   var reactiveMap = /* @__PURE__ */ new WeakMap();
   var shallowReactiveMap = /* @__PURE__ */ new WeakMap();
   var readonlyMap = /* @__PURE__ */ new WeakMap();
@@ -2692,7 +2699,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   function createReactiveObject(target, isReadonly, baseHandlers, collectionHandlers, proxyMap) {
     if (!isObject2(target)) {
-      if (false) {
+      if (true) {
         console.warn(`value cannot be made reactive: ${String(target)}`);
       }
       return target;
@@ -2731,10 +2738,13 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     cleanup22(unwatch);
   });
   magic("model", (el, { cleanup: cleanup22 }) => {
-    let func = generateModelAccessor(el.parentElement, cleanup22);
-    Object.defineProperty(func, "self", { get() {
-      return accessor(generateModelAccessor(el, cleanup22));
+    let func = generateModelAccessor(el, cleanup22);
+    Object.defineProperty(func, "closest", { get() {
+      let func2 = generateModelAccessor(el.parentElement, cleanup22);
+      func2._x_modelAccessor = true;
+      return accessor(func2);
     } });
+    func._x_modelAccessor = true;
     return accessor(func);
   });
   function generateModelAccessor(el, cleanup22) {
@@ -2747,17 +2757,20 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
         return this;
       return fallbackStateInitialValue;
     };
-    accessor2.exists = () => {
-      return !!closestModelEl;
+    let model = () => {
+      if (!closestModelEl) {
+        throw "Cannot find an available x-model directive to reference from $model.";
+      }
+      return closestModelEl._x_model;
     };
     accessor2.get = () => {
-      return closestModelEl._x_model.get();
+      return model().get();
     };
     accessor2.set = (value) => {
       if (typeof value === "function") {
-        closestModelEl._x_model.set(value(accessor2.get()));
+        model().set(value(accessor2.get()));
       } else {
-        closestModelEl._x_model.set(value);
+        model().set(value);
       }
     };
     accessor2.watch = (callback) => {
@@ -3076,14 +3089,20 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
       evaluateSet = () => {
       };
     }
-    let getValue = () => {
+    let getResult = () => {
       let result;
       evaluateGet((value) => result = value);
+      if (result._x_modelAccessor) {
+        return result._x_modelAccessor.closest;
+      }
+      return result;
+    };
+    let getValue = () => {
+      let result = getResult();
       return isGetterSetter(result) ? result.get() : result;
     };
     let setValue = (value) => {
-      let result;
-      evaluateGet((value2) => result = value2);
+      let result = getResult();
       if (isGetterSetter(result)) {
         result.set(value);
       } else {
