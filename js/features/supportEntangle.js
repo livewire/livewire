7749d3a7c7d509697b953fd3275aac2b2837cb5b
luanfreitasdev <luanfreitas10@protonmail.com>
2024-01-03 21:09:51 -0300

    Fix entangle

--- js/features/supportEntangle.js
+++ js/features/supportEntangle.js
@@ -15,7 +15,7 @@ export function generateEntangleFunction(component, cleanup) {
             // Check to see if the Livewire property exists and if not log a console error
             // and return so everything else keeps running.
             if (typeof livewirePropertyValue === 'undefined') {
-                console.error(`Livewire Entangle Error: Livewire property ['$${livewireProperty}'] cannot be found on component: ['${component.name}']`, component.el)
+                console.error(`Livewire Entangle Error: Livewire property ['${livewireProperty}'] cannot be found on component: ['${component.name}']`, component.el)
                 return
             }
 
