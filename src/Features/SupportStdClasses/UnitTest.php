9d4ebda4619e418ef30829977d3f83d6fbec2d40
Josh Hanley <josh@hitsystems.com.au>
2024-01-05 15:51:23 +1000

    Convert to unit test

--- src/Features/SupportStdClasses/UnitTest.php
+++ src/Features/SupportStdClasses/UnitTest.php
@@ -6,12 +6,12 @@ use Livewire\Component;
 use Livewire\Livewire;
 use Tests\BrowserTestCase;
 
-class BrowserTest extends BrowserTestCase
+class UnitTest extends BrowserTestCase
 {
     /** @test */
     function can_use_wire_stdclass_property()
     {
-        Livewire::visit(new class extends Component {
+        Livewire::test(new class extends Component {
             public $obj;
 
             function mount()
@@ -29,11 +29,11 @@ class BrowserTest extends BrowserTestCase
                 HTML;
             }
         })
-            ->type('@input', 'foo')
-            ->pause(300)
-            ->append('@input', 'bar')
-            ->pause(300)
-            ->assertSeeIn('@output', 'foobar')
+            ->assertSet('obj.property', null)
+            ->set('obj.property', 'foo')
+            ->assertSet('obj.property', 'foo')
+            ->set('obj.property', 'bar')
+            ->assertSet('obj.property', 'bar')
         ;
     }
 }
