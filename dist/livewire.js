91544359333d6d7225063f90ae2f6f118f473bc2
Josh Hanley <josh@hitsystems.com.au>
2024-01-05 16:04:30 +1000

    Build assets

--- dist/livewire.js
+++ dist/livewire.js
@@ -405,6 +405,21 @@
     }
     throw "Livewire: No CSRF token detected";
   }
+  var nonce2;
+  function getNonce() {
+    if (nonce2)
+      return nonce2;
+    if (window.livewireScriptConfig["nonce"] ?? false) {
+      nonce2 = window.livewireScriptConfig["nonce"];
+      return nonce2;
+    }
+    const elWithNonce = document.querySelector("style[data-livewire-style][nonce]");
+    if (elWithNonce) {
+      nonce2 = elWithNonce.nonce;
+      return nonce2;
+    }
+    return null;
+  }
   function getUpdateUri() {
     return document.querySelector("[data-update-uri]")?.getAttribute("data-update-uri") ?? window.livewireScriptConfig["uri"] ?? null;
   }
@@ -7319,6 +7334,10 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
       100% { transform: rotate(360deg); }
     }
     `;
+    nonce = getNonce();
+    if (nonce) {
+      style.nonce = nonce;
+    }
     document.head.appendChild(style);
   }
 
