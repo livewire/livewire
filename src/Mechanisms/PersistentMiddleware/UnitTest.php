ba98cf0f9ff1baa9bf9aa799774f4c6e71e23164
Tim MacDonald <hello@timacdonald.me>
2024-01-05 11:10:07 +1100

    Fix middleware memory leak

---
+++ src/Mechanisms/PersistentMiddleware/UnitTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Livewire\Mechanisms\PersistentMiddleware;
+
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Stringable;
+use Livewire\Component;
+use Livewire\Form;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class UnitTest extends \LegacyTests\Unit\TestCase
+{
+    /** @test */
+    public function it_does_not_have_persistent_middleware_memory_leak_when_adding_middleware()
+    {
+        Livewire::addPersistentMiddleware('MyMiddleware');
+
+        $config = $this->app['config'];
+        $this->app->forgetInstances();
+        $this->app->forgetScopedInstances();
+        Facade::clearResolvedInstances();
+        // Need to rebind these for the testcase cleanup to work.
+        $this->app->instance('app', $this->app);
+        $this->app->instance('config', $config);
+
+        // It hangs around because it is a static variable, so we do expect
+        // it to still exist here.
+        $this->assertCount(1, array_filter(Livewire::getPersistentMiddleware(), fn ($middleware) => $middleware === 'MyMiddleware'));
+
+        Livewire::addPersistentMiddleware('MyMiddleware');
+        $this->assertCount(1, array_filter(Livewire::getPersistentMiddleware(), fn ($middleware) => $middleware === 'MyMiddleware'));
+    }
+}
