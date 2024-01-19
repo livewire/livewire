de366b9f83756ebab447721c41cb02328bc044a1
Caleb Porzio <calebporzio@gmail.com>
2024-01-19 15:32:30 -0500

    wip

--- src/Features/SupportFileUploads/BrowserTest.php
+++ src/Features/SupportFileUploads/BrowserTest.php
@@ -61,4 +61,56 @@ class BrowserTest extends \Tests\BrowserTestCase
         })
         ;
     }
+
+    /** @test */
+    public function can_cancel_an_upload()
+    {
+        // WIP
+
+        Storage::persistentFake('tmp-for-tests');
+
+        Livewire::visit(new class extends Component {
+            use WithFileUploads;
+
+            public $photo;
+
+            function mount()
+            {
+                Storage::disk('tmp-for-tests')->deleteDirectory('photos');
+            }
+
+            function save()
+            {
+                $this->photo->storeAs('photos', 'photo.png');
+            }
+
+            function render() { return <<<'HTML'
+            <div x-on:livewire-upload-cancel="$el.querySelector('h1').textContent = 'cancelled'">
+                <input type="file" wire:model="photo" dusk="upload">
+
+                <div wire:loading wire:target="photo">uploading...</div>
+
+                <button wire:click="$cancelUpload('photo')" dusk="cancel">cancel</button>
+
+                <h1 dusk="output"></h1>
+
+                <button wire:click="save" dusk="save">Save</button>
+            </div>
+            HTML; }
+        })
+        ->assertMissing('@preview')
+        ->attach('@upload', __DIR__ . '/browser_test_image_big.png')
+        ->pause(10)
+        ->click('@cancel')
+        ->assertSeeIn('@output', 'cancelled')
+        // ->tap(function () {
+        //     Storage::disk('tmp-for-tests')->assertMissing('photos/photo.png');
+        // })
+        // ->waitForLivewire()
+        // ->click('@save')
+        // ->tap(function () {
+        //     Storage::disk('tmp-for-tests')->assertExists('photos/photo.png');
+        // })
+        ;
+    }
 }
