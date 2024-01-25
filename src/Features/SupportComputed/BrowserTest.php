e6500decf38b9f30b54ba511eb04184d3a0d3773
ju5t <m@ju5t.nl>
2024-01-25 21:32:42 +0100

    fix: use key combination with persist

--- src/Features/SupportComputed/BrowserTest.php
+++ src/Features/SupportComputed/BrowserTest.php
@@ -89,4 +89,47 @@ class BrowserTest extends BrowserTestCase
         ->refresh()
         ->assertSeeIn('@count', '1');
     }
+
+    /** @test */
+    public function can_persist_computed_with_key_between_requests_and_bust_them()
+    {
+        Livewire::visit(new class extends Component {
+            public $count = 0;
+
+            protected $thing = 'hey';
+
+            #[Computed(persist: true, key: 'there')]
+            public function foo() {
+                $this->count++;
+
+                return 'bar';
+            }
+
+            function unset()
+            {
+                unset($this->foo);
+            }
+
+            function render()
+            {
+                $noop = $this->foo;
+
+                return <<<'HTML'
+                <div>
+                    <button wire:click="$refresh" dusk="refresh">refresh</button>
+                    <button wire:click="unset" dusk="unset">unset</button>
+
+                    <div dusk="count">{{ $count }}</div>
+                </div>
+                HTML;
+            }
+        })
+        ->assertSeeIn('@count', '1')
+        ->waitForLivewire()->click('@refresh')
+        ->assertSeeIn('@count', '1')
+        ->waitForLivewire()->click('@unset')
+        ->assertSeeIn('@count', '2')
+        ->waitForLivewire()->click('@refresh')
+        ->assertSeeIn('@count', '2');
+    }
 }
