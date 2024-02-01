f05b57d223350d63db0bb1993f1744f91a8badeb
Günther Debrauwer <debrauwer.gunther@gmail.com>
2024-02-01 18:36:31 +0100

    Remove empty line

--- src/Features/SupportFileUploads/TemporaryUploadedFile.php
+++ src/Features/SupportFileUploads/TemporaryUploadedFile.php
@@ -82,7 +82,6 @@ class TemporaryUploadedFile extends UploadedFile
     public function getPathname(): string
     {
         return $this->getRealPath();
-
     }
 
     public function getClientOriginalName(): string
