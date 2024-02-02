6c45e58a248b6c0cfd1775188a5b8072b08343c6
Alex Wass <alex.wass@laser.red>
2024-02-02 11:47:15 +0000

    Build assets

--- dist/livewire.js
+++ dist/livewire.js
@@ -3607,21 +3607,13 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
       if (isObject22(items)) {
         items = Object.entries(items).map(([key, value]) => {
           let scope2 = getIterationScopeVariables(iteratorNames, value, key, items);
-          evaluateKey((value2) => {
-            if (keys.includes(value2))
-              warn("Duplicate key on x-for", el);
-            keys.push(value2);
-          }, { scope: { index: key, ...scope2 } });
+          evaluateKey((value2) => keys.push(value2), { scope: { index: key, ...scope2 } });
           scopes.push(scope2);
         });
       } else {
         for (let i = 0; i < items.length; i++) {
           let scope2 = getIterationScopeVariables(iteratorNames, items[i], i, items);
-          evaluateKey((value) => {
-            if (keys.includes(value))
-              warn("Duplicate key on x-for", el);
-            keys.push(value);
-          }, { scope: { index: i, ...scope2 } });
+          evaluateKey((value) => keys.push(value), { scope: { index: i, ...scope2 } });
           scopes.push(scope2);
         }
       }
@@ -3669,7 +3661,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
         let marker = document.createElement("div");
         mutateDom(() => {
           if (!elForSpot)
-            warn(`x-for ":key" is undefined or invalid`, templateEl, keyForSpot, lookup);
+            warn(`x-for ":key" is undefined or invalid`, templateEl);
           elForSpot.after(marker);
           elInSpot.after(elForSpot);
           elForSpot._x_currentIfEl && elForSpot.after(elForSpot._x_currentIfEl);
@@ -8792,7 +8784,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
             let handler4 = (e) => dispatchSelf(component, event, [e]);
             window.Echo.join(channel).listen(event_name, handler4);
             component.addCleanup(() => {
-              window.Echo[channel_type](channel).stopListening(event_name, handler4);
+              window.Echo.leaveChannel(channel);
             });
           }
         } else if (channel_type == "notification") {
