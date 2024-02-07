6ea5178db41fe5bc0bd05895dc03c8776f9bc395
Caleb Porzio <calebporzio@gmail.com>
2024-02-07 09:43:56 -0500

    remove unit tests

--- src/Features/SupportQueryString/UnitTest.php
+++ src/Features/SupportQueryString/UnitTest.php
@@ -23,48 +23,4 @@ class UnitTest extends \Tests\TestCase
 
         $this->assertTrue(isset($component->effects['url']));
     }
-
-    /** @test */
-    function can_handle_empty_querystring_value_as_empty_string()
-    {
-        Livewire::withQueryParams([
-            'foo' => null,
-        ])->test(new class extends Component {
-            #[BaseUrl]
-            public $foo;
-
-            function render() { return '<div></div>'; }
-        })->assertSet('foo', '');
-
-        Livewire::withQueryParams([
-            'foo' => null,
-        ])->test(new class extends Component {
-            #[BaseUrl(nullable: false)]
-            public ?string $foo;
-
-            function render() { return '<div></div>'; }
-        })->assertSet('foo', '');
-    }
-
-    /** @test */
-    function can_handle_empty_querystring_value_as_null()
-    {
-        Livewire::withQueryParams([
-            'foo' => null,
-        ])->test(new class extends Component {
-            #[BaseUrl]
-            public ?string $foo;
-
-            function render() { return '<div></div>'; }
-        })->assertSet('foo', null);
-
-        Livewire::withQueryParams([
-            'foo' => null,
-        ])->test(new class extends Component {
-            #[BaseUrl(nullable: true)]
-            public $foo;
-
-            function render() { return '<div></div>'; }
-        })->assertSet('foo', null);
-    }
 }
