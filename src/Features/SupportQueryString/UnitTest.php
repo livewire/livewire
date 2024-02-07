e8c2399659b88f89da6f68bcc5cf9015ccff235c
Günther Debrauwer <gunther@nextapps.be>
2024-02-07 12:05:10 +0100

    Add unit test

--- src/Features/SupportQueryString/UnitTest.php
+++ src/Features/SupportQueryString/UnitTest.php
@@ -23,4 +23,30 @@ class UnitTest extends \Tests\TestCase
 
         $this->assertTrue(isset($component->effects['url']));
     }
+
+    /** @test */
+    function can_handle_empty_querystring_value_as_empty_string()
+    {
+        Livewire::withQueryParams([
+            'foo' => null,
+        ])->test(new class extends Component {
+            #[BaseUrl]
+            public $foo;
+
+            function render() { return '<div></div>'; }
+        })->assertSet('foo', '');
+    }
+
+    /** @test */
+    function can_handle_empty_querystring_value_as_null()
+    {
+        Livewire::withQueryParams([
+            'foo' => null,
+        ])->test(new class extends Component {
+            #[BaseUrl]
+            public ?string $foo;
+
+            function render() { return '<div></div>'; }
+        })->assertSet('foo', null);
+    }
 }
