bfcc12452bd291091a7eed8ecb120506240b1211
Austen Cameron <austen.cameron@gmail.com>
2024-02-22 14:02:58 -0700

    wip

--- src/Features/SupportMorphAwareIfStatement/UnitTest.php
+++ src/Features/SupportMorphAwareIfStatement/UnitTest.php
@@ -429,7 +429,13 @@ class UnitTest extends \Tests\TestCase
             30 => [
                 1,
                 <<<'HTML'
-                <div> @if (preg_replace('/[^a-zA-Z]+/', '', $spinner))<span> {{ $someProperty }} </span> @endif Hello</div>
+                <div> @if (preg_replace('/[^a-zA-Z]+/', '', $spinner))<span> {{ $someProperty }} </span> @endif Else</div>
+                HTML
+            ],
+            31 => [
+                1,
+                <<<'HTML'
+                <div> @for ($i=0; $i<3; $i++)<span> {{ $someProperty }} </span> @endfor Else</div>
                 HTML
             ]
         ];
