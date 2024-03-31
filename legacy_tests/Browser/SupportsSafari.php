1bd48ee711c807cf3fa650e680d937730d746783
Josh Hanley <josh@hitsystems.com.au>
2024-03-31 19:16:14 +1000

    wip

--- legacy_tests/Browser/SupportsSafari.php
+++ legacy_tests/Browser/SupportsSafari.php
@@ -2,58 +2,18 @@
 
 namespace LegacyTests\Browser;
 
-use Override;
-
 // Thanks to https://github.com/appstract/laravel-dusk-safari for most of this source.
 trait SupportsSafari
 {
     protected static $safariProcess;
 
-    /**
-     * This is an override and customisation of orchestra/testbench-dusk/src/TestCase.php
-     * 
-     * @return void 
-     */
-    #[Override]
-    public static function setUpBeforeClass(): void
+    protected static function defineChromeDriver(): void
     {
-        static::setUpBeforeClassForInteractsWithWebDriverOptions();
-
-        if (! isset($_ENV['DUSK_DRIVER_URL'])) {
-            if (static::$useSafari) {
-                static::startSafariDriver();
-            } else {
-                static::startChromeDriver(['port' => 9515]);
-            }
+        if (static::$useSafari) {
+            static::startSafariDriver();
+        } else {
+            static::startChromeDriver(['port' => 9515]);
         }
-
-        /**
-         * As we don't want to call `parent::setUpBeforeClass();` which would call the
-         * method we are overriding. The method requires us to call it's parent but
-         * we can't do that. So we have had to create a copy of the method below
-         * and call it here instead.
-         */
-        static::parentOfParentSetUpBeforeClass();
-        static::startServing();
-    }
-
-    /**
-     * This is purely a copy of the core method from orchestra/testbench-core/src/TestCase.php
-     * As we can't call a parent of a parent, this seems to be the only way around it.
-     * 
-     * @return void 
-     */
-    public static function parentOfParentSetUpBeforeClass()
-    {
-        static::setUpBeforeClassUsingPHPUnit();
-
-        /** @phpstan-ignore-next-line */
-        if (static::usesTestingConcern(Pest\WithPest::class)) {
-            static::setUpBeforeClassUsingPest(); // @phpstan-ignore-line
-        }
-
-        static::setUpBeforeClassUsingTestCase();
-        static::setUpBeforeClassUsingWorkbench();
     }
 
     public function onlyRunOnChrome()
