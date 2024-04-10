8735ff994991c6eb3867d1fd8769772ad8fb0703
luanfreitasdev <luanfreitas10@protonmail.com>
2024-04-10 14:27:10 -0300

    Fix teleport directive when view is cached

--- src/Features/SupportTeleporting/SupportTeleporting.php
+++ src/Features/SupportTeleporting/SupportTeleporting.php
@@ -2,17 +2,18 @@
 
 namespace Livewire\Features\SupportTeleporting;
 
+use Illuminate\Support\Facades\Blade;
 use Livewire\ComponentHook;
 
 class SupportTeleporting extends ComponentHook
 {
     static function provide()
     {
-        app('livewire')->directive('teleport', function ($expression) {
+        Blade::directive('teleport', function ($expression) {
             return '<template x-teleport="<?php echo e('.$expression.'); ?>">';
         });
 
-        app('livewire')->directive('endteleport', function () {
+        Blade::directive('endteleport', function () {
             return '</template>';
         });
     }
