ed89372eb6eee3952bf7ccd1a6a082b29a709a4b
Perry van der Meer <11609290+PerryvanderMeer@users.noreply.github.com>
2024-04-18 20:14:44 +0200

    Fix hydrating union types (#8286)

--- src/Mechanisms/HandleComponents/Synthesizers/Tests/IntSynthUnitTest.php
+++ src/Mechanisms/HandleComponents/Synthesizers/Tests/IntSynthUnitTest.php
@@ -4,6 +4,7 @@ namespace Livewire\Mechanisms\HandleComponents\Synthesizers\Tests;
 
 use Livewire\Component;
 use Livewire\Livewire;
+use PHPUnit\Framework\Attributes\Test;
 
 class IntSynthUnitTest extends \Tests\TestCase
 {
@@ -16,12 +17,81 @@ class IntSynthUnitTest extends \Tests\TestCase
             ->set('intField', null)
             ->assertSetStrict('intField', null);
     }
+
+    #[Test]
+    public function int_value_hydrated_returns_int()
+    {
+        Livewire::test(ComponentWithInt::class)
+            ->set('intField', 3)
+            ->assertSetStrict('intField', 3);
+    }
+
+    #[Test]
+    public function string_value_hydrated_returns_int()
+    {
+        Livewire::test(ComponentWithInt::class)
+            ->set('intField', '3')
+            ->assertSetStrict('intField', 3)
+            ->set('intField', '3.14')
+            ->assertSetStrict('intField', 3);
+    }
+
+    #[Test]
+    public function float_value_hydrated_returns_int()
+    {
+        Livewire::test(ComponentWithInt::class)
+            ->set('intField', 3.00)
+            ->assertSetStrict('intField', 3)
+            ->set('intField', 3.14)
+            ->assertSetStrict('intField', 3);
+    }
+
+    #[Test]
+    public function can_hydrate_int_or_string()
+    {
+        Livewire::test(ComponentWithIntOrString::class)
+            ->set('intOrStringField', 3)
+            ->assertSetStrict('intOrStringField', 3)
+            ->set('intOrStringField', 3.00)
+            ->assertSetStrict('intOrStringField', 3)
+            ->set('intOrStringField', 3.14)
+            ->assertSetStrict('intOrStringField', 3)
+            ->set('intOrStringField', '3')
+            ->assertSetStrict('intOrStringField', 3)
+            ->set('intOrStringField', '3.00')
+            ->assertSetStrict('intOrStringField', 3)
+            ->set('intOrStringField', '3.14')
+            ->assertSetStrict('intOrStringField', '3.14')
+            ->set('intOrStringField', 'foo')
+            ->assertSetStrict('intOrStringField', 'foo')
+            ->set('intOrStringField', null)
+            ->assertSetStrict('intOrStringField', null);
+    }
 }
 
 class ComponentWithNullableInt extends Component
 {
     public ?int $intField = null;
 
+    public function render()
+    {
+        return view('null-view');
+    }
+}
+
+class ComponentWithInt extends Component
+{
+    public int $intField;
+
+    public function render()
+    {
+        return view('null-view');
+    }
+}
+
+class ComponentWithIntOrString extends Component
+{
+    public int|string $intOrStringField;
 
     public function render()
     {
