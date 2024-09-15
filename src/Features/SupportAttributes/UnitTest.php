d14b09d17358056583b0e99e355903c579233aa0
Cam Kemshal-Bell <cam@iterated.tech>
2024-09-15 23:42:53 +1000

    feat(attribute-test): add tests for the new assertion methods

--- src/Features/SupportAttributes/UnitTest.php
+++ src/Features/SupportAttributes/UnitTest.php
@@ -2,6 +2,17 @@
 
 namespace Livewire\Features\SupportAttributes;
 
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Isolate;
+use Livewire\Attributes\Js;
+use Livewire\Attributes\Locked;
+use Livewire\Attributes\Modelable;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Reactive;
+use Livewire\Attributes\Session;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
+use Livewire\Attributes\Lazy;
 use Livewire\Livewire;
 use Tests\TestComponent;
 
@@ -48,6 +59,111 @@ class UnitTest extends \Tests\TestCase
         })
             ->assertSetStrict('count', 0);
     }
+
+    public function test_component_has_attribute()
+    {
+        Livewire::test(NewComponent::class)
+                ->assertComponentHasAttribute(Title::class, 'Class Level Attribute')
+                ->assertComponentHasAttribute(Validate::class, ['required', 'integer'])
+                ->assertComponentHasAttribute(On::class, 'fooEvent');
+    }
+
+    public function test_component_has_class_level_attribute_with_value()
+    {
+        Livewire::test(NewComponent::class)
+            ->assertClassHasAttribute(Lazy::class, ['isolate' => false])
+            ->assertClassHasAttribute(Title::class, 'Class Level Attribute');
+    }
+
+    public function test_component_has_method_level_attribute()
+    {
+        Livewire::test(NewComponent::class)
+            ->assertMethodHasAttribute('jsMethod', Js::class)
+            ->assertMethodHasAttribute('doubleFoo', On::class, 'fooEvent')
+            ->assertMethodHasAttribute('barMethod', On::class, ['barEvent', 'bazEvent']);
+    }
+
+    public function test_component_has_property_level_attribute()
+    {
+        Livewire::test(NewComponent::class)
+            ->assertPropertyHasAttribute('bar', Session::class, ['key' => 'foo'])
+            ->assertPropertyHasAttribute('foo', Validate::class, ['required', 'integer']);
+    }
+
+    public function test_component_has_property_with_multiple_attributes()
+    {
+        Livewire::test(NewComponent::class)
+            ->assertPropertyHasAttribute('bar', Validate::class, [
+                'rule' => 'required',
+                'as' => 'date of birth',
+            ])
+            ->assertPropertyHasAttribute('baz', Validate::class, [
+                'rule' => 'required',
+                'message' => 'This is a custom message',
+            ])
+            ->assertPropertyHasAttribute('baz', Isolate::class);
+    }
+
+}
+
+#[Lazy(isolate: false)]
+#[Title('Class Level Attribute')]
+class NewComponent extends TestComponent {
+
+    #[Validate(['required', 'integer'])]
+    public ?int $foo = null;
+
+    #[Validate('required', as: 'date of birth')]
+    #[Session(key: 'foo')]
+    public string $bar = 'bar';
+
+    #[Validate('required', message: 'This is a custom message')]
+    #[Validate('integer', message: 'Why is this not an integer?')]
+    #[Isolate]
+    public $baz = 'baz';
+
+    #[Js]
+    public function jsMethod()
+    {
+        return <<<'JS'
+            $wire.on('fooEvent', () => {
+                console.log('fooEvent');
+            });
+        JS;
+    }
+
+    #[On(['barEvent', 'bazEvent'])]
+    public function barMethod() {
+        return 'bar';
+    }
+
+    #[On('fooEvent')]
+    public function doubleFoo() {
+        return $this->foo * 2;
+    }
+}
+
+class HasAllAttributes extends TestComponent {
+
+    #[Validate('required')]
+    public $content = '';
+
+    #[Locked]
+    public $foo = 'bar';
+
+    #[Computed]
+    public function getFoo() {
+        return $this->foo;
+    }
+
+    #[Reactive]
+    public $bar = 'foo';
+
+    #[Modelable]
+    public $baz = 'baz';
+
+    #[LifecycleHookAttribute]
+    public $count2 = 0;
 }
 
 #[\Attribute]
