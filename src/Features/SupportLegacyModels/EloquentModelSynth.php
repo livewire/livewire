8885b60120c3618a5d0c52cbe45e082368347557
François M. <francoism90@users.noreply.github.com>
2024-12-13 14:59:27 +0100

    Check if model exists

--- src/Features/SupportLegacyModels/EloquentModelSynth.php
+++ src/Features/SupportLegacyModels/EloquentModelSynth.php
@@ -72,6 +72,10 @@ class EloquentModelSynth extends Synth
             $model = $this->loadModel($meta);
         }
 
+        if (! $model instanceof Model) {
+            return null;
+        }
+
         if (isset($meta['relations'])) {
             foreach($meta['relations'] as $relationKey) {
                 if (! isset($data[$relationKey])) continue;
