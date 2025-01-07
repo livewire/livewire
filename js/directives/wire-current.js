ee9dcc57338d363dce1e4c4785aa8bcc322d06f2
Josh Hanley <josh@hitsystems.com.au>
2025-01-07 13:31:19 +1100

    Add `.attr` modifier to wire:current

--- js/directives/wire-current.js
+++ js/directives/wire-current.js
@@ -30,10 +30,18 @@ globalDirective('current', ({ el, directive, cleanup }) => {
     let classes = expression.split(' ').filter(String)
 
     let refreshCurrent = url => {
-        if (pathMatches(hrefUrl, url, options)) {
-            el.classList.add(...classes)
+        if (directive.modifiers.includes('attr')) {
+            if (pathMatches(hrefUrl, url, options)) {
+                el.setAttribute(expression, true)
+            } else {
+                el.removeAttribute(expression)
+            }
         } else {
-            el.classList.remove(...classes)
+            if (pathMatches(hrefUrl, url, options)) {
+                el.classList.add(...classes)
+            } else {
+                el.classList.remove(...classes)
+            }
         }
     }
 
