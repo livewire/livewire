a6f5fff872a720182dcb5646af30ac3d53ec4ecf
Josh Hanley <josh@hitsystems.com.au>
2025-04-10 13:16:08 +1000

    Scenario 3 - incorrect keys on elements and components in a loop

--- src/Tests/SnapshotMissingBrowserTest.php
+++ src/Tests/SnapshotMissingBrowserTest.php
@@ -168,4 +168,224 @@ class SnapshotMissingBrowserTest extends \Tests\BrowserTestCase
             ->assertSee('2-1')
             ->assertSee('2-2');
     }
+
+    // https://github.com/livewire/livewire/discussions/8877
+    public function test_scenario_3_keys_on_nested_in_div_passing()
+    {
+        Livewire::visit([
+            new class () extends Component {
+                public $prepend = false;
+
+                #[\Livewire\Attributes\Computed]
+                public function numbers()
+                {
+                    if ($this->prepend) {
+                        return range(0, 2);
+                    }
+
+                    return range(1, 2);
+                }
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <button wire:click="$toggle('prepend')" dusk="prepend">Prepend</button>
+                        <div>
+                            @foreach ($this->numbers as $index => $number)
+                                <div wire:key="{{ $number }}">
+                                    <livewire:child :$number :key="$number" />
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    HTML;
+                }
+            },
+            'child' => new class () extends Component {
+                public $number;
+
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        {{ $number }}
+                    </div>
+                    HTML;
+                }
+            },
+        ])
+            ->waitForLivewireToLoad()
+            ->assertSee('1')
+            ->assertSee('2')
+            ->waitForLivewire()->click('@prepend')
+            ->assertConsoleLogHasNoErrors()
+            ->assertSee('0')
+            ->assertSee('1')
+            ->assertSee('2');
+    }
+
+    // https://github.com/livewire/livewire/discussions/8877
+    public function test_scenario_3_keys_on_nested_in_div_failing_1_no_key_on_nested_component()
+    {
+        Livewire::visit([
+            new class () extends Component {
+                public $prepend = false;
+
+                #[\Livewire\Attributes\Computed]
+                public function numbers()
+                {
+                    if ($this->prepend) {
+                        return range(0, 2);
+                    }
+
+                    return range(1, 2);
+                }
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <button wire:click="$toggle('prepend')" dusk="prepend">Prepend</button>
+                        <div>
+                            @foreach ($this->numbers as $index => $number)
+                                <div wire:key="{{ $number }}">
+                                    <livewire:child :$number />
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    HTML;
+                }
+            },
+            'child' => new class () extends Component {
+                public $number;
+
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        {{ $number }}
+                    </div>
+                    HTML;
+                }
+            },
+        ])
+            ->waitForLivewireToLoad()
+            ->assertSee('1')
+            ->assertSee('2')
+            ->waitForLivewire()->click('@prepend')
+            ->assertConsoleLogHasNoErrors()
+            ->assertSee('0')
+            ->assertSee('1')
+            ->assertSee('2');
+    }
+
+    // https://github.com/livewire/livewire/discussions/8877
+    public function test_scenario_3_keys_on_nested_in_div_failing_2_no_key_on_loop_root_element()
+    {
+        Livewire::visit([
+            new class () extends Component {
+                public $prepend = false;
+
+                #[\Livewire\Attributes\Computed]
+                public function numbers()
+                {
+                    if ($this->prepend) {
+                        return range(0, 2);
+                    }
+
+                    return range(1, 2);
+                }
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <button wire:click="$toggle('prepend')" dusk="prepend">Prepend</button>
+                        <div>
+                            @foreach ($this->numbers as $index => $number)
+                                <div>
+                                    <livewire:child :$number :key="$number" />
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    HTML;
+                }
+            },
+            'child' => new class () extends Component {
+                public $number;
+
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        {{ $number }}
+                    </div>
+                    HTML;
+                }
+            },
+        ])
+            ->waitForLivewireToLoad()
+            ->assertSee('1')
+            ->assertSee('2')
+            ->waitForLivewire()->click('@prepend')
+            ->assertConsoleLogHasNoErrors()
+            ->assertSee('0')
+            ->assertSee('1')
+            ->assertSee('2');
+    }
+
+    // https://github.com/livewire/livewire/discussions/8921
+    public function test_scenario_3_keys_on_nested_in_div_failing_3_incorrect_key_on_loop_root_element()
+    {
+        Livewire::visit([
+            new class () extends Component {
+                public $prepend = false;
+
+                #[\Livewire\Attributes\Computed]
+                public function numbers()
+                {
+                    if ($this->prepend) {
+                        return range(0, 2);
+                    }
+
+                    return range(1, 2);
+                }
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <button wire:click="$toggle('prepend')" dusk="prepend">Prepend</button>
+                        <div>
+                            @foreach ($this->numbers as $index => $number)
+                                <div wire:key="number">
+                                    <livewire:child :$number :key="$number" />
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    HTML;
+                }
+            },
+            'child' => new class () extends Component {
+                public $number;
+
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        {{ $number }}
+                    </div>
+                    HTML;
+                }
+            },
+        ])
+            ->waitForLivewireToLoad()
+            ->assertSee('1')
+            ->assertSee('2')
+            ->waitForLivewire()->click('@prepend')
+            ->assertConsoleLogHasNoErrors()
+            ->assertSee('0')
+            ->assertSee('1')
+            ->assertSee('2');
+    }
 }
