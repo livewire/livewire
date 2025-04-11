2665dc34caad5e102e1476fc29e0ed1b9e6a770e
luanfreitasdev <luanfreitas10@protonmail.com>
2025-04-11 15:32:00 -0300

    Fixing history browser failing tests

--- dist/livewire.js
+++ dist/livewire.js
@@ -713,7 +713,7 @@
     uploadManager.cancelUpload(name, cancelledCallback);
   }
 
-  // ../alpine/packages/alpinejs/dist/module.esm.js
+  // node_modules/alpinejs/dist/module.esm.js
   var flushPending = false;
   var flushing = false;
   var queue = [];
@@ -4824,7 +4824,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     }
   };
 
-  // ../alpine/packages/collapse/dist/module.esm.js
+  // node_modules/@alpinejs/collapse/dist/module.esm.js
   function src_default2(Alpine3) {
     Alpine3.directive("collapse", collapse);
     collapse.inline = (el, { modifiers }) => {
@@ -4918,7 +4918,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   var module_default2 = src_default2;
 
-  // ../alpine/packages/focus/dist/module.esm.js
+  // node_modules/@alpinejs/focus/dist/module.esm.js
   var candidateSelectors = ["input", "select", "textarea", "a[href]", "button", "[tabindex]:not(slot)", "audio[controls]", "video[controls]", '[contenteditable]:not([contenteditable="false"])', "details>summary:first-of-type", "details"];
   var candidateSelector = /* @__PURE__ */ candidateSelectors.join(",");
   var NoElement = typeof Element === "undefined";
@@ -5867,7 +5867,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   var module_default3 = src_default3;
 
-  // ../alpine/packages/persist/dist/module.esm.js
+  // node_modules/@alpinejs/persist/dist/module.esm.js
   function src_default4(Alpine3) {
     let persist = () => {
       let alias;
@@ -5929,7 +5929,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   var module_default4 = src_default4;
 
-  // ../alpine/packages/intersect/dist/module.esm.js
+  // node_modules/@alpinejs/intersect/dist/module.esm.js
   function src_default5(Alpine3) {
     Alpine3.directive("intersect", Alpine3.skipDuringClone((el, { value, expression, modifiers }, { evaluateLater: evaluateLater2, cleanup: cleanup2 }) => {
       let evaluate3 = evaluateLater2(expression);
@@ -5984,7 +5984,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   var module_default5 = src_default5;
 
-  // ../alpine/packages/resize/dist/module.esm.js
+  // node_modules/@alpinejs/resize/dist/module.esm.js
   function src_default6(Alpine3) {
     Alpine3.directive("resize", Alpine3.skipDuringClone((el, { value, expression, modifiers }, { evaluateLater: evaluateLater2, cleanup: cleanup2 }) => {
       let evaluator = evaluateLater2(expression);
@@ -6029,7 +6029,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   var module_default6 = src_default6;
 
-  // ../alpine/packages/anchor/dist/module.esm.js
+  // node_modules/@alpinejs/anchor/dist/module.esm.js
   var min = Math.min;
   var max = Math.max;
   var round = Math.round;
@@ -8161,9 +8161,9 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     let state = window.history.state || {};
     if (!state.alpine)
       state.alpine = {};
-    let isDirty = JSON.stringify(state.alpine[key]) !== JSON.stringify(unwrap(object));
-    if (!isDirty)
+    if (shouldSkipHistoryUpdate(state, key, object)) {
       return;
+    }
     state.alpine[key] = unwrap(object);
     window.history.replaceState(state, "", url.toString());
   }
@@ -8171,11 +8171,20 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     let state = window.history.state || {};
     if (!state.alpine)
       state.alpine = {};
-    let isDirty = JSON.stringify(state.alpine[key]) !== JSON.stringify(unwrap(object));
-    if (!isDirty)
+    if (shouldSkipHistoryUpdate(state, key, object)) {
       return;
-    state = { alpine: { ...state.alpine, ...{ [key]: unwrap(object) } } };
+    }
+    state = { alpine: { ...state.alpine, [key]: unwrap(object) } };
     window.history.pushState(state, "", url.toString());
+  }
+  function shouldSkipHistoryUpdate(currentState, key, newObject) {
+    const currentValue = currentState.alpine?.[key];
+    const newValue = unwrap(newObject);
+    const hasChanges = JSON.stringify(currentValue) !== JSON.stringify(newValue);
+    if (!hasChanges) {
+      return currentValue?.value === newObject.value;
+    }
+    return false;
   }
   function unwrap(object) {
     if (object === void 0)
@@ -8272,7 +8281,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     return data2;
   }
 
-  // ../alpine/packages/morph/dist/module.esm.js
+  // node_modules/@alpinejs/morph/dist/module.esm.js
   function morph(from, toHtml, options) {
     monkeyPatchDomSetAttributeToAllowAtSymbols();
     let fromEl;
@@ -8620,7 +8629,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   var module_default8 = src_default8;
 
-  // ../alpine/packages/mask/dist/module.esm.js
+  // node_modules/@alpinejs/mask/dist/module.esm.js
   function src_default9(Alpine3) {
     Alpine3.directive("mask", (el, { value, expression }, { effect: effect3, evaluateLater: evaluateLater2, cleanup: cleanup2 }) => {
       let templateFn = () => expression;
--- js/plugins/history/index.js
+++ js/plugins/history/index.js
@@ -137,31 +137,44 @@ export function track(name, initialSeedValue, alwaysShow = false, except = null)
 }
 
 function replace(url, key, object) {
-    let state = window.history.state || {}
+    let state = window.history.state || {};
 
-    if (! state.alpine) state.alpine = {}
+    if (!state.alpine) state.alpine = {};
 
-    let isDirty = JSON.stringify(state.alpine[key]) !== JSON.stringify(unwrap(object))
-
-    if (!isDirty) return;
+    if (shouldSkipHistoryUpdate(state, key, object)) {
+        return;
+    }
 
     state.alpine[key] = unwrap(object);
 
-    window.history.replaceState(state, '', url.toString())
+    window.history.replaceState(state, '', url.toString());
 }
 
 function push(url, key, object) {
-    let state = window.history.state || {}
+    let state = window.history.state || {};
 
-    if (! state.alpine) state.alpine = {}
+    if (!state.alpine) state.alpine = {};
 
-    let isDirty = JSON.stringify(state.alpine[key]) !== JSON.stringify(unwrap(object))
+    if (shouldSkipHistoryUpdate(state, key, object)) {
+        return;
+    }
 
-    if (!isDirty) return;
+    state = { alpine: {...state.alpine, [key]: unwrap(object)} };
 
-    state = { alpine: {...state.alpine, ...{[key]: unwrap(object)}} }
+    window.history.pushState(state, '', url.toString());
+}
 
-    window.history.pushState(state, '', url.toString())
+function shouldSkipHistoryUpdate(currentState, key, newObject) {
+    const currentValue = currentState.alpine?.[key];
+    const newValue = unwrap(newObject);
+
+    const hasChanges = JSON.stringify(currentValue) !== JSON.stringify(newValue);
+
+    if (!hasChanges) {
+        return currentValue?.value === newObject.value;
+    }
+
+    return false;
 }
 
 function unwrap(object) {
