7d101ebac1d184a60d8cfad4f5a6b7e46ffee831
Anders Jenbo <anders@jenbo.dk>
2025-04-28 10:13:31 +0200

    Update src/helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -165,7 +165,7 @@ function before($name, $callback) {
  * @return void
  */
 function off($name, $callback) {
-    app(\Livewire\EventBus::class)->off($name, $callback);
+    return app(\Livewire\EventBus::class)->off($name, $callback);
 }
 
 /**
