3b4ceae973e9a341eea7a64a73c8d5e2da0f3a3b
Anders Jenbo <anders@jenbo.dk>
2025-04-30 15:42:03 +0200

    Add types to HandlesEvents
    This resolves warnings when type checking is enabled on projects that use Livewire when they specify the $listeners for a component.

--- src/Features/SupportEvents/HandlesEvents.php
+++ src/Features/SupportEvents/HandlesEvents.php
@@ -6,12 +6,22 @@ use function Livewire\store;
 
 trait HandlesEvents
 {
+    /** @var array<string> */
     protected $listeners = [];
 
+    /**
+     * @return array<string>
+     */
     protected function getListeners() {
         return $this->listeners;
     }
 
+    /**
+     * @param string $event 
+     * @param mixed... $params 
+     *
+     * @return Event 
+     */
     public function dispatch($event, ...$params)
     {
         $event = new Event($event, $params);
