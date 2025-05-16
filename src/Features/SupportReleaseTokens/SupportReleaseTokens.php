3264c60967cb45e34b26119a322127d971a026bc
Josh Hanley <josh@hitsystems.com.au>
2025-05-16 16:19:35 +1000

    Fix lazy components to not check release token on temporary mount params container

--- src/Features/SupportReleaseTokens/SupportReleaseTokens.php
+++ src/Features/SupportReleaseTokens/SupportReleaseTokens.php
@@ -13,6 +13,10 @@ class SupportReleaseTokens extends ComponentHook
         // Use `snapshot-verified` to run the check before any component properties are hydrated
         // but after the snapshot has been verified to ensure it hasn't been tampered with...
         on('snapshot-verified', function ($snapshot) {
+            // The we've mounted a lazy component params container, don't
+            // check the release token as there won't be one...
+            if ($snapshot['memo']['name'] === '__mountParamsContainer') return;
+
             ReleaseToken::verify($snapshot);
         });
 
