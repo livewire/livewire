82b409de7552c72280a86a0fe0fec4785da52cec
Josh Hanley <josh@hitsystems.com.au>
2025-05-20 14:42:56 +1000

    Add more elaborate test

--- src/Tests/RequestPoolingBrowserTest.php
+++ src/Tests/RequestPoolingBrowserTest.php
@@ -62,4 +62,109 @@ class RequestPoolingBrowserTest extends \Tests\BrowserTestCase
             ->pause(600)
             ->assertConsoleLogHasNoErrors();
     }
+
+    public function test_component_not_found_error_is_not_thrown_when_a_faster_component_finishes_first_and_triggers_processing_of_queued_commits()
+    {
+        Livewire::visit([
+            new class () extends Component {
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <livewire:fastchild :item="1" />
+                        <livewire:slowchild :item="2" />
+
+                        {{-- This needs to be here... --}}
+                        <div x-data="{ show: false }" x-cloak>
+                            <div x-show="show"></div>
+                        </div>
+                    </div>
+                    HTML;
+                }
+            },
+            'fastchild' => new #[\Livewire\Attributes\Isolate] class () extends Component {
+                public $item;
+
+                public $show = false;
+
+                public function showChild(): void
+                {
+                    $this->show = true;
+                    usleep(200*1000); // 200ms
+                }
+
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <button wire:click="showChild" class="size-10" dusk="fastchild-show">Show Fast Child</button>
+                        <div>
+                            @if ($show)
+                                <livewire:child :key="$item" :item="$item" />
+                            @endif
+                        </div>
+
+                        {{-- This needs to be here... --}}
+                        <div x-data="{ show: false }" x-cloak>
+                            <div x-show="show"></div>
+                        </div>
+                    </div>
+                    HTML;
+                }
+            },
+            'slowchild' => new class () extends Component {
+                public $item;
+
+                public $show = false;
+
+                public function showChild(): void
+                {
+                    $this->show = true;
+                    usleep(1000*1000); // 1000ms
+                }
+
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        <button wire:click="showChild" class="size-10" dusk="slowchild-show">Show Slow Child</button>
+                        <div>
+                            @if ($show)
+                                <livewire:child :key="$item" :item="$item" />
+                            @endif
+                        </div>
+
+                        {{-- This needs to be here... --}}
+                        <div x-data="{ show: false }" x-cloak>
+                            <div x-show="show"></div>
+                        </div>
+                    </div>
+                    HTML;
+                }
+            },
+            'child' => new class () extends Component {
+                public $item;
+
+                public function render()
+                {
+                    return <<<'HTML'
+                    <div>
+                        Child {{ $item }}
+                    </div>
+                    HTML;
+                }
+            },
+        ])
+            ->waitForLivewireToLoad()
+            ->click('@slowchild-show')
+            // Pause a moment before clicking again to ensure the call is in a new request...
+            ->pause(20)
+            ->click('@slowchild-show')
+            // Pause a moment before clicking again to ensure the call is in a new request...
+            ->pause(20)
+            ->click('@fastchild-show')
+            // Wait for the second slow request to complete...
+            ->pause(1200)
+            ->assertConsoleLogHasNoErrors();
+    }
 }
