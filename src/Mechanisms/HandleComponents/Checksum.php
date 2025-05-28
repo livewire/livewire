03092c9ea87e6d5e1e63fe8ed0e93cbf333aa3f0
Josh Hanley <josh@hitsystems.com.au>
2025-05-29 07:32:39 +1000

    Fix component not found error (#9339)

--- src/Mechanisms/HandleComponents/Checksum.php
+++ src/Mechanisms/HandleComponents/Checksum.php
@@ -22,6 +22,11 @@ class Checksum {
     static function generate($snapshot) {
         $hashKey = app('encrypter')->getKey();
 
+        // Remove the children from the memo in the snapshot, as it is actually Ok
+        // if the "children" tracking is tampered with. This way JavaScript can
+        // modify children as it needs to for dom-diffing purposes...
+        unset($snapshot['memo']['children']);
+        
         $checksum = hash_hmac('sha256', json_encode($snapshot), $hashKey);
 
         trigger('checksum.generate', $checksum, $snapshot);
