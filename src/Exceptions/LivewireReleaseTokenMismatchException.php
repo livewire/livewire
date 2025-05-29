1233603146e76603f9f4d9e67231cd459001a1e1
Josh Hanley <josh@hitsystems.com.au>
2025-05-29 14:28:34 +1000

    Fix exception message

--- src/Exceptions/LivewireReleaseTokenMismatchException.php
+++ src/Exceptions/LivewireReleaseTokenMismatchException.php
@@ -11,7 +11,7 @@ class LivewireReleaseTokenMismatchException extends HttpException
         parent::__construct(
             419,
             "Livewire detected a release token mismatch. \n".
-            "This happens when Livewire's, the application's, or a component's release token has changed since this page was loaded."
+            "This happens when a user's browser session has been invalidated by a new deployment."
         );
     }
 }
