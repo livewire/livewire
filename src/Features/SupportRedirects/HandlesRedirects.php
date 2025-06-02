22f13a23fe55651805f442323efbd999b0663a2f
ghabriel25 <151924216+ghabriel25@users.noreply.github.com>
2025-06-02 10:50:58 +0700

    Redirect action usage

--- src/Features/SupportRedirects/HandlesRedirects.php
+++ src/Features/SupportRedirects/HandlesRedirects.php
@@ -10,7 +10,9 @@ trait HandlesRedirects
     {
         store($this)->set('redirect', $url);
 
-        if ($navigate) store($this)->set('redirectUsingNavigate', true);
+        if ($navigate) {
+            store($this)->set('redirectUsingNavigate', true);
+        }
 
         $shouldSkipRender = ! config('livewire.render_on_redirect', false);
 
@@ -25,7 +27,7 @@ trait HandlesRedirects
     public function redirectIntended($default = '/', $navigate = false)
     {
         $url = session()->pull('url.intended', $default);
-    
+
         $this->redirect($url, $navigate);
     }
 
