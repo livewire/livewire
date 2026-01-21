2efa1c2885a643569f555680151cbc516b30a0d7
Josh Hanley <josh@hitsystems.com.au>
2026-01-22 07:46:55 +1000

    Restore Compiler changes

--- src/Compiler/CacheManager.php
+++ src/Compiler/CacheManager.php
@@ -113,6 +113,7 @@ class CacheManager
 
         $classPath = $this->getClassPath($sourcePath);
 
+        $this->ensureCacheDirectoryExists();
         File::ensureDirectoryExists($this->cacheDirectory . '/classes');
 
         File::put($classPath, $contents);
@@ -122,6 +123,7 @@ class CacheManager
     {
         $viewPath = $this->getViewPath($sourcePath);
 
+        $this->ensureCacheDirectoryExists();
         File::ensureDirectoryExists($this->cacheDirectory . '/views');
 
         File::put($viewPath, $contents);
@@ -133,6 +135,7 @@ class CacheManager
     {
         $scriptPath = $this->getScriptPath($sourcePath);
 
+        $this->ensureCacheDirectoryExists();
         File::ensureDirectoryExists($this->cacheDirectory . '/scripts');
 
         File::put($scriptPath, $contents);
@@ -142,6 +145,7 @@ class CacheManager
     {
         $stylePath = $this->getStylePath($sourcePath);
 
+        $this->ensureCacheDirectoryExists();
         File::ensureDirectoryExists($this->cacheDirectory . '/styles');
 
         File::put($stylePath, $contents);
@@ -151,6 +155,7 @@ class CacheManager
     {
         $stylePath = $this->getGlobalStylePath($sourcePath);
 
+        $this->ensureCacheDirectoryExists();
         File::ensureDirectoryExists($this->cacheDirectory . '/styles');
 
         File::put($stylePath, $contents);
@@ -160,6 +165,7 @@ class CacheManager
     {
         $placeholderPath = $this->getPlaceholderPath($sourcePath);
 
+        $this->ensureCacheDirectoryExists();
         File::ensureDirectoryExists($this->cacheDirectory . '/placeholders');
 
         File::put($placeholderPath, $contents);
@@ -171,6 +177,7 @@ class CacheManager
     {
         $viewPath = $this->getViewPath($sourcePath);
 
+        $this->ensureCacheDirectoryExists();
         File::ensureDirectoryExists($this->cacheDirectory . '/views');
 
         File::put($viewPath, $contents);
@@ -182,6 +189,21 @@ class CacheManager
     {
         if (function_exists('opcache_invalidate')) {
             opcache_invalidate($sourcePath, true);
+        }
+    }
+
+    protected function ensureCacheDirectoryExists(): void
+    {
+        // Same approach as Blade's Compiler::ensureCompiledDirectoryExists()
+        // @see \Illuminate\View\Compilers\Compiler::ensureCompiledDirectoryExists()
+        if (! is_dir($this->cacheDirectory)) {
+            File::makeDirectory($this->cacheDirectory, 0777, true, true);
+        }
+
+        $gitignorePath = $this->cacheDirectory . '/.gitignore';
+
+        if (! file_exists($gitignorePath)) {
+            File::put($gitignorePath, "*\n!.gitignore");
         }
     }
 
@@ -200,8 +222,16 @@ class CacheManager
 
     public function clearCompiledFiles($output = null): void
     {
-        if (is_dir($this->cacheDirectory)) {
-            File::deleteDirectory($this->cacheDirectory);
+        try {
+            if (is_dir($this->cacheDirectory)) {
+                File::deleteDirectory($this->cacheDirectory);
+            }
+        } catch (\Exception $e) {
+            // Silently fail to avoid breaking view:clear if there's an issue
+            // But we can log it if output is available
+            if ($output && method_exists($output, 'writeln')) {
+                $output->writeln("<comment>Note: Could not clear Livewire compiled files.</comment>");
+            }
         }
     }
 }
