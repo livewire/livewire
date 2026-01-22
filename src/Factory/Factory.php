226238435adac9024e99a063077c911b764ae358
Josh Hanley <josh@hitsystems.com.au>
2026-01-23 09:20:09 +1000

    Fix resolveMissingComponent not working with Octane
    Change Factory from scoped to singleton binding and add flush-state
    handler to clear per-request cache while preserving resolver callbacks.

--- src/Factory/Factory.php
+++ src/Factory/Factory.php
@@ -6,6 +6,7 @@ use Livewire\Exceptions\ComponentNotFoundException;
 use Livewire\Compiler\Compiler;
 use Livewire\Finder\Finder;
 use Livewire\Component;
+use function Livewire\on;
 
 class Factory
 {
@@ -16,7 +17,11 @@ class Factory
     public function __construct(
         protected Finder $finder,
         protected Compiler $compiler,
-    ) {}
+    ) {
+        on('flush-state', function () {
+            $this->resolvedComponentCache = [];
+        });
+    }
 
     public function create($name, $id = null): Component
     {
