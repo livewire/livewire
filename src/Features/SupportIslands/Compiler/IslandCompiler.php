3bbf96ba6a8b08000c0130b8befae225bdcfd776
Josh Hanley <josh@hitsystems.com.au>
2026-01-27 09:57:09 +1000

    Extract comment handling into dedicated methods

--- src/Features/SupportIslands/Compiler/IslandCompiler.php
+++ src/Features/SupportIslands/Compiler/IslandCompiler.php
@@ -46,19 +46,11 @@ class IslandCompiler
         }
 
         // Strip Blade comments so @island directives inside them are ignored...
-        $comments = [];
-        $contents = preg_replace_callback('/\{\{--(.*?)--\}\}/s', function ($match) use (&$comments) {
-            $placeholder = '[BLADE_COMMENT:' . count($comments) . ']';
-            $comments[] = $match[0];
-            return $placeholder;
-        }, $this->mutableContents);
+        [$contents, $comments] = $this->stripBladeComments($this->mutableContents);
 
         $result = $compiler->compileStatementsMadePublic($contents);
 
-        // Restore Blade comments...
-        $result = preg_replace_callback('/\[BLADE_COMMENT:(\d+)\]/', function ($match) use ($comments) {
-            return $comments[(int) $match[1]];
-        }, $result);
+        $result = $this->restoreBladeComments($result, $comments);
 
         for ($i=$maxNestingLevel; $i >= $currentNestingLevel; $i--) {
             $result = preg_replace_callback('/(\[STARTISLAND:([0-9]+):' . $i . '\])\((.*?)\)(.*?)(\[ENDISLAND:' . $i . '\])/s', function ($matches) use ($i) {
@@ -160,6 +152,26 @@ PHP;
         return app('livewire.compiler')->cacheManager->getHash(
             $this->pathSignature,
         );
+    }
+
+    protected function stripBladeComments(string $contents): array
+    {
+        $comments = [];
+
+        $contents = preg_replace_callback('/\{\{--(.*?)--\}\}/s', function ($match) use (&$comments) {
+            $placeholder = '[BLADE_COMMENT:' . count($comments) . ']';
+            $comments[] = $match[0];
+            return $placeholder;
+        }, $contents);
+
+        return [$contents, $comments];
+    }
+
+    protected function restoreBladeComments(string $contents, array $comments): string
+    {
+        return preg_replace_callback('/\[BLADE_COMMENT:(\d+)\]/', function ($match) use ($comments) {
+            return $comments[(int) $match[1]];
+        }, $contents);
     }
 
     public function getHackedBladeCompiler()
