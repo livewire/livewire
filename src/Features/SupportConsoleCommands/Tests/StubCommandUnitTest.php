0b2af05e97d11f51038df38cdeab6687804613cc
Josh Hanley <josh@hitsystems.com.au>
2026-02-09 21:21:54 +1000

    Update `StubCommandUnitTest` to assert all new stubs are created

--- src/Features/SupportConsoleCommands/Tests/StubCommandUnitTest.php
+++ src/Features/SupportConsoleCommands/Tests/StubCommandUnitTest.php
@@ -25,6 +25,13 @@ class StubCommandUnitTest extends \Tests\TestCase
         $this->assertTrue(File::exists($this->stubsPath('livewire.pest.stub')));
         $this->assertTrue(File::exists($this->stubsPath('livewire.form.stub')));
         $this->assertTrue(File::exists($this->stubsPath('livewire.attribute.stub')));
+        $this->assertTrue(File::exists($this->stubsPath('livewire.layout.stub')));
+        $this->assertTrue(File::exists($this->stubsPath('livewire-sfc.stub')));
+        $this->assertTrue(File::exists($this->stubsPath('livewire-mfc-class.stub')));
+        $this->assertTrue(File::exists($this->stubsPath('livewire-mfc-view.stub')));
+        $this->assertTrue(File::exists($this->stubsPath('livewire-mfc-test.stub')));
+        $this->assertTrue(File::exists($this->stubsPath('livewire-mfc-js.stub')));
+        $this->assertTrue(File::exists($this->stubsPath('livewire-mfc-css.stub')));
     }
 
     public function test_component_is_created_with_view_and_class_custom_default_stubs()
