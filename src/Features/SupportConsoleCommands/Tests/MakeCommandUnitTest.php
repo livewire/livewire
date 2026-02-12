438f1965980829759732bc00d12f42ed37a953c6
Josh Hanley <josh@hitsystems.com.au>
2026-02-13 08:40:31 +1000

    Use unique component name in test to avoid collision with earlier test

--- src/Features/SupportConsoleCommands/Tests/MakeCommandUnitTest.php
+++ src/Features/SupportConsoleCommands/Tests/MakeCommandUnitTest.php
@@ -870,13 +870,13 @@ class MakeCommandUnitTest extends \Tests\TestCase
     {
         // The "pages" namespace is registered as a view namespace only (via component_namespaces config),
         // not a class namespace. Creating a class-based component with it should show an error, not crash.
-        $exitCode = Artisan::call('make:livewire', ['name' => 'pages::create-post', '--class' => true]);
+        $exitCode = Artisan::call('make:livewire', ['name' => 'pages::edit-post', '--class' => true]);
 
         $this->assertEquals(1, $exitCode);
         $this->assertStringContainsString('Class namespace [pages] not found', Artisan::output());
 
         // Ensure no files were created
-        $this->assertFalse(File::exists($this->livewireClassesPath('CreatePost.php')));
+        $this->assertFalse(File::exists($this->livewireClassesPath('EditPost.php')));
     }
 
     public function test_unregistered_namespace_shows_error_for_sfc()
