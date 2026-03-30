3d8a2324e0b029cc39541d98eddb8abcd9d0d99d
Filip Ganyicz <ganyicz.filip@gmail.com>
2026-03-30 23:40:20 +0200

    Revert "Replace verbose waitUntil with waitForTextIn in AlpineUi tests"
    This reverts commit 640064b6979bc7551fd8cfc8fb0b6ec5a06c9020.

--- src/Tests/AlpineUiBrowserTest.php
+++ src/Tests/AlpineUiBrowserTest.php
@@ -56,7 +56,7 @@ class AlpineUiBrowserTest extends \Tests\BrowserTestCase
             }
         })
             ->waitForLivewireToLoad()
-            ->waitForTextIn('@updatesCount', '1')
+            ->waitUntil('document.querySelector("[dusk=updatesCount]")?.textContent.trim() === "1"', 15)
             ->click('@openListbox')
             ->waitFor('@listboxOption')
             ->assertSeeIn('@updatesCount', '1')
@@ -134,7 +134,7 @@ class AlpineUiBrowserTest extends \Tests\BrowserTestCase
             }
         })
             ->waitForLivewireToLoad()
-            ->waitForTextIn('@updatesCount', '1')
+            ->waitUntil('document.querySelector("[dusk=updatesCount]")?.textContent.trim() === "1"', 15)
             ->click('@openCombobox')
             ->waitFor('@comboboxOption')
             ->assertSeeIn('@updatesCount', '1')
