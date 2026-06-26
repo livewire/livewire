6bc0c0b5b17442b648c9c1e92693d681994856f7
Hamdy Elbatal <elbatalhamdy77@gmail.com>
2026-06-26 14:42:49 +0300

    [4.x] Preserve named error bags during component rendering (#10314)
    Co-authored-by: Josh Hanley <josh@hitsystems.com.au>

--- src/Features/SupportQueryString/BrowserTest.php
+++ src/Features/SupportQueryString/BrowserTest.php
@@ -437,12 +437,12 @@ class BrowserTest extends \Tests\BrowserTestCase
             },
         ])
             ->assertInputValue('@search', 'hello')
-            ->assertScript('return window.location.search', '?form[search]=hello')
+            ->assertScript('return decodeURIComponent(window.location.search)', '?form[search]=hello')
             ->waitForLivewire()->type('@search', 'world')
-            ->assertScript('return window.location.search', '?form[search]=world')
+            ->assertScript('return decodeURIComponent(window.location.search)', '?form[search]=world')
             ->refresh()
             ->assertInputValue('@search', 'world')
-            ->assertScript('return window.location.search', '?form[search]=world')
+            ->assertScript('return decodeURIComponent(window.location.search)', '?form[search]=world')
         ;
     }
 
@@ -1133,7 +1133,7 @@ class BrowserTest extends \Tests\BrowserTestCase
                     }
                 }
             ])
-            ->assertScript('return window.location.search', '?foo[bar]=baz');
+            ->assertScript('return decodeURIComponent(window.location.search)', '?foo[bar]=baz');
     }
 
     public function test_it_skips_query_string_encoded_keys_not_tracked_by_livewire()
@@ -1157,7 +1157,7 @@ class BrowserTest extends \Tests\BrowserTestCase
                     }
                 }
             ])
-            ->assertScript('return window.location.search', '?foo[bar]=baz&bob%5Blob%5D=law');
+            ->assertScript('return decodeURIComponent(window.location.search)', '?foo[bar]=baz&bob[lob]=law');
     }
 
     public function test_except_does_remove_value_from_query_string_when_loaded_with_value_then_changed_to_except_value()
