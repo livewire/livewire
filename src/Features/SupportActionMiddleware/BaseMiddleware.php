6027b30c3bd3c57ce3873af9f3018972d8fba03a
ghabriel <rihulfaakbar@gmail.com>
2026-06-30 12:19:38 +0700

    Exclude authorization middleware

--- src/Features/SupportActionMiddleware/BaseMiddleware.php
+++ src/Features/SupportActionMiddleware/BaseMiddleware.php
@@ -3,6 +3,7 @@
 namespace Livewire\Features\SupportActionMiddleware;
 
 use Attribute;
+use Illuminate\Auth\Middleware\Authorize as AuthorizeMiddleware;
 use Illuminate\Support\Arr;
 use Livewire\Drawer\Utils;
 use Livewire\Features\SupportAttributes\Attribute as LivewireAttribute;
@@ -10,14 +11,19 @@ use Livewire\Features\SupportAttributes\Attribute as LivewireAttribute;
 #[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
 class BaseMiddleware extends LivewireAttribute
 {
-    public function __construct(public array|string $middleware)
+    public function __construct(public string $middleware)
     {
         //
     }
 
     public function call()
     {
-        $middleware = app('router')->resolveMiddleware(Arr::wrap($this->middleware));
+        // Remove authorization middleware as we already have `#[Authorize]` attribute
+        $argument = array_filter(Arr::wrap($this->middleware), function ($m) {
+            return ! str_contains($m, 'can:') && $m !== AuthorizeMiddleware::class;
+        });
+
+        $middleware = app('router')->resolveMiddleware($argument);
 
         if ($middleware === []) return;
 
