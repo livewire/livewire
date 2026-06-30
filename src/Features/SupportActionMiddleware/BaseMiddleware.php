65f606ee769497f28443d0099ef7e7eea9e342ab
ghabriel <rihulfaakbar@gmail.com>
2026-06-30 08:55:45 +0700

    Filter component attributes instead using ReflectionMethod

--- src/Features/SupportActionMiddleware/BaseMiddleware.php
+++ src/Features/SupportActionMiddleware/BaseMiddleware.php
@@ -3,50 +3,24 @@
 namespace Livewire\Features\SupportActionMiddleware;
 
 use Attribute;
-use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use Livewire\Drawer\Utils;
 use Livewire\Features\SupportAttributes\Attribute as LivewireAttribute;
-use Livewire\Features\SupportRedirects\SupportRedirects;
 
 #[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
 class BaseMiddleware extends LivewireAttribute
 {
-    public function __construct(public string $middleware)
+    public function __construct(public array|string $middleware)
     {
         //
     }
 
     public function call()
     {
-        // Restore Laravel's redirector back into container
-        app()->instance('redirect', array_pop(SupportRedirects::$redirectorCacheStack));
+        $middleware = app('router')->resolveMiddleware(Arr::wrap($this->middleware));
 
-        $middleware = $this->parseMiddleware($this->middleware);
-        
-        if (is_null($middleware)) return;
+        if ($middleware === []) return;
 
-        Utils::applyMiddleware(request(), [$middleware]);
-    }
-
-    protected function parseMiddleware($middleware)
-    {
-        if (class_exists($middleware)) {
-            return $middleware;
-        }
-
-        // Using `getMiddleware()` to get middleware aliases as array keys
-        $routeMiddleware = app('router')->getMiddleware();
-
-        $name = Str::before($middleware, ':');
-
-        if (! isset($routeMiddleware[$name])) {
-            return null;
-        }
-
-        if (str_contains($middleware, ':')) {
-            return $routeMiddleware[$name] . ':' . Str::after($middleware, ':');
-        }
-
-        return $routeMiddleware[$name];
+        Utils::applyMiddleware(request(), $middleware);
     }
 }
---
+++ src/Features/SupportActionMiddleware/SupportActionMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Livewire\Features\SupportActionMiddleware;
+
+use Livewire\ComponentHook;
+use Livewire\Features\SupportRedirects\SupportRedirects;
+
+use function Livewire\on;
+
+class SupportActionMiddleware extends ComponentHook
+{
+    public static function provide()
+    {
+        on('call', function ($component, $method, $params, $componentContext, $earlyReturn) {
+            if (static::hasMiddlewareAttribute($component, $method)) {
+                app()->instance('redirect', array_pop(SupportRedirects::$redirectorCacheStack));
+            }
+        });
+    }
+
+    static function hasMiddlewareAttribute($component, $method)
+    {
+        return $component->getAttributes()
+            ->filter(fn ($attr) => $attr instanceof BaseMiddleware)
+            ->filter(fn ($attr) => $attr->getName() === $method)
+            ->isNotEmpty();
+    }
+}
