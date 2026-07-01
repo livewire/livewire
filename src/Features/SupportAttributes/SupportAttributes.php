764a190064aadb63d5ae463342638042bd42d68b
ghabriel <rihulfaakbar@gmail.com>
2026-07-01 10:35:03 +0700

    Refactor SupportAttributes

--- src/Features/SupportAttributes/SupportAttributes.php
+++ src/Features/SupportAttributes/SupportAttributes.php
@@ -9,45 +9,40 @@ class SupportAttributes extends ComponentHook
 {
     function boot(...$params)
     {
-        $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
-            ->each(function ($attribute) use ($params) {
-                if (method_exists($attribute, 'boot')) {
-                    $attribute->boot(...$params);
-                }
-            });
+        $callback = function ($attribute) use ($params) {
+            if (method_exists($attribute, 'boot')) {
+                $attribute->boot(...$params);
+            }
+        };
+
+        $this->invokeLivewireAttribute($callback);
     }
 
     function mount(...$params)
     {
-        $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
-            ->each(function ($attribute) use ($params) {
-                if (method_exists($attribute, 'mount')) {
-                    $attribute->mount(...$params);
-                }
-            });
+        $callback = function ($attribute) use ($params) {
+            if (method_exists($attribute, 'mount')) {
+                $attribute->mount(...$params);
+            }
+        };
+
+        $this->invokeLivewireAttribute($callback);
     }
 
     function hydrate(...$params)
     {
-        $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
-            ->each(function ($attribute) use ($params) {
-                if (method_exists($attribute, 'hydrate')) {
-                    $attribute->hydrate(...$params);
-                }
-            });
+        $callback = function ($attribute) use ($params) {
+            if (method_exists($attribute, 'hydrate')) {
+                $attribute->hydrate(...$params);
+            }
+        };
+
+        $this->invokeLivewireAttribute($callback);
     }
 
     function update($propertyName, $fullPath, $newValue)
     {
-        $callbacks = $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
+        $callbacks = $this->getLivewireAttributes()
             ->filter(fn ($attr) => $attr->getLevel() === AttributeLevel::PROPERTY)
             // Call "update" on the root property attribute even if it's a deep update...
             ->filter(fn ($attr) => str($fullPath)->startsWith($attr->getName() . '.') || $fullPath === $attr->getName())
@@ -66,9 +61,7 @@ class SupportAttributes extends ComponentHook
 
     function call($method, $params, $returnEarly)
     {
-        $callbacks = $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
+        $callbacks = $this->getLivewireAttributes()
             ->filter(fn ($attr) => $attr->getLevel() === AttributeLevel::METHOD)
             ->filter(fn ($attr) => $attr->getName() === $method)
             ->map(function ($attribute) use ($params, $returnEarly) {
@@ -86,9 +79,7 @@ class SupportAttributes extends ComponentHook
 
     function render(...$params)
     {
-        $callbacks = $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
+        $callbacks = $this->getLivewireAttributes()
             ->map(function ($attribute) use ($params) {
                 if (method_exists($attribute, 'render')) {
                     return $attribute->render(...$params);
@@ -106,38 +97,46 @@ class SupportAttributes extends ComponentHook
 
     function dehydrate(...$params)
     {
-        $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
-            ->each(function ($attribute) use ($params) {
-                if (method_exists($attribute, 'dehydrate')) {
-                    $attribute->dehydrate(...$params);
-                }
-            });
+        $callback = function ($attribute) use ($params) {
+            if (method_exists($attribute, 'dehydrate')) {
+                $attribute->dehydrate(...$params);
+            }
+        };
+
+        $this->invokeLivewireAttribute($callback);
     }
 
     function destroy(...$params)
     {
-        $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
-            ->each(function ($attribute) use ($params) {
-                if (method_exists($attribute, 'destroy')) {
-                    $attribute->destroy(...$params);
-                }
-            });
+        $callback = function ($attribute) use ($params) {
+            if (method_exists($attribute, 'destroy')) {
+                $attribute->destroy(...$params);
+            }
+        };
+
+        $this->invokeLivewireAttribute($callback);
     }
 
     function exception(...$params)
     {
-        $this->component
-            ->getAttributes()
-            ->whereInstanceOf(LivewireAttribute::class)
-            ->each(function ($attribute) use ($params) {
-                if (method_exists($attribute, 'exception')) {
-                    $attribute->exception(...$params);
-                }
-            });
+        $callback = function ($attribute) use ($params) {
+            if (method_exists($attribute, 'exception')) {
+                $attribute->exception(...$params);
+            }
+        };
+
+        $this->invokeLivewireAttribute($callback);
     }
 
+    protected function invokeLivewireAttribute(callable $callback)
+    {
+        $this->getLivewireAttributes()->each($callback);
+    }
+
+    protected function getLivewireAttributes()
+    {
+        return $this->component
+            ->getAttributes()
+            ->whereInstanceOf(LivewireAttribute::class);
+    }
 }
