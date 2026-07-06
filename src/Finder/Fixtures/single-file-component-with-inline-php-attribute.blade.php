6629a13cfcd4971ce00154ee0108f5ed08ff5daf
Josh Hanley <josh@hitsystems.com.au>
2026-07-07 05:36:46 +1000

    [4.x] Skip Volt functional components during SFC detection (#10367)

---
+++ src/Finder/Fixtures/single-file-component-with-inline-php-attribute.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+new #[Layout('layouts.app')] #[Title('Dashboard')] class extends Component
+{
+    //
+};
+?>
+
+<div>Finder Test Single File Component With Inline Attribute</div>
