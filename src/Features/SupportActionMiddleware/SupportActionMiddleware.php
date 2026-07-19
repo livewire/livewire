cc92c83dfb2c23f4e3d87b031077511816dfb112
ghabriel <rihulfaakbar@gmail.com>
2026-07-19 09:04:30 +0700

    Improve middleware filtering

--- src/Features/SupportActionMiddleware/SupportActionMiddleware.php
+++ src/Features/SupportActionMiddleware/SupportActionMiddleware.php
@@ -7,7 +7,9 @@ use Livewire\ComponentHook;
 use Livewire\Drawer\Utils;
 use Livewire\Exceptions\EventHandlerDoesNotExist;
 use Livewire\Features\SupportEvents\SupportEvents;
+use Livewire\Mechanisms\PersistentMiddleware\PersistentMiddleware;
 
+use function Livewire\invade;
 use function Livewire\on;
 
 class SupportActionMiddleware extends ComponentHook
@@ -21,10 +23,12 @@ class SupportActionMiddleware extends ComponentHook
 
             if (empty($actionMiddleware)) return;
 
-            $resolved = static::resolveMiddleware($actionMiddleware);
+            [$request, $resolved] = static::resolveMiddleware($actionMiddleware);
+
+            if (empty($resolved)) return;
 
             // Gather all action middleware from method and apply it all at once
-            Utils::applyMiddleware(request(), $resolved);
+            Utils::applyMiddleware($request, $resolved);
         });
     }
 
@@ -57,11 +61,20 @@ class SupportActionMiddleware extends ComponentHook
 
     protected static function resolveMiddleware(array $middleware): array
     {
-        return collect(app('router')->resolveMiddleware($middleware))
+        $mechanism = invade(app(PersistentMiddleware::class));
+
+        $request = $mechanism->makeFakeRequest();
+
+        $applicableMiddleware = $mechanism->getApplicablePersistentMiddleware($request);
+
+        // Since PersistentMiddleware runs first, we need to exclude any middleware
+        // that has been applied from it along with authorization middleware
+        $resolved = collect(app('router')->resolveMiddleware($middleware, $applicableMiddleware))
             ->filter(fn ($m) => is_string($m))
-            // Exclude any authorization middleware since we already have `#[Authorize]` attribute
             ->reject(fn ($m) => str_starts_with($m, AuthorizeMiddleware::class))
             ->values()
             ->all();
+
+        return [$request, $resolved];
     }
 }
