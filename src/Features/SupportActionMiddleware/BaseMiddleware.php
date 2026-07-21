38044718c429ae6ff3c599829159cd4bed911c79
ghabriel <rihulfaakbar@gmail.com>
2026-07-21 12:45:12 +0700

    Store middleware attributes by method name

--- src/Features/SupportActionMiddleware/BaseMiddleware.php
+++ src/Features/SupportActionMiddleware/BaseMiddleware.php
@@ -5,11 +5,24 @@ namespace Livewire\Features\SupportActionMiddleware;
 use Attribute;
 use Livewire\Features\SupportAttributes\Attribute as LivewireAttribute;
 
+use function Livewire\store;
+
 #[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
 class BaseMiddleware extends LivewireAttribute
 {
     public function __construct(public string $middleware)
     {
         //
+    }
+
+    function boot()
+    {
+        $store = store($this->component);
+
+        $middleware = $store->find('middlewareAttributes', $this->getName(), []);
+
+        $middleware[] = $this->middleware;
+
+        $store->push('middlewareAttributes', $middleware, $this->getName());
     }
 }
