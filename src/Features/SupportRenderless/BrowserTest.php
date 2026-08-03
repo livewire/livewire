f332df347cbd274d9d83f2c62777482acfe3ef37
TWithers <timothy.withers@gmail.com>
2026-08-03 14:21:57 -0700

    Add browser test for renderless action batched with a normal action
    Drives the real JS batch (`$wire.renderlessAction(); $wire.updateName()`
    collapsed into one request) and asserts the DOM reflects the normal
    action's state change, guarding the render-skip regression end to end.

--- src/Features/SupportRenderless/BrowserTest.php
+++ src/Features/SupportRenderless/BrowserTest.php
@@ -81,4 +81,31 @@ class BrowserTest extends BrowserTestCase
             ->waitForLivewire()->click('@button')
             ->assertSeeIn('@button', '1');
     }
+
+    public function test_render_is_not_skipped_when_a_renderless_action_is_batched_with_a_normal_action()
+    {
+        Livewire::visit(new class extends Component {
+            public $name = 'foo';
+
+            #[BaseRenderless]
+            function renderlessAction() { }
+
+            function updateName()
+            {
+                $this->name = 'bar';
+            }
+
+            function render()
+            {
+                return Blade::render(<<<'HTML'
+                <div>
+                    <button x-on:click="$wire.renderlessAction(); $wire.updateName()" dusk="button">{{ $name }}</button>
+                </div>
+                HTML, ['name' => $this->name]);
+            }
+        })
+            ->assertSeeIn('@button', 'foo')
+            ->waitForLivewire()->click('@button')
+            ->assertSeeIn('@button', 'bar');
+    }
 }
