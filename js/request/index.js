83655a20de1a2cc6f83e60c777dd1ef95fb4d194
Josh Hanley <josh@hitsystems.com.au>
2026-08-06 11:54:27 +1000

    Finish request profiling before fetch redirect

--- js/request/index.js
+++ js/request/index.js
@@ -142,6 +142,8 @@ export async function sendRequest(pool) {
     if (response.redirected) {
         window.location.href = response.url
 
+        finishProfile({ content, failed: false })
+
         return
     }
 
