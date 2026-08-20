beb29a6c880f5044c19a6d96a5304f2aef2757b3
Caleb Porzio <calebporzio@gmail.com>
2026-08-20 08:10:03 -0400

    Ignore repeated lazy-load calls (#10562)

--- src/Features/SupportLazyLoading/UnitTest.php
+++ src/Features/SupportLazyLoading/UnitTest.php
@@ -7,6 +7,7 @@ use Livewire\Attributes\Defer;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Lazy;
 use Livewire\Component;
+use Livewire\Exceptions\MethodNotFoundException;
 use Livewire\Livewire;
 use Livewire\Mechanisms\HandleComponents\CorruptComponentPayloadException;
 
@@ -90,6 +91,38 @@ class UnitTest extends \Tests\TestCase
             ->assertSee('level:5');
     }
 
+    public function test_a_repeat_lazy_load_call_is_ignored_after_the_component_has_loaded()
+    {
+        SupportLazyLoading::$disableWhileTesting = false;
+
+        Livewire::component('lazy-alpha', LazyAlpha::class);
+
+        $component = Livewire::test('lazy-alpha', ['level' => 5]);
+
+        preg_match("/__lazyLoad\('([^']+)'\)/", html_entity_decode($component->html()), $matches);
+
+        $component
+            ->call('__lazyLoad', $matches[1])
+            ->assertSee('level:5')
+            ->call('__lazyLoad', $matches[1])
+            ->assertSee('level:5')
+            ->assertSee('mounts:1')
+            ->call('__lazyLoad', $matches[1])
+            ->assertSee('level:5')
+            ->assertSee('mounts:1');
+    }
+
+    public function test_a_lazy_load_call_on_a_non_lazy_component_is_not_claimed()
+    {
+        $this->expectException(MethodNotFoundException::class);
+
+        Livewire::test(new class extends Component {
+            public function render() {
+                return '<div></div>';
+            }
+        })->call('__lazyLoad', 'invalid');
+    }
+
     public function test_a_mount_params_container_is_scoped_to_its_own_component()
     {
         SupportLazyLoading::$disableWhileTesting = false;
@@ -111,9 +144,11 @@ class UnitTest extends \Tests\TestCase
 #[Lazy]
 class LazyAlpha extends Component {
     public $level = 0;
+    public $mounts = 0;
 
     public function mount($level = 0) {
         $this->level = $level;
+        $this->mounts++;
     }
 
     public function placeholder() {
@@ -121,7 +156,7 @@ class LazyAlpha extends Component {
     }
 
     public function render() {
-        return '<div>level:'.$this->level.'</div>';
+        return '<div>level:'.$this->level.' mounts:'.$this->mounts.'</div>';
     }
 }
 
@@ -181,4 +216,3 @@ class PageWithCustomLayoutOnView extends Component {
         return view('show-name', ['name' => 'foo'])->layout('components.layouts.custom');
     }
 }
-
