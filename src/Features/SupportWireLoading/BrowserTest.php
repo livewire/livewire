b80ff87ab67167aef15db7eab1020f2961a64e2f
Caleb Porzio <calebporzio@gmail.com>
2026-08-31 10:11:47 -0400

    [3.x] Backport wire:loading restoration and minute polling (#10639)
    Co-authored-by: Josh Hanley <josh@hitsystems.com.au>

--- src/Features/SupportWireLoading/BrowserTest.php
+++ src/Features/SupportWireLoading/BrowserTest.php
@@ -104,6 +104,84 @@ class BrowserTest extends \Tests\BrowserTestCase
         ;
     }
 
+    function test_wire_loading_attr_restores_existing_attribute_after_renderless_action()
+    {
+        Livewire::visit(new class extends Component {
+            public bool $disabled = true;
+
+            #[\Livewire\Attributes\Renderless]
+            public function renderlessAction() {
+                usleep(250000);
+            }
+
+            public function render() {
+                return <<<'HTML'
+                    <div>
+                        <button type="button" wire:click="renderlessAction" dusk="renderless">
+                            Renderless action
+                        </button>
+
+                        <button
+                            type="button"
+                            x-bind:disabled="$wire.$get('disabled')"
+                            wire:loading.attr="disabled"
+                            dusk="target">
+                            Target
+                        </button>
+                    </div>
+                HTML;
+            }
+        })
+        ->waitUntil('document.querySelector(\'[dusk="target"]\').disabled === true')
+        ->waitForLivewire()->click('@renderless')
+        ->assertScript('document.querySelector(\'[dusk="target"]\').disabled', true)
+        ;
+    }
+
+    function test_wire_loading_class_restores_existing_class_state_after_renderless_action()
+    {
+        Livewire::visit(new class extends Component {
+            #[\Livewire\Attributes\Renderless]
+            public function renderlessAction() {
+                usleep(250000);
+            }
+
+            public function render() {
+                return <<<'HTML'
+                    <div>
+                        <button type="button" wire:click="renderlessAction" dusk="renderless">
+                            Renderless action
+                        </button>
+
+                        <div
+                            class="existing"
+                            wire:loading.class="existing added"
+                            dusk="add">
+                            Add classes
+                        </div>
+
+                        <div
+                            class="existing"
+                            wire:loading.class.remove="existing absent"
+                            dusk="remove">
+                            Remove classes
+                        </div>
+                    </div>
+                HTML;
+            }
+        })
+        ->assertHasClass('@add', 'existing')
+        ->assertScript('document.querySelector(\'[dusk="add"]\').classList.contains(\'added\')', false)
+        ->assertHasClass('@remove', 'existing')
+        ->assertScript('document.querySelector(\'[dusk="remove"]\').classList.contains(\'absent\')', false)
+        ->waitForLivewire()->click('@renderless')
+        ->assertHasClass('@add', 'existing')
+        ->assertScript('document.querySelector(\'[dusk="add"]\').classList.contains(\'added\')', false)
+        ->assertHasClass('@remove', 'existing')
+        ->assertScript('document.querySelector(\'[dusk="remove"]\').classList.contains(\'absent\')', false)
+        ;
+    }
+
     function test_wire_loading_attr_doesnt_conflict_with_exist_one()
     {
         Livewire::visit(new class extends Component {
